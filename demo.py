812f81dfa433660bb68aecf2740f019d6ed93795
Tony Lee <tonyh.lee@yahoo.com>
2021-12-27 14:57:34 -0800

    add another example to demo

--- demo.py
+++ demo.py
@@ -6,19 +6,53 @@ import getpass
 from urllib import parse
 
 auth = {
-    'username': 'crfm',
-    'password': getpass.getpass(),
+    "username": "crfm",
+    "password": getpass.getpass(),
 }
 
+# Generation
 request = {
-    'prompt': 'Life is like a box of',
-    'model': 'openai/davinci',
+    "prompt": "Life is like a box of",
+    "model": "openai/davinci",
 }
 
 params = {
-    'auth': json.dumps(auth),
-    'request': json.dumps(request),
+    "auth": json.dumps(auth),
+    "request": json.dumps(request),
 }
 
-response = requests.get(f'http://crfm-models.stanford.edu/api/makeRequest?' + parse.urlencode(params)).json()
+response = requests.get(
+    f"http://crfm-models.stanford.edu/api/makeRequest?" + parse.urlencode(params)
+).json()
 print(json.dumps(response, indent=2))
+
+
+# Few-shot sentiment classification
+request = {
+    "prompt": (
+        '\nTweet: "I loved the new Batman movie!" Sentiment: positive###'
+        '\nTweet: "I hate it when my phone battery dies." Sentiment: negative###'
+        '\nTweet: "This is the link to the article" Sentiment: neutral###'
+        '\nTweet: "This new music video was okay." Sentiment:'
+    ),
+    "model": "openai/davinci",
+    "temperature": 0.3,
+    "maxTokens": 60,
+    "topP": 1.0,
+    "frequencyPenalty": 0.5,
+    "presencePenalty": 0.0,
+    "stopSequences": ["###"],
+}
+
+params = {
+    "auth": json.dumps(auth),
+    "request": json.dumps(request),
+}
+
+response = requests.get(
+    f"http://crfm-models.stanford.edu/api/makeRequest?{parse.urlencode(params)}"
+).json()
+
+print(f"Response: {json.dumps(response, indent=2)}")
+sentiment = response["completions"][0]["text"].strip()
+print(f"Output sentiment: {sentiment}")
