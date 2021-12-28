ac56dc906c2eecd41744d80110b526c85e2b3e95
Tony Lee <tonyh.lee@yahoo.com>
2021-12-28 14:30:03 -0800

    pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Run this script before you commit.
+# Run this script before you commit: ./pre-commit.sh
 
-# Script fails immediately when any command fails.
+# This script fails immediately when any of its commands fail.
 set -e
 
 if ! [ -e venv ]; then
   python3 -m pip install virtualenv
   python3 -m virtualenv -p python3 venv
 fi
+venv/bin/pip install -r requirements.txt
 venv/bin/pip install -r requirements.dev.txt
 
 venv/bin/pip check
@@ -20,3 +21,5 @@ venv/bin/black .
 ## Check if there are any mypy or flake8 errors
 venv/bin/mypy .
 venv/bin/flake8 .
+
+echo "Done."
