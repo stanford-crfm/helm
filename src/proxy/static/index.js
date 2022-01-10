5aff38f0cd10b9e91e0115e3ac455f93884099d8
Tony Lee <tonyh.lee@yahoo.com>
2022-01-10 09:48:40 -0800

    Authenticate as soon as the user logs in

--- src/proxy/static/index.js
+++ src/proxy/static/index.js
@@ -45,8 +45,18 @@ $(function () {
         if (!api_key) {
           return;
         }
-        createCookie('api_key', api_key);
-        updateLogin();
+
+        const args = {auth: JSON.stringify({api_key})};
+        $.getJSON('/api/account', args, (response) => {
+          console.log('/api/account', response);
+          if ('error' in response) {
+            alert('The API key you’ve entered is invalid. Try again.');
+          }
+          else {
+            createCookie('api_key', api_key);
+            updateLogin();
+          }
+        });
       }));
     }
   }
