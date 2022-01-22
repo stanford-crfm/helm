2910009dc2fb1bbaac21a91b46e4276309dc704c
Yian Zhang <yian.zhang@nyu.edu>
2022-01-22 14:27:22 -0800

    make compute_reference_metrics() and compute_language_modeling_metrics() methods

--- src/benchmark/basic_metrics.py
+++ src/benchmark/basic_metrics.py
@@ -10,6 +10,11 @@ def exact_match(gold: str, pred: str) -> float:
     return 1 if gold == pred else 0
 
 
+def get_num_bytes(text: str) -> int:
+    """Compute the byte length of the input string"""
+    return len(bytes(text, encoding="utf-8"))
+
+
 class BasicMetric(Metric):
     """
     Defines basic metrics which don't require domain knowledge.  This should be
@@ -22,7 +27,7 @@ class BasicMetric(Metric):
     def __init__(self, names: List[str]):
         self.names = names
 
-    def evaluate_generation(
+    def compute_reference_metrics(
         self, adapter_spec: AdapterSpec, request_state: RequestState, metric_service: MetricService
     ) -> List[Stat]:
         """
@@ -37,69 +42,66 @@ class BasicMetric(Metric):
         - ${score}@k: max_{i,j} score(Gi, Pj)
         """
 
-        def get_num_bytes(text: str) -> int:
-            """Compute the byte length of the input string"""
-            return len(bytes(text, encoding="utf-8"))
+        def compute_metrics_helper(name: str, score_func: Callable[[str, str], float]) -> List[Stat]:
+            score_1 = max(score_func(gold, preds[0]) for gold in golds)
+            score_k = max(score_func(gold, pred) for gold in golds for pred in preds)
 
-        def compute_reference_metrics():
-            """Compute the reference metrics."""
+            return [
+                Stat(name).add(score_1),
+                Stat(f"{name}@{adapter_spec.num_outputs}").add(score_k),
+            ]
 
-            def compute_metrics_helper(name: str, score_func: Callable[[str, str], float]) -> List[Stat]:
-                score_1 = max(score_func(gold, preds[0]) for gold in golds)
-                score_k = max(score_func(gold, pred) for gold in golds for pred in preds)
+        reference_metrics = []
+        if "exact_match" in self.names:
+            # Gold outputs
+            golds = [reference.output for reference in request_state.instance.references if reference.is_correct]
+            assert len(golds) > 0
 
-                return [
-                    Stat(name).add(score_1),
-                    Stat(f"{name}@{adapter_spec.num_outputs}").add(score_k),
-                ]
+            # Predicted outputs
+            assert request_state.result is not None
+            # TODO: Sort the predictions, or take them from the top tokens of the first completion
+            #       https://github.com/stanford-crfm/benchmarking/issues/42
+            preds = [completion.text.strip() for completion in request_state.result.completions]
 
-            reference_metrics = []
+            # Apply mapping if exists (e.g., for multiple-choice questions A -> Boston, B -> New York)
+            if request_state.output_mapping is not None:
+                preds = [request_state.output_mapping.get(pred) for pred in preds]
+            reference_metrics.extend(compute_metrics_helper("exact_match", exact_match))
+        return reference_metrics
 
-            if "exact_match" in self.names:
-                # Gold outputs
-                golds = [reference.output for reference in request_state.instance.references if reference.is_correct]
-                assert len(golds) > 0
+    def compute_language_modeling_metrics(
+        self, adapter_spec: AdapterSpec, request_state: RequestState, metric_service: MetricService
+    ) -> List[Stat]:
+        """Compute the logprob and normalization factors for the first completion"""
+        assert request_state.result is not None
+        sequence = request_state.result.completions[0]
+        logprob, num_tokens, num_bytes = sequence.logprob, len(sequence.tokens), get_num_bytes(sequence.text)
 
-                # Predicted outputs
-                assert request_state.result is not None
-                # TODO: Sort the predictions, or take them from the top tokens of the first completion
-                #       https://github.com/stanford-crfm/benchmarking/issues/42
-                preds = [completion.text.strip() for completion in request_state.result.completions]
+        # Ignore the conditioning prefix
+        conditioning_prefix_length = 0
+        conditioning_prefix_tokens = []
+        for token in sequence.tokens:
+            if conditioning_prefix_length >= len(adapter_spec.conditioning_prefix):
+                break
+            conditioning_prefix_tokens.append(token)
+            conditioning_prefix_length += len(token.text)
+        assert "".join([token.text for token in conditioning_prefix_tokens]) == adapter_spec.conditioning_prefix
 
-                # Apply mapping if exists (e.g., for multiple-choice questions A -> Boston, B -> New York)
-                if request_state.output_mapping is not None:
-                    preds = [request_state.output_mapping.get(pred) for pred in preds]
-                reference_metrics.extend(compute_metrics_helper("exact_match", exact_match))
-            return reference_metrics
+        logprob -= sum(token.logprob for token in conditioning_prefix_tokens)
+        num_tokens -= len(conditioning_prefix_tokens)
+        num_bytes -= get_num_bytes(adapter_spec.conditioning_prefix)
 
-        def compute_language_modeling_metrics():
-            """Compute the logprob and normalization factors for the first completion"""
-            sequence = request_state.result.completions[0]
-            logprob, num_tokens, num_bytes = sequence.logprob, len(sequence.tokens), get_num_bytes(sequence.text)
+        return [Stat("logprob").add(logprob), Stat("num_tokens").add(num_tokens), Stat("num_bytes").add(num_bytes)]
 
-            # Ignore the conditioning prefix
-            conditioning_prefix_length = 0
-            conditioning_prefix_tokens = []
-            for token in sequence.tokens:
-                if conditioning_prefix_length >= len(adapter_spec.conditioning_prefix):
-                    break
-                conditioning_prefix_tokens.append(token)
-                conditioning_prefix_length += len(token.text)
-            assert "".join([token.text for token in conditioning_prefix_tokens]) == adapter_spec.conditioning_prefix
-
-            logprob -= sum(token.logprob for token in conditioning_prefix_tokens)
-            num_tokens -= len(conditioning_prefix_tokens)
-            num_bytes -= get_num_bytes(adapter_spec.conditioning_prefix)
-
-            return [Stat("logprob").add(logprob), Stat("num_tokens").add(num_tokens), Stat("num_bytes").add(num_bytes)]
-
+    def evaluate_generation(
+        self, adapter_spec: AdapterSpec, request_state: RequestState, metric_service: MetricService
+    ) -> List[Stat]:
+        """Compute the reference metrics and language modeling metrics"""
         metrics = []
-
         if len(request_state.instance.references) > 0:
-            metrics.extend(compute_reference_metrics())
+            metrics.extend(self.compute_reference_metrics(adapter_spec, request_state, metric_service))
 
-        if request_state.result is not None:
-            metrics.extend(compute_language_modeling_metrics())
+        metrics.extend(self.compute_language_modeling_metrics(adapter_spec, request_state, metric_service))
 
         # Future: add F1, BLEU, etc.
         # TODO: pass in arguments to `BasicMetric`
