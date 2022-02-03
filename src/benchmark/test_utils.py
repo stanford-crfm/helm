2ec68cba6d2cae4c3229a80de78894f8458d02fd
Eric Zelikman <ezelikman@stanford.edu>
2022-02-03 09:30:04 -0800

    increased eval instances

--- src/benchmark/test_utils.py
+++ src/benchmark/test_utils.py
@@ -154,7 +154,7 @@ def get_lpm_spec(difficulty: str) -> RunSpec:
         method=ADAPT_GENERATION,
         instructions="Please solve the following problem.",
         max_train_instances=3,
-        max_eval_instances=10,
+        max_eval_instances=100,
         num_outputs=3,
         num_train_trials=1,
         model="openai/davinci",
