a19ba9ca7ca1dc1c97ba0940a0a72ea445993f6e
Eric Zelikman <ezelikman@stanford.edu>
2022-02-05 13:44:14 -0800

    refactored code into dataclasses

--- src/benchmark/lpm_scenario.py
+++ src/benchmark/lpm_scenario.py
@@ -1,8 +1,10 @@
 import random
+import dataclasses
 from copy import copy
-from typing import List
+from typing import List, Dict, Literal, Tuple
 from common.hierarchical_logger import hlog
 from .scenario import Scenario, Instance, Reference, TRAIN_TAG, VALID_TAG, TEST_TAG, CORRECT_TAG
+from dataclasses import dataclass
 
 """Language Pattern Matching Scenario.
 
@@ -53,7 +55,88 @@ We also support multiple levels of difficulty.
 """
 
 
-def get_vocab():
+@dataclass
+class LanguageLogicalStatement:
+    subject: str  # e.g. either the individual or group to which this statement applies
+    subject_category: str  # e.g. the group to which this fact applies
+    specifier_type: Literal["a", "the"]  # the specifier used for the subject
+
+    def generate_specified_subject(self, upper=False, specifier_type=None) -> str:
+        """Figure out whether to use "A" or "An" (heuristic), handles "the" and "The" too
+        specifier_type can be used to optionally override the class property specifier_type
+
+        Example:
+            if (subject="cat", subject_category="animal", specifier_type="the", upper=False) -> "the cat"
+            if (subject="apple", subject_category="plant", specifier_type="a", upper=True) -> "An apple"
+        """
+
+        specifier_type = self.specifier_type if specifier_type is None else specifier_type
+        if not (self.subject_category != "person") or (self.subject == "person"):
+            return self.subject
+        base_char = specifier_type[0].upper() if upper else specifier_type[0].lower()
+        if specifier_type == "a":
+            if self.subject[0].lower() in ["a", "e", "i", "o", "u"]:
+                return f"{base_char}n {self.subject}"
+        else:
+            return f"{base_char}{specifier_type[1:]} {self.subject}"
+        return f"{base_char} {self.subject}"
+
+
+@dataclass
+class LanguageRule(LanguageLogicalStatement):
+    """Class describing how a set of attributes about an individual/group imply another attribute."""
+
+    condition: List[str]  # a list of attributes which must apply for the rule to apply
+    condition_conjunction: Literal["and", "or"]  # "and" or "or", corresponding to
+    consequent: str  # the attribute resulting from the application of the rule
+
+    def __str__(self) -> str:
+        """Takes in a rule as a dictionary, i.e. corresponding to "if x (and/or y) then z"
+        and returns a string corresponding to the rule
+
+        Rules should have the following format:
+        {
+            'subject': 'person',
+            'condition': ['red', 'kind'],
+            'condition_conjunction': 'and',
+            'consequent': 'cold'
+            'specified': True
+        }
+
+        and this example will output a string: "If a person is red and kind, then the person is cold."
+        """
+
+        condition = f" {self.condition_conjunction} ".join(self.condition)
+        specified_subject = self.generate_specified_subject()
+        specified_particular_subject = self.generate_specified_subject(specifier_type="the")
+        return f"If {specified_subject} is {condition}, then {specified_particular_subject} is {self.consequent}."
+
+
+@dataclass
+class LanguageFact(LanguageLogicalStatement):
+    """Class describing a statement that a subject has some attributes."""
+
+    specific_attributes: List[str]  # more specific versions of the attributes
+    generic_attributes: List[str]  # a list of attributes which apply to the subject
+    use_specific_attributes: bool  # whether to use the more specific attributes
+    prefix: str = ""  # the attribute resulting from the application of the rule
+    upper: bool = True  # whether the statement should be uppercase
+
+    def __str__(self) -> str:
+        """Maps from a set of attributes about a subject to a string
+
+        e.g. if (subject="dog", attributes=["big", "red"], specifier="The") ->
+        "The dog is big and red."
+        """
+
+        if len(self.generic_attributes) == 0:
+            return "Nothing."
+        target_attributes = self.specific_attributes if self.use_specific_attributes else self.generic_attributes
+        specified_subject = self.generate_specified_subject(upper=self.upper)
+        return f"{self.prefix}{specified_subject} is {' and '.join(target_attributes)}."
+
+
+def get_vocab() -> Tuple[Dict[str, List[str]], Dict[str, List[str]]]:
     """All potential subjects for the facts and rules for LPM as well as their categories.
     Subjects is a dictionary of subject categories like "person" and "animal" which correspond to
     a list of potential subjects.
@@ -62,11 +145,11 @@ def get_vocab():
     Intially, we default to not including these attributes, but we leave this feature in for convenience.
 
     Attribute groups are a more general version of attributes, where a single attribute corresponds to a class of
-    attributes e.g. if we know something is chilly, we know that it is cold (but not assuming the reverse.
+    attributes e.g. if we know something is chilly, we know that it is cold (but not assuming the reverse).
     """
 
     # A list of subjects and their categories
-    subjects = {
+    subjects: Dict[str, List[str]] = {
         "person": ["Alice", "Bob", "Carol", "Dan", "Erin", "Frank"],
         "animal": [
             "dog",
@@ -88,10 +171,10 @@ def get_vocab():
     }
 
     # A list of attributes for the facts and rules which only correspond to themselves
-    attributes = ["young", "rough", "soft", "sad", "scary", "white", "grey", "black"]
+    attributes: List[str] = ["young", "rough", "soft", "sad", "scary", "white", "grey", "black"]
 
     # Convert list of attributes into dictionary
-    attribute_groups = {attribute: [attribute] for attribute in attributes}
+    attribute_groups: Dict[str, List[str]] = {attribute: [attribute] for attribute in attributes}
     # A list of attributes and their overarching meaning (used in hard difficulty)
     attribute_groups.update(
         {
@@ -119,69 +202,18 @@ def get_vocab():
         }
     )
     # Remove any keys which duplicate subitems
-    new_attribute_groups = copy(attribute_groups)
+    new_attribute_groups: Dict[str, List[str]] = copy(attribute_groups)
     for general_attribute, specific_attributes in attribute_groups.items():
         for specific_attribute in specific_attributes:
             if (general_attribute != specific_attribute) and (specific_attribute in attribute_groups):
                 del new_attribute_groups[specific_attribute]
-    attribute_groups = new_attribute_groups
 
-    return attribute_groups, subjects
-
-
-def generate_specifier(subject, upper=False):
-    """Figure out whether to use "A" or "An" (heuristic)
-
-    This determines whether to use A or An in a given sentence for a subject.
-
-    Example:
-        generate_specifier("cat", upper=False) -> "a"
-        generate_specifier("apple", upper=True) -> "An"
-    """
-
-    base_char = "A" if upper else "a"
-    if subject[0].lower() in ["a", "e", "i", "o", "u"]:
-        return base_char + "n"
-    return base_char
+    return new_attribute_groups, subjects
 
 
-def parse_rule(rule):
-    """Takes in a rule as a dictionary, i.e. corresponding to "if x (and/or y) then z"
-    and returns a string corresponding to the rule
-
-    Rules should have the following format:
-    {
-        'subject': 'person',
-        'condition': ['red', 'kind'],
-        'condition_conjunction': 'and',
-        'consequent': 'cold'
-        'specified': True
-    }
-
-    and this example will output a string: "If a person is red and kind, then the person is cold."
-    """
-
-    condition = f" {rule['condition_conjunction']} ".join(rule["condition"])
-    specified = rule["specified"]
-    if specified:
-        specifier = generate_specifier(rule["subject"])
-        return f"If {specifier} {rule['subject']} is {condition}, then the {rule['subject']} is {rule['consequent']}."
-    return f"If {rule['subject']} is {condition}, then {rule['subject']} is {rule['consequent']}."
-
-
-def parse_fact(subject, attributes, prefix="", specifier=""):
-    """Maps from a set of attributes about a subject to a string
-
-    e.g. parse_fact(subject="dog", attributes=["big", "red"], specifier="The") ->
-    "The dog is big and red."
-    """
-
-    if len(attributes) == 0:
-        return "Nothing."
-    return f"{prefix}{specifier}{subject} is {' and '.join(attributes)}."
-
-
-def generate_rules(attribute_groups, subject_category, subject, max_rules=5, specific_category=False):
+def generate_rules(
+    attribute_groups, subject, subject_category, max_rules=5, specific_category=False
+) -> List[LanguageRule]:
     """ Generates a random set of rules about a subject as dictionaries,
     given a list of potential attributes and the category (e.g. person) of the subject (e.g. Alice)
 
@@ -190,57 +222,74 @@ def generate_rules(attribute_groups, subject_category, subject, max_rules=5, spe
     """
     attributes_shuffled = list(attribute_groups.keys()).copy()
     random.shuffle(attributes_shuffled)
-    rules = []
+    rules: List[LanguageRule] = []
     hlog("Generating language pattern matching examples")
     while len(attributes_shuffled) > 2 and len(rules) < max_rules:
-        rule_subject_category = subject if specific_category else random.choice([subject_category, subject])
+        rule_subject = subject if specific_category else random.choice([subject_category, subject])
         n_rule_attributes = random.randint(2, 3)
         rule_attributes, attributes_shuffled = (
             attributes_shuffled[:n_rule_attributes],
             attributes_shuffled[n_rule_attributes:],
         )
-        condition = rule_attributes[:-1]
-        condition_conjunction = random.choice(["and", "or"])
-        consequent = rule_attributes[-1]
-        specied = (subject_category != "person") or (rule_subject_category == "person")
         rules.append(
-            {
-                "subject": rule_subject_category,
-                "condition": condition,
-                "condition_conjunction": condition_conjunction,
-                "consequent": consequent,
-                "specified": specied,
-            }
+            LanguageRule(
+                subject=rule_subject,
+                subject_category=subject_category,
+                specifier_type="a",
+                condition=rule_attributes[:-1],
+                condition_conjunction=random.choice(["and", "or"]),
+                consequent=rule_attributes[-1],
+            )
         )
     return rules
 
 
-def generate_test(attribute_groups, subject, rules, p_consequenceless=0.1):
+def generate_test(
+    attribute_groups, subject, subject_category, rules, use_specific_attributes, p_consequenceless=0.1
+) -> Tuple[LanguageFact, List[LanguageRule], LanguageFact]:
     """ Generates a test case given a set of rules, i.e. a statement about the subject from which something
     can be potentially deduced given the rules. We include an argument, p_consequenceless, to re-roll with
     some probability if the generated fact does not allow anything to be determined.
     """
-    test_attributes = random.sample(list(attribute_groups.keys()), 2)
-    test_attributes_specific = [random.choice(attribute_groups[subcondition]) for subcondition in test_attributes]
-    test_consequents = []
-    test_rules_used = []
+
+    # The generic attributes which the test fact will assign to the subject
+    test_attributes: List[str] = random.sample(list(attribute_groups.keys()), 2)
+    # The specific versions of the test attributes
+    test_attributes_specific: List[str] = [
+        random.choice(attribute_groups[subcondition]) for subcondition in test_attributes
+    ]
+    test_consequents: List[str] = []  # The attributes implied by the test attributes and rules
+    test_rules_used: List[LanguageRule] = []
     for rule in rules:
-        condition_conjunction = rule["condition_conjunction"]
-        rule_condition = rule["condition"]
-        rule_consequent = rule["consequent"]
-        if rule_consequent in test_attributes:
+        if rule.consequent in test_attributes:
             continue
-        if condition_conjunction == "and":
-            if set(rule_condition).issubset(test_attributes):
+        if rule.condition_conjunction == "and":
+            if set(rule.condition).issubset(test_attributes):
                 test_rules_used.append(rule)
-                test_consequents.append(rule_consequent)
-        elif condition_conjunction == "or":
-            if not set(rule_condition).isdisjoint(test_attributes):
+                test_consequents.append(rule.consequent)
+        elif rule.condition_conjunction == "or":
+            if not set(rule.condition).isdisjoint(test_attributes):
                 test_rules_used.append(rule)
-                test_consequents.append(rule_consequent)
+                test_consequents.append(rule.consequent)
     if len(test_consequents) == 0 and random.random() > p_consequenceless:
-        return generate_test(attribute_groups, subject, rules, p_consequenceless)
-    return test_attributes, test_attributes_specific, test_consequents, test_rules_used
+        return generate_test(
+            attribute_groups, subject, subject_category, rules, use_specific_attributes, p_consequenceless
+        )
+
+    test_fact = LanguageFact(
+        subject,
+        subject_category,
+        specifier_type="the",
+        specific_attributes=test_attributes_specific,
+        generic_attributes=test_attributes,
+        use_specific_attributes=use_specific_attributes,
+    )
+
+    target_fact: LanguageFact = dataclasses.replace(
+        test_fact, specific_attributes=test_consequents, generic_attributes=test_consequents,
+    )
+
+    return test_fact, test_rules_used, target_fact
 
 
 class LPMScenario(Scenario):
@@ -255,47 +304,45 @@ class LPMScenario(Scenario):
 
     def __init__(self, difficulty: str, random_seed=42):
         self.attribute_groups, self.subjects = get_vocab()
+
         # specific_category specifies that the specific category should always be used
         # e.g. "dog" instead of "an animal"
-        self.specific_category = difficulty == "easy"
-        # generic_attributes specifies that the top level attribute should always be used
-        # e.g. "cold" instead of "chill"
-        self.generic_attributes = difficulty != "hard"
-        self.include_intermediates = False
-        self.num_train_instances = 5
-        self.num_val_instances = 50
-        self.num_test_instances = 50
+        self.specific_category: bool = difficulty == "easy"
+        # use_specific_attributes specifies that the synonymous attributes can be used
+        # e.g. "chill" instead of "cold"
+        self.use_specific_attributes: bool = difficulty == "hard"
+        self.include_intermediates: bool = True
+        self.num_train_instances: int = 5
+        self.num_val_instances: int = 50
+        self.num_test_instances: int = 50
         random.seed(random_seed)
+
+    def generate_problem(self) -> Tuple[List[LanguageRule], LanguageFact, List[LanguageRule], LanguageFact]:
+        subject_category = random.choice(list(self.subjects.keys()))
+        subject = random.choice(self.subjects[subject_category])
+        rules = generate_rules(
+            self.attribute_groups, subject, subject_category, specific_category=self.specific_category
+        )
+        test_fact, test_rules_used, target_fact = generate_test(
+            self.attribute_groups, subject, subject_category, rules, self.use_specific_attributes
+        )
+        return rules, test_fact, test_rules_used, target_fact
 
     def get_instances(self) -> List[Instance]:
         # Read all the instances
         instances = []
 
         for sample_idx in range(self.num_train_instances + self.num_val_instances + self.num_test_instances):
-            subject_category = random.choice(list(self.subjects.keys()))
-            subject = random.choice(self.subjects[subject_category])
-            rules = generate_rules(
-                self.attribute_groups, subject_category, subject, specific_category=self.specific_category
-            )
-            test_attributes, test_attributes_specific, test_consequents, test_rules_used = generate_test(
-                self.attribute_groups, subject, rules
-            )
+            rules, test_fact, test_rules_used, target_fact = self.generate_problem()
 
-            question = "\n".join(parse_rule(rule) for rule in rules) + "\n"
-
-            test_specifier_base = generate_specifier(subject, upper=True)
-            test_specifier_first = test_specifier_base + " " if subject_category != "person" else ""
-            test_specifier_second = "The " if subject_category != "person" else ""
-            test_specifier_third = "the " if subject_category != "person" else ""
-            print_test_attributes = test_attributes if self.generic_attributes else test_attributes_specific
-            question += "Fact:\n"
-            question += parse_fact(subject, print_test_attributes, specifier=test_specifier_first) + "\n"
+            question = "\n".join(str(rule) for rule in rules) + "\n"
+            test_specified_subject = test_fact.generate_specified_subject(upper=False)
+            question += f"Fact:\n{test_fact}\n"
             if self.include_intermediates:
-                question += "Rule(s) used:\n"
-                for rule in test_rules_used:
-                    question += parse_rule(rule) + "\n"
-            question += f"The following can be determined about {test_specifier_third}{subject}:\n"
-            correct_answer = parse_fact(subject, test_consequents, specifier=test_specifier_second)
+                question += "Rule(s) used:\n" + "\n".join(str(test_rule) for test_rule in test_rules_used) + "\n"
+            question += f"The following can be determined about {test_specified_subject}:\n"
+
+            print(question)
 
             if sample_idx < self.num_train_instances:
                 cur_tag = TRAIN_TAG
@@ -304,7 +351,7 @@ class LPMScenario(Scenario):
             else:
                 cur_tag = TEST_TAG
             instance = Instance(
-                input=question, references=[Reference(output=correct_answer, tags=[CORRECT_TAG])], tags=[cur_tag],
+                input=question, references=[Reference(output=str(target_fact), tags=[CORRECT_TAG])], tags=[cur_tag],
             )
             instances.append(instance)
 
