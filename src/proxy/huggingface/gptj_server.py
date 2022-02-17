8164f6b2e46667351e2949d4ff6b0be1698d1e21
Deepak Narayanan <deepakn94@gmail.com>
2022-02-17 23:42:48 +0000

    Get logprobs

--- src/proxy/huggingface/gptj_server.py
+++ src/proxy/huggingface/gptj_server.py
@@ -1,11 +1,14 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 
 
 def serve_request(raw_request):
-    encoded_input = tokenizer(raw_request['prompt'], return_tensors='pt')
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B").to(device)
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+
+    encoded_input = tokenizer(raw_request['prompt'], return_tensors='pt').to(device)
     output = model.generate(
         **encoded_input,
         temperature=raw_request['temperature'],
@@ -14,6 +17,13 @@ def serve_request(raw_request):
         output_scores=True
     )
     gen_tokens = output.sequences
+    all_logprobs = []
+    for i in range(len(gen_tokens[0]) - len(encoded_input.input_ids[0])):
+        logprobs = torch.log(torch.nn.functional.softmax(output.scores[i][0]))
+        all_logprobs.append(logprobs)
+        # Log probability of selected token is given by:
+        # >> j = i + len(encoded_input.input_ids[0])
+        # >> print(i, logprobs[gen_tokens[0][j]])
     gen_tokens = [tokenizer.convert_ids_to_tokens(gen_token) for gen_token in gen_tokens]
     return gen_tokens
 
