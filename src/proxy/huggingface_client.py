ea3f5a7884507a986f8fcd1be190da58d482af81
Deepak Narayanan <deepakn94@gmail.com>
2022-02-18 22:14:22 +0000

    Refactor code a bit

--- src/proxy/huggingface_client.py
+++ src/proxy/huggingface_client.py
@@ -1,14 +1,71 @@
-from huggingface import gptj_server
-
 from common.cache import Cache
 from common.request import Request, RequestResult, Sequence, Token
 from client import Client, wrap_request_time
 
 
+import time
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+class GPTJServer:
+
+    def __init__(self):
+        self.device: str = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+        start_time: float = time.time()
+        self.model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B").to(self.device)
+        self.tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+        runtime: float = time.time() - start_time
+        print(f"Done loading model and tokenizer! Took {runtime:.2f} seconds...")
+
+    def serve_request(self, raw_request):
+        encoded_input = self.tokenizer(raw_request["prompt"], return_tensors="pt").to(self.device)
+        output = self.model.generate(
+            **encoded_input,
+            temperature=raw_request["temperature"],
+            max_length=raw_request["max_tokens"],
+            num_return_sequences=raw_request["num_completions"],
+            do_sample=True,
+            return_dict_in_generate=True,
+            output_scores=True,
+        )
+        sequences = output.sequences
+        all_logprobs = []
+        logprobs_of_chosen_tokens = []
+        for i in range(len(sequences[0]) - len(encoded_input.input_ids[0])):
+            logprobs = torch.log(torch.nn.functional.softmax(output.scores[i][0]))
+            all_logprobs.append(logprobs)
+
+            # Get log probability of chosen token.
+            j = i + len(encoded_input.input_ids[0])
+            logprobs_of_chosen_tokens.append(logprobs[sequences[0][j]].item())
+
+        # Remove prompt from the start of each sequence if echo_prompt is False.
+        if not raw_request["echo_prompt"]:
+            sequences = [sequence[len(encoded_input.input_ids[0]) :] for sequence in sequences]
+        all_tokens = [self.tokenizer.convert_ids_to_tokens(sequence) for sequence in sequences]
+        all_decoded_text = self.tokenizer.batch_decode(sequences)
+
+        completions = []
+        for (decoded_text, tokens) in zip(all_decoded_text, all_tokens):
+            completions.append({"text": decoded_text, "tokens": tokens, "logprobs": logprobs_of_chosen_tokens})
+
+        return {"completions": completions}
+
+
 class HuggingFaceClient(Client):
-    def __init__(self, server_location: str, cache_path: str):
-        self.server_location = server_location
+    def __init__(self, cache_path: str):
         self.cache = Cache(cache_path)
+        self.model_stubs = {}
+
+    def get_model_stub(self, model_name):
+        if model_name not in self.model_stubs:
+            if model_name == "huggingface/gptj_6b":
+                self.model_stubs[model_name] = GPTJServer()
+            else:
+                raise Exception("Unknown model!")
+        return self.model_stubs[model_name]
 
     def make_request(self, request: Request) -> RequestResult:
         print(request)
@@ -26,11 +83,12 @@ class HuggingFaceClient(Client):
             "frequency_penalty": request.frequency_penalty,
             "echo_prompt": request.echo_prompt,
         }
+        model_stub = self.get_model_stub("huggingface/gptj_6b")
 
         try:
 
             def do_it():
-                return gptj_server.serve_request(raw_request)
+                return model_stub.serve_request(raw_request)
 
             cache_key = Client.make_cache_key(raw_request, request)
             response, cached = self.cache.get(cache_key, wrap_request_time(do_it))
@@ -54,5 +112,6 @@ class HuggingFaceClient(Client):
 
 
 if __name__ == "__main__":
-    client = HuggingFaceClient(server_location="localhost", cache_path="huggingface_cache")
+    client = HuggingFaceClient(cache_path="huggingface_cache")
     print(client.make_request(Request(prompt="I am a computer scientist.", num_completions=2)))
+    print(client.make_request(Request(prompt="My name is Joe.", num_completions=1, max_tokens=10)))
