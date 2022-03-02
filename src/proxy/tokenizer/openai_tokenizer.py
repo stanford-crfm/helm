e00d8993760ef87b8025c419ce439390b07d8ec8
Yian Zhang <yian.zhang@nyu.edu>
2022-03-01 20:30:18 -0800

    implement tokenizers

---
+++ src/proxy/tokenizer/openai_tokenizer.py
@@ -0,0 +1,35 @@
+from typing import List
+from transformers import GPT2TokenizerFast
+
+from .tokenizer import Tokenizer
+
+
+class OpenAITokenizer(Tokenizer):
+
+    # The max length of the model input, not the max length of a request (2049).
+    MAX_SEQ_LEN = 2048
+
+    END_OF_TEXT_TOKEN = "<|endoftext|>"
+
+    def __init__(self):
+        # OpenAI uses the same tokenizer for GPT-2 and GPT-3.
+        # Weights are cached at ~/.cache/huggingface/transformers.
+        self._tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+    def encode(self, text: str) -> List[int]:
+        """
+        Encodes the input text to tokens.
+        """
+        return self._tokenizer.encode(text)
+
+    def decode(self, tokens: List[int]) -> str:
+        """
+        Given a list of tokens, outputs the corresponding text.
+        """
+        return self._tokenizer.decode(tokens, clean_up_tokenization_spaces=False)
+
+    def tokenize(self, text: str) -> List[str]:
+        """
+        Tokenizes the text.
+        """
+        return self._tokenizer.tokenize(text)
---
+++ src/proxy/tokenizer/tokenizer.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Tokenizer(ABC):
+
+    # The max length of the model input, not the max length of a request.
+    MAX_SEQ_LEN: int
+
+    END_OF_TEXT_TOKEN: str
+
+    @abstractmethod
+    def encode(self, text: str) -> List[int]:
+        """
+        Encodes the input text to tokens.
+        """
+        pass
+
+    @abstractmethod
+    def decode(self, tokens: List[int]) -> str:
+        """
+        Given a list of tokens, outputs the corresponding text.
+        """
+        pass
+
+    @abstractmethod
+    def tokenize(self, text: str) -> List[str]:
+        """
+        Tokenizes the text.
+        """
+        pass
