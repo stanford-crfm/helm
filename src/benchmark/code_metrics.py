8d00d49fe40e95c429c56b159b7515ca297d0893
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-03-05 20:21:04 -0800

    Fix typing errors.

--- src/benchmark/code_metrics.py
+++ src/benchmark/code_metrics.py
@@ -12,19 +12,18 @@ from .metric_service import MetricService
 
 def _convert_scores(scores: Sequence[Union[int, bool]]) -> List[float]:
     """Convert boolean scores to int."""
-    # `scores` is returned by `run_test`, and is a list of bools/ints.
+    # `scores` is returned by `code_metrics_helper.run_test` and is a list of bools/ints.
     return [1.0 if isinstance(score, bool) and score else 0.0 for score in scores]
 
 
-def _test_avg(scores: Sequence[Union[int, bool]]) -> float:
+def _test_avg(scores: List[float]) -> float:
     """Compute the average number of tests passed."""
-    scores = _convert_scores(scores)
-    # Division by zero should not be a convern, given our data processing.
+    # Division by zero should not be a concern, given our data processing.
     return sum(scores) / len(scores)
 
 
-def _strict_acc(scores: Sequence[Union[int, bool]]) -> float:
-    scores = _convert_scores(scores)
+def _strict_acc(scores: List[float]) -> float:
+    """Return 1.0 if all tests passed; otherwise return 0.0."""
     return 1.0 if sum(scores) == len(scores) else 0.0
 
 
@@ -57,12 +56,13 @@ class APPSMetric(Metric):
         for name in self.names:
             metric_fn = METRICS[name]
 
-            score = 0.0
+            best_score = 0.0
             for completion in request_result.completions:
                 completion = completion.text.strip()
                 scores = code_metrics_helper.run_test(root=root, test=completion)  # type: ignore
-                metric = metric_fn(scores)
-                if metric > score:
-                    score = metric
-            metrics.append(Stat(name).add(score))
+                scores = _convert_scores(scores)  # Convert list of bool/int to list of ints.
+                this_score = metric_fn(scores)
+                if this_score > best_score:
+                    best_score = this_score
+            metrics.append(Stat(name).add(best_score))
         return metrics
