f2abb7cb716088c59beef65bd639dbb9a5406c80
dilara <dilara>
2022-03-15 23:03:49 -0700

    Modify init

--- src/benchmark/__init__.py
+++ src/benchmark/__init__.py
@@ -3,6 +3,7 @@
 # Scenarios
 from . import simple_scenarios  # noqa
 from . import mmlu_scenario  # noqa
+from . import msmarco_scenario
 from . import commonsense_qa_scenario  # noqa
 from . import twitter_aae_scenario  # noqa
 from . import real_toxicity_prompts_scenario  # noqa
@@ -16,6 +17,7 @@ from . import raft_scenario  # noqa
 
 from . import basic_metrics  # noqa
 from . import commonsense_qa_metrics  # noqa
+from . import msmarco_metrics
 from . import toxicity_metrics  # noqa
 from . import tokens_metric  # noqa
 from . import copyright_metrics  # noqa
