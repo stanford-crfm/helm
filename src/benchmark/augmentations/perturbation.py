abaa66ba60afe74acebd896155db68f9c8b2c483
Yuta Koreeda <yuta.koreeda@hal.hitachi.com>
2022-03-17 22:19:58 -0700

    Implement CityNameReplacementPerturbation This commit makes reference to https://github.com/GEM-benchmark/NL-Augmenter/tree/1.0.0/transformations/city_names_transformation and adopts some files.

--- src/benchmark/augmentations/perturbation.py
+++ src/benchmark/augmentations/perturbation.py
@@ -1,10 +1,15 @@
+import itertools
+import os
+import random
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, replace
-from typing import List
+from typing import List, Set
 
-from .perturbation_description import PerturbationDescription
+import spacy
+
 from benchmark.scenario import Instance, Reference
 from common.object_spec import ObjectSpec, create_object
+from .perturbation_description import PerturbationDescription
 
 
 class Perturbation(ABC):
@@ -88,3 +93,80 @@ class ExtraSpacePerturbation(Perturbation):
 
     def perturb(self, text: str) -> str:
         return text.replace(" ", " " * self.num_spaces)
+
+
+def pairwise(iterable):
+    # "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    # From https://docs.python.org/3.8/library/itertools.html#itertools.pairwise
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+@dataclass
+class CityNameReplacementPerturbation(Perturbation):
+    """
+    A perturbation that replaces the name of a populous city to less populated
+    ones. This implements B20 of Dhole et al. 2021. "NL-Augmenter A Framework
+    for Task-Sensitive Natural Language Augmentation". arXiv:2112.02721. with
+    a reference to https://github.com/GEM-benchmark/NL-Augmenter/blob/1.0.0/
+    transformations/city_names_transformation/transformation.py
+
+    This maps each city name to a random city. All city names with the same
+    surface text will be mapped to a same name unlike the original
+    implementation.
+    """
+
+    name: str = "city_name_replacement"
+    replacees_filename: str = "Eng_Pop.txt"
+    replacers_filename: str = "Eng_Scarce.txt"
+
+    def __init__(self, nlp=None):
+        if nlp is None:
+            try:
+                self.nlp = spacy.load("en_core_web_sm", exclude=["tagger", "parser", "lemmatizer", "textcat"])
+            except OSError:
+                spacy.cli.download("en_core_web_sm")
+                self.nlp = spacy.load("en_core_web_sm", exclude=["tagger", "parser", "lemmatizer", "textcat"])
+        else:
+            self.nlp = nlp
+        data_dir = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(data_dir, self.replacees_filename)) as fin:
+            self.replacees: Set[str] = set(fin.read().split("\n"))
+        with open(os.path.join(data_dir, self.replacers_filename)) as fin:
+            self.replacers: List[str] = sorted(set(fin.read().split("\n")))
+
+    def _is_target(self, ner_tag: str, text: str) -> bool:
+        return (ner_tag == "GPE" or ner_tag == "LOC") and text in self.replacees
+
+    @staticmethod
+    def _span_overlap(spans) -> bool:
+        return any((s1[1] > s2[0] for s1, s2 in pairwise(sorted(spans, key=lambda s: s[0]))))
+
+    def perturb(self, text: str) -> str:
+        doc = self.nlp(text)
+        replaced_spans = [
+            (doc[ent.start].idx, doc[ent.end - 1].idx + len(doc[ent.end - 1]), ent.text)
+            for ent in doc.ents
+            if self._is_target(ent.label_, ent.text)
+        ]
+        assert not self._span_overlap(replaced_spans)
+
+        city_names = {span[2] for span in replaced_spans}
+        if len(city_names) < len(self.replacers):
+            replacers = random.sample(self.replacers, len(city_names))
+        else:
+            # in case there are more city names than our list
+            replacers = random.choices(list(self.replacers), k=len(city_names))
+        # map the same city names to a single city name unlike original impl.
+        # make sure that it doesn't choose the same city name twice
+        name_mapping = dict(zip(city_names, replacers))
+
+        last_char_idx = 0
+        text_fragments = []
+        for span in sorted(replaced_spans, key=lambda s: s[0]):
+            text_fragments.append(text[last_char_idx : span[0]])
+            text_fragments.append(name_mapping[span[2]])
+            last_char_idx = span[1]
+        text_fragments.append(text[last_char_idx:])
+        return "".join(text_fragments)
--- src/benchmark/augmentations/test_perturbation.py
+++ src/benchmark/augmentations/test_perturbation.py
@@ -1,9 +1,13 @@
+import random
 from typing import List
 
+from benchmark.augmentations.perturbation import (
+    IdentityPerturbation,
+    ExtraSpacePerturbation,
+    CityNameReplacementPerturbation,
+)
 from benchmark.scenario import Instance, Reference
-
 from .data_augmenter import DataAugmenter
-from benchmark.augmentations.perturbation import IdentityPerturbation, ExtraSpacePerturbation
 
 
 def test_identity_perturbation():
@@ -26,3 +30,36 @@ def test_extra_space_perturbation():
     assert instances[0].perturbation.num_spaces == 2
     assert instances[0].input == "Hello  my  name  is"
     assert instances[0].references[0].output == "some  name"
+
+
+def test_city_name_replacement_perturbation():
+    def _perturb(text) -> str:
+        instance = Instance(input=text, references=[])
+        random.seed(123)
+        perturbation = CityNameReplacementPerturbation()
+        output: Instance = perturbation.apply("id0", instance)
+        assert output.id == "id0"
+        assert output.perturbation.name == "city_name_replacement"  # type: ignore
+        return output.input
+
+    output = _perturb("I think San Francisco is a nice city.")
+    assert output == "I think Bell is a nice city."
+
+    # Not in the populous city list (but in the scarcely populated city list)
+    text = "I think Stanford is a nice place."
+    output = _perturb(text)
+    assert output == text
+
+    # Starts with a city name
+    output = _perturb("San Francisco is a nice city.")
+    assert output == "Bell is a nice city."
+
+    # Non-city mentions
+    output = _perturb("San Francisco Giants is looking at buying properties in San Francisco.")
+    assert output == "San Francisco Giants is looking at buying properties in Bell."
+
+    # Multiple mentions
+    output = _perturb(
+        "San Jose is a nice city near San Francisco. It is one hour drive from San Jose to San Francisco."
+    )
+    assert output == "Forest Park is a nice city near Bell. It is one hour drive from Forest Park to Bell."
