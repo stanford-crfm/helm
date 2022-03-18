d7ac5303edafafc1a506d568ac9fa288be63b0a6
Michael Xie <xie@cs.stanford.edu>
2022-03-17 20:08:52 -0700

    misspellings

--- src/benchmark/augmentations/perturbation.py
+++ src/benchmark/augmentations/perturbation.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, replace
 from typing import List
+import numpy as np
+import json
+from pathlib import Path
+from ntlk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 from .perturbation_description import PerturbationDescription
 from benchmark.scenario import Instance, Reference
@@ -88,3 +93,65 @@ class ExtraSpacePerturbation(Perturbation):
 
     def perturb(self, text: str) -> str:
         return text.replace(" ", " " * self.num_spaces)
+
+
+# The implementation below is based on the following list of common misspellings:
+# https://en.wikipedia.org/wiki/Wikipedia:Lists_of_common_misspellings/For_machines
+@dataclass
+class Misspellings(Perturbation):
+    """
+	Replaces words randomly with common misspellings, from a list of common misspellings.
+    Perturbation example:
+    Input:
+		Already, the new product is not available.
+    Output:
+		Aready, the new product is not availible.
+    """
+
+    @dataclass(frozen=True)
+    class Description(PerturbationDescription):
+        name: str
+
+    name: str = "misspellings"
+
+    def __init__(self, prob: float):
+		'''
+		prob (float): probability between [0,1] of perturbing a word to a common misspelling (if we have a common misspelling for the word)
+		'''
+        self.prob = prob
+		misspellings_file = Path(__file__).resolve().expanduser().parent / 'correct_to_misspelling.json'
+		with open(misspellings_file, 'r') as f:
+			self.correct_to_misspelling = json.load(f)
+        self.detokenizer = TreebankWordDetokenizer()
+
+    @property
+    def description(self) -> PerturbationDescription:
+        return Misspellings.Description(self.name)
+
+    def count_upper(self, word: str) -> int:
+        return sum(c.isupper() for c in word)
+
+    def perturb_word(self, word: str) -> str:
+        perturbed = False
+        if word in self.correct_to_misspelling and np.random.rand() < self.prob:
+            word = np.random.choice(self.correct_to_misspelling[word])
+            perturbed = True
+        return word, perturbed
+
+    def perturb_seed(self, text: str, seed: int=0) -> str:
+        words = word_tokenize(text)
+        new_words = []
+        for word in words:
+            # check if word in original form is in dictionary
+            word, perturbed = self.perturb_word(word)
+            if not perturbed and self.count_upper(word) == 1 and word[0].isupper():
+                # if only the first letter is upper case
+                # check if the lowercase version is in dict
+                lowercase_word = word.lower()
+                word, perturbed = self.perturb_word(lowercase_word)
+                word[0] = word[0].upper()
+            new_words.append(word)
+        return self.detokenizer.detokenize(new_words)
+
+    def perturb(self, text: str) -> str:
+        return self.perturb_seed(text)
--- src/benchmark/augmentations/test_perturbation.py
+++ src/benchmark/augmentations/test_perturbation.py
@@ -3,7 +3,7 @@ from typing import List
 from benchmark.scenario import Instance, Reference
 
 from .data_augmenter import DataAugmenter
-from benchmark.augmentations.perturbation import IdentityPerturbation, ExtraSpacePerturbation
+from benchmark.augmentations.perturbation import IdentityPerturbation, ExtraSpacePerturbation, Misspellings
 
 
 def test_identity_perturbation():
@@ -26,3 +26,15 @@ def test_extra_space_perturbation():
     assert instances[0].perturbation.num_spaces == 2
     assert instances[0].input == "Hello  my  name  is"
     assert instances[0].references[0].output == "some  name"
+
+
+def test_misspelling_perturbation():
+    data_augmenter = DataAugmenter(perturbations=[Misspellings(prob=1.0)], should_perturb_references=True)
+    instance: Instance = Instance(
+        input="Already, the new product is not available.", references=[],
+    )
+    instances: List[Instance] = data_augmenter.generate([instance], include_original=True)
+
+    assert len(instances) == 2
+    assert instances[0].id == "id0"
+    assert instances[0].perturbation.name == "misspellings"
