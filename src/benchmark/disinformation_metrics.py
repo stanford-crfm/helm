ad084bdd8bb74aa4b2c032f843a62d3550c54f5e
Ben Newman <nlb9991@gmail.com>
2022-03-20 18:43:19 -0400

    Handle cases with one or fewer completions and add tests

--- src/benchmark/disinformation_metrics.py
+++ src/benchmark/disinformation_metrics.py
@@ -16,8 +16,13 @@ def _self_bleu(completions: List[Sequence], **unused_kwargs) -> float:
     """Self-BLEU.
 
     Average over all scores, where each score is the BLEU of one generation compared against all other generations.
+
+    If there is fewer than one completion, the self-bleu scoe is 0.
     """
-    completions = [completion.text.strip() for completion in completions]
+    completions = [completion.text.strip() for completion in completions if completion.text.strip()]
+
+    if len(completions) <= 1:
+        return 0
 
     scores = []
     for i in range(len(completions)):
@@ -34,6 +39,12 @@ def _monte_carlo_entropy(completions: List[Sequence], **unused_kwargs) -> float:
     """Monte Carlo estimate of model entropy in nats."""
     # TODO(lxuechen): This estimator is biased with non-unit temperature, since OpenAI API doesn't adjust logprob
     #  computation based on temperature.
+    completions = [completion for completion in completions if completion.tokens]
+
+    # bnewm0609: If there are no completions with tokens, there is no support for calcualting entropy, so return nan
+    if not completions:
+        return float("nan")
+
     mlogps = [-sum(token.logprob for token in completion.tokens) for completion in completions]
     return sum(mlogps) / len(mlogps)
 
@@ -54,3 +65,95 @@ class DisinformationMetric(Metric):
         request_result: RequestResult = request_state.result
         result = self._metric_fn(completions=request_result.completions, references=request_state.instance.references)
         return [Stat(self._name).add(result)]
+
+
+if __name__ == "__main__":
+    # Test metrics
+    from common.request import Token
+    import numpy as np
+
+    # Test tokens
+    test_1_tokens: List[Token] = [
+        Token("This", logprob=-0.25, top_logprobs={}),
+        Token("is", logprob=-0.25, top_logprobs={}),
+        Token("a", logprob=-0.25, top_logprobs={}),
+        Token("test", logprob=-0.25, top_logprobs={}),
+    ]
+    test_2_tokens: List[Token] = [
+        Token("This", logprob=-0.25, top_logprobs={}),
+        Token("is", logprob=-0.25, top_logprobs={}),
+        Token("another", logprob=-0.5, top_logprobs={}),
+        Token("test", logprob=-0.25, top_logprobs={}),
+    ]
+    test_empty_tokens: List[Token] = []
+    test_empty_str_tokens: List[Token] = [
+        Token("", logprob=0, top_logprobs={}),
+    ]
+
+    # Test Sequences (two standard, one with an empty token, and one with no tokens)
+    test_1 = Sequence(text="This is a test", logprob=-1, tokens=test_1_tokens)
+    test_2 = Sequence(text="This is another test", logprob=-1.25, tokens=test_2_tokens)
+    test_empty = Sequence(text="", logprob=-float("nan"), tokens=test_empty_tokens)
+    test_empty_str = Sequence(text="", logprob=0, tokens=test_empty_str_tokens)
+
+    # Test Self-BLEU
+    separator = "-" * 20 + "\n"
+
+    def run_test(label, inputs, pass_condition_lmbda, metric):
+        print(label)
+        print("Inputs", inputs)
+        score = metric(inputs)
+        print("Score", score)
+        pass_condition = pass_condition_lmbda(score)
+        assert pass_condition, "FAILED"
+        print("PASSED")
+        print(separator)
+
+    run_test(
+        "Self-BLEU with self", [test_1, test_1], lambda score: np.isclose(score, 100), _self_bleu,
+    )
+
+    run_test(
+        "Self-BLEU with other", [test_1, test_2], lambda score: 0 < score < 100, _self_bleu,
+    )
+
+    run_test(
+        "Self-BLEU with one sequence", [test_1], lambda score: score == 0, _self_bleu,
+    )
+
+    run_test(
+        "Self-BLEU with one full and one empty sequence",
+        [test_1, test_empty_str],
+        lambda score: score == 0,
+        _self_bleu,
+    )
+
+    # Test MC Entropy
+    run_test(
+        "MC Entropy with self",
+        [test_1, test_1],
+        lambda score: np.isclose(score, -test_1.logprob),
+        _monte_carlo_entropy,
+    )
+
+    run_test(
+        "MC Entropy with other",
+        [test_1, test_2],
+        lambda score: np.isclose(score, -(test_1.logprob + test_2.logprob) / 2),
+        _monte_carlo_entropy,
+    )
+
+    run_test(
+        "MC Entropy with one sequence", [test_1], lambda score: score == -test_1.logprob, _monte_carlo_entropy,
+    )
+
+    run_test(
+        "MC Entropy with sequence with one empty token",
+        [test_empty_str],
+        lambda score: score == test_empty_str.logprob,
+        _monte_carlo_entropy,
+    )
+
+    run_test(
+        "MC Entropy with sequence with no tokens", [test_empty], lambda score: np.isnan(score), _monte_carlo_entropy,
+    )
