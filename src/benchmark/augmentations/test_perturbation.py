b305f00976645711e31ad2b9810d46b65f1bbb2b
Huaxiu Yao <yhx662012@gmail.com>
2022-03-22 16:43:11 -0700

    use a seperate file for typos_perturbation.py

--- src/benchmark/augmentations/test_perturbation.py
+++ src/benchmark/augmentations/test_perturbation.py
@@ -5,6 +5,7 @@ from benchmark.scenario import Instance, Reference
 from .data_augmenter import DataAugmenter
 from .extra_space_perturbation import ExtraSpacePerturbation
 from .identity_perturbation import IdentityPerturbation
+from .typos_perturbation import TyposPerturbation
 
 
 def test_identity_perturbation():
@@ -29,3 +30,15 @@ def test_extra_space_perturbation():
     assert instances[0].perturbation.num_spaces == 2
     assert instances[0].input == "Hello  my  name  is"
     assert instances[0].references[0].output == "some  name"
+
+
+def test_typos_perturbation():
+    data_augmenter = DataAugmenter(perturbations=[TyposPerturbation(prob=0.1)], should_perturb_references=True)
+    instance: Instance = Instance(
+        input="After their marriage, she started a close collaboration with Karvelas.", references=[],
+    )
+    instances: List[Instance] = data_augmenter.generate([instance], include_original=True)
+
+    assert len(instances) == 2
+    assert instances[0].id == "id0"
+    assert instances[0].perturbation.name == "typos"
