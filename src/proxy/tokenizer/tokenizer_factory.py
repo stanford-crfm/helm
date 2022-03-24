19e7d5b75ab8d76615b7e92a7b353c84122c0d27
Deepak Narayanan <deepakn94@gmail.com>
2022-03-24 15:06:21 -0700

    Add estimated runtime as a metric to get clean-slate runtime (#166)
    * Also add training efficiency metrics
    * Use JSON file for inputs to cost model
    
    Co-authored-by: Tony Lee <tonyh.lee@yahoo.com>

--- src/proxy/tokenizer/tokenizer_factory.py
+++ src/proxy/tokenizer/tokenizer_factory.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from benchmark.tokenizer_service import TokenizerService
 from .ai21_tokenizer import AI21Tokenizer
 from .tokenizer import Tokenizer
@@ -6,8 +8,8 @@ from .openai_tokenizer import OpenAITokenizer
 
 class TokenizerFactory:
     @staticmethod
-    def get_tokenizer(model: str, service: TokenizerService) -> Tokenizer:
-        """Returns a `Tokenizer` given the model."""
+    def create_tokenizer(model: str, service: TokenizerService) -> Tokenizer:
+        """Creates a `Tokenizer` given the model."""
         organization: str = model.split("/")[0]
 
         tokenizer: Tokenizer
@@ -19,3 +21,16 @@ class TokenizerFactory:
             raise ValueError(f"Unsupported model: {model}")
 
         return tokenizer
+
+    def __init__(self):
+        self.tokenizers: Dict[str, Tokenizer] = {}
+
+    def get_tokenizer(self, model: str, service: TokenizerService) -> Tokenizer:
+        """Caches and returns a `Tokenizer` given the model."""
+        organization: str = model.split("/")[0]
+
+        if organization not in self.tokenizers:
+            # If the tokenizer for a specific organization is not found, create and cache it.
+            tokenizer: Tokenizer = TokenizerFactory.create_tokenizer(model, service)
+            self.tokenizers[organization] = tokenizer
+        return self.tokenizers[organization]
