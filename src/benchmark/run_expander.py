b6ab7f62a15edd859a242f66e415b1759b1c236a
Michael Xie <xie@cs.stanford.edu>
2022-04-09 11:20:20 -0700

    remove f string

--- src/benchmark/run_expander.py
+++ src/benchmark/run_expander.py
@@ -112,9 +112,9 @@ PERTURBATION_SPECS_DICT: Dict[str, Dict[str, List[PerturbationSpec]]] = {
     "misspelling": {"misspelling0.1": [misspelling(prob=0.1)]},
     "misspelling_sweep": {f"misspelling{prob}": [misspelling(prob=prob)]
         for prob in [0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]},
-    "misspelling_easy": {f"misspelling0.05": [misspelling(prob=0.05)]},
-    "misspelling_medium": {f"misspelling0.2": [misspelling(prob=0.2)]},
-    "misspelling_hard": {f"misspelling0.5": [misspelling(prob=0.5)]},
+    "misspelling_easy": {"misspelling0.05": [misspelling(prob=0.05)]},
+    "misspelling_medium": {"misspelling0.2": [misspelling(prob=0.2)]},
+    "misspelling_hard": {"misspelling0.5": [misspelling(prob=0.5)]},
     "all": {"all": [extra_space(num_spaces=2), misspelling(prob=0.1)]},
 }
 
