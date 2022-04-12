0873e0af429b3a79ab2fb850b78f61be67cbf6b1
Neel Guha <neelguha@gmail.com>
2022-04-11 22:36:12 -0700

    Fixed docstring.

--- src/benchmark/legal_support_scenario.py
+++ src/benchmark/legal_support_scenario.py
@@ -11,38 +11,28 @@ class LegalSupportScenario(Scenario):
     This dataset is the result of ongoing/yet-to-be-released work. For more questions
     on its construction, contact Neel Guha (nguha@stanford.edu).
 
-    The LegalSupport dataset tests a model's ability to determine which case summary best
-    supports a given legal passage. The task is structured as multiple choice questions.
-    There are two choices per question.
+    The LegalSupport dataset evaluates fine-grained reverse entailment. Each sample consists of a
+    text passage making a legal claim, and two case summaries. Each summary describes a legal conclusion
+    reached by a different court. The task is to determine which case (i.e. legal conclusion) most forcefully
+    and directly supports the legal claim in the passage. The construction of this benchmark leverages
+    annotations derived from a legal taxonomy expliciting different levels of entailment (e.g.
+    "directly supports" vs "indirectly supports"). As such, the benchmark tests a model's ability to reason
+    regarding the strength of support a particular case summary provides.
 
-    We prompt models using the following format:
-        Which statement best supports the passage?
-        <passage>
-        a <case summary>
-        b <case summary>
-        Answer:
-
-    Per convention:
-        - The string "Which statement best supports the passage?" is included as an Adaptor
-        instruction (see run_specs.py).
-        - The passage is prefixed with "Passage:",
-        - Each choice is represented as a Reference. We use lower case letters to enumerate
-        the choices.
+    The task is structured as multiple choice questions. There are two choices per question.
 
     Using an example from the test dataset, we have
-        Passage: Rather, we hold the uniform rule is ... that of 'good moral character". Courts have also endorsed
-        using federal, instead of state, standards to interpret federal laws regulating immigration.
 
-        a Interpreting "adultery” for the purpose of eligibility for voluntary departure,
-        and holding that "the appropriate approach is the application of a uniform federal standard."
+    Input: Rather, we hold the uniform rule is ... that of 'good moral character". Courts have also endorsed
+    using federal, instead of state, standards to interpret federal laws regulating immigration.
 
-        b Using state law to define "adultery” in the absence of a federal definition, and suggesting that
-        arguably, Congress intended to defer to the state in which an alien chooses to live for the precise
-        definition ... for it is that particular community which has the greatest interest in its residents moral
-        character.
+    Reference [CORRECT]: Interpreting "adultery” for the purpose of eligibility for voluntary departure,
+    and holding that "the appropriate approach is the application of a uniform federal standard."
 
-        Answer:
-            a
+    Reference: Using state law to define "adultery” in the absence of a federal definition, and suggesting that
+    arguably, Congress intended to defer to the state in which an alien chooses to live for the precise
+    definition ... for it is that particular community which has the greatest interest in its residents moral
+    character.
     """
 
     name = "legal_support"
