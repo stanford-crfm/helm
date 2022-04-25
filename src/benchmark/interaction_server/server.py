abc4fbe72b386f567743a37f574455df66e2598b
Ashwin Paranjape <ashwing.2005@gmail.com>
2022-04-24 23:28:16 -0700

    Adding backend support for loading finished dialogues and finished surveys

--- src/benchmark/interaction_server/server.py
+++ src/benchmark/interaction_server/server.py
@@ -13,39 +13,51 @@ import sys
 import argparse
 from flask import Flask, request
 
-sys.path = sys.path + ['../']
+sys.path = sys.path + ["../"]
 
 from common.general import ensure_directory_exists
-from dialogue_interface import start_conversation, conversational_turn, submit_interview
+import dialogue_interface
 
 app = Flask(__name__)
 
 file_globals: dict = {}
 
-@app.route("/api/dialogue/start", methods=['POST'])
+
+@app.route("/api/dialogue/start", methods=["POST"])
 def start_dialogue():
     args = request.json
     args["output_path"] = file_globals["output_path"]
     args["base_path"] = file_globals["base_path"]
-    response = start_conversation(args)
+    response = dialogue_interface.start_dialogue(args)
     return response
 
 
-@app.route("/api/dialogue/conversation", methods=['POST'])
+@app.route("/api/dialogue/conversation", methods=["POST"])
 def handle_utterance():
     args = request.json
     args["output_path"] = file_globals["output_path"]
     args["base_path"] = file_globals["base_path"]
-    response = conversational_turn(args)
+    response = dialogue_interface.dialogue_turn(args)
     return response
 
-@app.route("/api/dialogue/interview", methods=['POST'])
-def submit_dialogue():
+
+@app.route("/api/dialogue/end", methods=["POST"])
+def end_dialogue():
     args = request.json
     args["output_path"] = file_globals["output_path"]
     args["base_path"] = file_globals["base_path"]
-    response = submit_interview(args)
+    response = dialogue_interface.end_dialogue(args)
     return response
+
+
+@app.route("/api/dialogue/interview", methods=["POST"])
+def submit_interview():
+    args = request.json
+    args["output_path"] = file_globals["output_path"]
+    args["base_path"] = file_globals["base_path"]
+    response = dialogue_interface.submit_interview(args)
+    return response
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -61,9 +73,10 @@ def main():
     ensure_directory_exists(args.output_path)
     file_globals["output_path"] = args.output_path
     file_globals["base_path"] = args.base_path
-    
-   # app.static_url_path = "../../proxy/static/dialogue/#"
+
+    # app.static_url_path = "../../proxy/static/dialogue/#"
     app.run(host="127.0.0.1", port=args.port)
+
 
 if __name__ == "__main__":
     main()
