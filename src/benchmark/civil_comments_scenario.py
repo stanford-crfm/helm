72e13fb2f32cbb9b4356ef32017ce33ec39c81c5
Ryan <ryan.andrew.chi@gmail.com>
2022-05-05 09:30:35 -0700

    civil comments: use smaller dataset for subject=all

--- src/benchmark/civil_comments_scenario.py
+++ src/benchmark/civil_comments_scenario.py
@@ -1,10 +1,9 @@
 import random
 import numpy as np
 import pandas as pd
-import json
 import os
 from sklearn.model_selection import train_test_split
-from typing import List, Dict
+from typing import List
 
 from .scenario import (
     Scenario,
@@ -24,22 +23,15 @@ bool_to_response = {
 class CivilCommentsScenario(Scenario):
     """
     The Civil Comments dataset is from the following Hugging Face dataset:
-
         https://huggingface.co/datasets/civil_comments
-
     which, in turn, is from the following Kaggle contest:
-
         https://www.kaggle.com/c/jigsaw-unintended-bias-in-toxicity-classification/data
-
     Example prompt:
-
     PROMPT                                          TOXICITY    SEVERE_TOXICITY
     This is malfeasance by the Administrator        0           0
     and the Board. They are wasting our money!
     OBSCENE         THREAT      INSULT      IDENTITY_ATTACK     SEXUAL_EXPLICIT
     0               0           0           0                   0
-
-
     """
 
     name = "civil_comments"
@@ -53,43 +45,16 @@ class CivilCommentsScenario(Scenario):
 
     def get_instances(self) -> List[Instance]:
         random.seed(self.random_seed)
+
         data_path: str = "/u/scr/nlp/crfm/benchmarking/civil_comments/"
-        instances: List[Instance] = []
 
         if self.subject == "all":
-            file_path = os.path.join(data_path, "civil_comments.json")
-
+            file_path = os.path.join(data_path, "civil_comments_10pct.csv")
             assert os.path.exists(file_path)
-
-            loaded_lines: List[Dict] = []
-
-            with open(file_path, "r") as f:
-                for line in f:
-                    loaded_line = json.loads(line)
-                    loaded_lines.append(loaded_line)
-
-                random.seed(0)
-                random.shuffle(loaded_lines)
-
-                split = TRAIN_SPLIT
-                for index, loaded_line in enumerate(loaded_lines):
-                    x = loaded_line["comment_text"]
-                    y = loaded_line["severe_toxicity"]
-
-                    if index == int(len(loaded_lines) * 0.8):
-                        split = TEST_SPLIT
-                    instance = Instance(
-                        input=f"{x}\nQuestion: Is the passage above severely toxic?",
-                        references=[Reference(output=bool_to_response[y >= 0.5], tags=[CORRECT_TAG])],
-                        split=split,
-                    )
-                    instances.append(instance)
-
-                return instances
+            df = pd.read_csv(file_path)
 
         else:
             file_path = os.path.join(data_path, "civil_comments.csv")
-
             assert os.path.exists(file_path)
             df = pd.read_csv(file_path)
 
@@ -123,26 +88,27 @@ class CivilCommentsScenario(Scenario):
             df = df[df[self.subject].notnull()]
             df = df[df[self.subject] != 0]
 
-            # TODO: Only using labeled instances for now.
-            data = np.stack(df["comment_text"].tolist())
-            train_x, test_x, train_y, test_y = train_test_split(
-                data, df["severe_toxicity"], test_size=0.2, random_state=self.random_seed
-            )
+        # TODO: Only using labeled instances now.
+        data = np.stack(df["comment_text"].tolist())
+        train_x, test_x, train_y, test_y = train_test_split(
+            data, df["severe_toxicity"], test_size=0.2, random_state=self.random_seed
+        )
 
-            dataset_splits = {
-                TRAIN_SPLIT: (train_x, train_y),
-                TEST_SPLIT: (test_x, test_y),
-            }
+        dataset_splits = {
+            TRAIN_SPLIT: (train_x, train_y),
+            TEST_SPLIT: (test_x, test_y),
+        }
 
-            # Read all instances
-            for split, subset in dataset_splits.items():
-                data_x, data_y = subset
-                for (x, y) in zip(data_x, data_y):
-                    instance = Instance(
-                        input=f"{x}\nQuestion: Is the passage above severely toxic?",
-                        references=[Reference(output=bool_to_response[y >= 0.5], tags=[CORRECT_TAG])],
-                        split=split,
-                    )
-                    instances.append(instance)
+        # Read all instances
+        instances: List[Instance] = []
+        for split, subset in dataset_splits.items():
+            data_x, data_y = subset
+            for (x, y) in zip(data_x, data_y):
+                instance = Instance(
+                    input=f"{x}\nQuestion: Is the passage above severely toxic?",
+                    references=[Reference(output=bool_to_response[y >= 0.5], tags=[CORRECT_TAG])],
+                    split=split,
+                )
+                instances.append(instance)
 
-            return instances
+        return instances
