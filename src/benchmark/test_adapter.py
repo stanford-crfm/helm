f01c7992049ece718ef94d3f2c1697e215192d22
Yian Zhang <yian.zhang@nyu.edu>
2022-05-25 13:34:38 -0700

    Implement test_fits_tokens_within_context_window()

--- src/benchmark/test_adapter.py
+++ src/benchmark/test_adapter.py
@@ -135,3 +135,26 @@ def test_sample_examples_without_references():
 
     examples = adapter.sample_examples(all_train_instances, seed=0)
     assert len(examples) == 1
+
+
+def test_fits_tokens_within_context_window():
+    model: str = "openai/davinci"
+    adapter_spec = AdapterSpec(
+        method=ADAPT_LANGUAGE_MODELING, input_prefix="", model=model, output_prefix="", max_tokens=0,
+    )
+    adapter = Adapter(adapter_spec, get_test_adapter_service())
+    tokenizer = TokenizerFactory.get_tokenizer(model)
+
+    # The tokens translate to: '<|endoftext|>The the the the ... the the'
+    # There are 1 `conditioning_token` and 2049 `pred_tokens`. Since the `max_request_length`
+    # of GPT-3 is 2049, calling `fits_tokens_within_context_window` will remove the last `pred_token`
+    conditioning_tokens, pred_tokens = [50256], [464] + [262] * 2048
+    prompt, pred_tokens = adapter.fits_tokens_within_context_window(
+        conditioning_tokens, pred_tokens, tokenizer, tokenizer.max_request_length
+    )
+
+    # Ensure the prompt is correct
+    assert prompt == "<|endoftext|>The" + " the" * 2047
+
+    # Ensure the pred_tokens are correct
+    assert pred_tokens == [464] + [262] * 2047
