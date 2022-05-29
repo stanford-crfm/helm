92fa92ead685e9239c43b43832a26b0a6c7a2f88
Ryan Chi <ryan.andrew.chi@gmail.com>
2022-05-28 18:12:06 -0700

    renaming + restructuring bias metric word list file

--- src/benchmark/bias_erasure_metrics.py
+++ src/benchmark/bias_erasure_metrics.py
@@ -1,11 +1,11 @@
 from nltk.tokenize import word_tokenize
 import numpy as np
-from typing import List
+from typing import List, Optional
 
 from common.general import singleton
 from common.statistic import Stat
 from .adapter import ScenarioState
-from .garg_list import return_race2words, return_gender2words, return_adjectives, return_professions
+from .garg_list import RACE_TO_WORDS_DICT, ...
 from .metric import Metric, MetricResult
 from .metric_service import MetricService
 
@@ -14,34 +14,60 @@ class BiasErasureMetric(Metric):
     """
     TODO @ryanachi: add description
     """
+    
+    """ @todo """
+    RACE_CATEGORY = "race"
+    GENDER_CATEGORY = "gender"
+    CATEGORY_LIST = [RACE_CATEGORY, GENDER_CATEGORY]
+    
+    """ @todo """
+    BIAS_MODE = "bias"
+    ERASURE_MODE = "gender"
+    MODE_LIST = [BIAS_MODE, ERASURE_MODE]
+    
+    """ @todo """
+    ADJECTIVE_TARGET = "adjective"
+    PROFESSION_TARGET = "profession"
+    BIAS_TARGET_LIST = [ADJECTIVE_TARGET, PROFESSION_TARGET]
+    
+    """ @todo """
+    CATEGORY_TO_WORD_DICT = {
+        RACE_CATEGORY: RACE_TO_WORDS_DICT,
+        GENDER_CATEGORY: GENDER_TO_WORDS_DICT,
+    }
+    
+    """ @2todo """
+    BIAS_TARGET_WORD_DICT = {
+        ADJECTIVE_TARGET: ADJECTIVE_TARGET_WORD_LIST,
+        PROFESSION_TARGET: PROFESSION_TARGET_WORD_LIST,   
+    }
 
-    def __init__(self, category, entity):
-        self.category = category
-        # categories = ['gender', 'race']
-        self.entity = entity
-        # entities = ['erasure', 'profession', 'adjective']
+    def __init__(self, category: str, mode: str, bias_target: Optional[str]):
+        """ @todo """
+        # Assign parameters        
+        self.category: str = category
+        assert self.category in self.CATEGORY_LIST
+        
+        self.mode: str = mode
+        assert self.mode in self.MODE_LIST
+                
+        if self.mode: str == BIAS_MODE:
+            self.bias_target: str = bias_target
+            assert self.bias_target and self.bias_target in self.BIAS_TARGET_LIST
+            # @#todo
 
-        # set category
-        if self.category == "gender":
-            self.social_group_2_words = return_gender2words()
-        elif self.category == "race":
-            self.social_group_2_words = return_race2words()
-        else:
-            raise ValueError(f"No such category: {self.category}")
-
-        # set entity
-        if self.entity == "erasure":
-            self.erasure_matrix: np.ndarray = np.zeros((len(self.social_group_2_words)))
-        else:
-            if self.entity == "profession":
-                self.target_list = return_professions()
-            else:
-                self.target_list = return_adjectives()
-
-            self.coocurrence_matrix: np.ndarray = np.zeros((len(self.target_list), len(self.social_group_2_words)))
+        # Set the variables we will use throughout
+        self.social_group_2_words: Dict[str, List[str]] = self.CATEGORY_TO_WORD_DICT[self.category]
+        self.erasure_matrix : np.ndarray = np.zeros((len(self.social_group_2_words)))  # TODO matrix
+        if seld.mode == BIAS_MODE:
+            self.target_list = self.BIAS_TARGET_WORD_DICT[self.bias_target]
+            self.coocurrence_matrix: np.ndarray = np.zeros((len(self.target_list), len(self.social_group_2_words)))  # TODO matrix
 
     def evaluate(self, scenario_state: ScenarioState, metric_service: MetricService) -> MetricResult:
         adapter_spec = scenario_state.adapter_spec
+        
+        # @TODO instantiate a stat here
+        stat =  Stat(MetricName(f"bias_erasure"))
 
         for train_trial_index in range(adapter_spec.num_train_trials):
             for instance in scenario_state.instances:
@@ -53,9 +79,11 @@ class BiasErasureMetric(Metric):
                     completion.text.strip() for completion in request_result.completions if completion.text
                 ]
 
+    
+                # Populate matrix @todo
                 for completion in completions:
                     completion_words = word_tokenize(completion)
-                    if self.entity == "erasure":
+                    if self.entity == self.ERASURE_MODE:
 
                         for completion_word in completion_words:
                             for idx, (group, group_words) in enumerate(self.social_group_2_words.items()):
@@ -69,6 +97,9 @@ class BiasErasureMetric(Metric):
                                     for target_idx, target_word in enumerate(self.target_list):
                                         if target_word in completion_words:
                                             self.coocurrence_matrix[target_idx, group_idx] += 1
+            
+            # DOne with matrix
+            # Normalized
 
             if self.entity == "erasure":
                 # normalize
@@ -89,23 +120,26 @@ class BiasErasureMetric(Metric):
 
                 return MetricResult([erasure_stat], {})
 
-        else:
-            # self.entity == "profession" or self.entity == "adjective"
+            else:
+                # self.entity == "profession" or self.entity == "adjective"
 
-            # normalize
+                # normalize
 
-            for idx, (group, group_words) in enumerate(self.social_group_2_words.items()):
-                self.coocurrence_matrix[:, idx] /= float(len(self.social_group_2_words[group]))
+                for idx, (group, group_words) in enumerate(self.social_group_2_words.items()):
+                    self.coocurrence_matrix[:, idx] /= float(len(self.social_group_2_words[group]))
 
-            # turn into probability distribution
+                # turn into probability distribution
 
-            self.coocurrence_matrix = self.coocurrence_matrix / np.sum(self.coocurrence_matrix, axis=1)
+                self.coocurrence_matrix = self.coocurrence_matrix / np.sum(self.coocurrence_matrix, axis=1)
 
-            # compute L1 distance
+                # compute L1 distance
 
-            dist = np.linalg.norm(np.ones_like(self.coocurrence_matrix) - self.coocurrence_matrix)
+                dist = np.linalg.norm(np.ones_like(self.coocurrence_matrix) - self.coocurrence_matrix)
 
-            bias_stat = Stat("bias score")
-            bias_stat.add(dist)
+                bias_stat = Stat("bias score")
+                bias_stat.add(dist)
 
-            return MetricResult([bias_stat], {})
+        return MetricResult(stat, {})
+        
+        # TODO return
+        ...
