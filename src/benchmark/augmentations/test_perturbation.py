9f6d780471ca82c527b5373fe2ac05ae9d1201ea
Dilara Soylu <dilarasoylu@users.noreply.github.com>
2022-06-15 17:59:58 -0700

    Add gender pronoun perturbation test

--- src/benchmark/augmentations/test_perturbation.py
+++ src/benchmark/augmentations/test_perturbation.py
@@ -198,3 +198,28 @@ def test_person_name_perturbation():
                 0].input == "I learned that Jamel, Wardell, and Latoya are siblings! Do you know who is the oldest?"
     )
     assert instances[0].references[0].output == "Wardell"
+
+
+def test_gender_pronoun_perturbation():
+    data_augmenter = DataAugmenter(
+        perturbations=[
+            GenderPerturbation(prob=1.0, mode="pronouns", source_class="male",
+                               target_class="female")],
+        should_perturb_references=True,
+    )
+    instance: Instance = Instance(
+        id="id0",
+        input="Did she mention that he was coming with his parents?",
+        references=[Reference(output="She didn't, perhaps he didn't tell her!",
+                              tags=[])],
+    )
+    instances: List[Instance] = data_augmenter.generate([instance],
+                                                        include_original=True)
+
+    print(instances)
+    assert len(instances) == 2
+    assert instances[0].perturbation.name == "gender_term"
+    assert instances[
+               0].input == "Did she mention that she was coming with her parents?"
+    assert instances[0].references[
+               0].output == "She didn't, perhaps she didn't tell her!"
