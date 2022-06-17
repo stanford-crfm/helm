684a1d21f6be7d6f8e83e1a93ae112733c1a72f1
Dilara Soylu <dilarasoylu@users.noreply.github.com>
2022-06-16 19:18:33 -0700

    Add tags to the perturbation description

--- src/benchmark/augmentations/perturbation_description.py
+++ src/benchmark/augmentations/perturbation_description.py
@@ -1,4 +1,10 @@
 from dataclasses import dataclass
+from typing import List
+
+
+""" Perturbation tags """
+ROBUSTNESS_TAG: str = "robustness"
+FAIRNESS_TAG: str = "fairness"
 
 
 @dataclass(frozen=True)
@@ -7,3 +13,7 @@ class PerturbationDescription:
 
     # Name of the Perturbation
     name: str
+
+    # Extra metadata (e.g., ["robustness"] or ["fairness"])
+    # Used to group instances to compute metrics.
+    tags: List[str]
