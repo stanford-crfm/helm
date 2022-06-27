a9f90dad2f27624a36b63156800e1a3e156395cb
Tony Lee <tonyh.lee@yahoo.com>
2022-06-27 16:27:41 -0700

    cleanup script

--- scripts/benchmark-present-all-06272022.sh
+++ scripts/benchmark-present-all-06272022.sh
@@ -1,10 +1,14 @@
 : '
-Run RunSpecs in parallel by models using benchmark-present.
+Run RunSpecs in parallel by models using benchmark-present. To bypass the proxy server, append --local.
 
 Usage:
 
+  bash scripts/benchmark-present-all.sh <Any additional CLI arguments for benchmark-present>
+
+  e.g.,
+
   conda activate crfm_benchmarking
-  bash scripts/benchmark-present-all-06272022.sh --max-eval-instances 1000 --conf-path src/benchmark/presentation/run_specs.conf --num-threads 1 --priority 2 --local
+  bash scripts/benchmark-present-all-06272022.sh --max-eval-instances 1000 --num-threads 1 --priority 2 --local
 
 To kill a running process:
 
@@ -27,13 +31,20 @@ models=(
   "ai21/j1-grande"
   "ai21/j1-large"
   # Not enough OpenAI credits at the moment
-  # "openai/davinci openai/curie openai/babbage openai/ada"
-  # "openai/text-davinci-002 openai/text-davinci-001 openai/text-curie-001 openai/text-babbage-001 openai/text-ada-001"
-  # "openai/code-davinci-002 openai/code-davinci-001 openai/code-cushman-001"
-  # "openai/text-curie-001"
-  # "openai/text-davinci-002"
+  # To conserve OpenAI credits evaluate code-davinci-002 and text-davinci-002
+  # instead of code-davinci-001 and text-davinci-001
+  # "openai/davinci"
   # "openai/curie"
+  # "openai/babbage"
+  # "openai/ada"
+  # "openai/text-davinci-002"
+  # "openai/text-curie-001"
+  # "openai/text-babbage-001"
+  # "openai/text-ada-001"
+  # "openai/code-davinci-002"
+  # "openai/code-cushman-001"
   "gooseai/gpt-j-6b"
+  # "gooseai/gpt-neo-20b"
   "anthropic/stanford-online-all-v4-s3"
   "microsoft/TNLGv2_530B"
 )
