151d1f4b662fdc7e114e30bf9929b11f73568907
Dimitris Tsipras <dtsip0@gmail.com>
2022-08-12 12:27:23 -0700

    make checks rern cleanly

--- src/benchmark/augmentations/perturbation_description.py
+++ src/benchmark/augmentations/perturbation_description.py
@@ -12,7 +12,7 @@ class PerturbationDescription:
 
     # Fields to capture which types of Instances we are evaluating, to be populated during metric evaluation.
     # `includes_perturbed` means we are evaluating on pertubed instances, `includes_identity` means we are evaluating
-    # the unperturbed version of instances where this perturbation appplies, and if both are true, we are
+    # the unperturbed version of instances where this perturbation appplies, and, if both are true, we are
     # considering the minimum metric between the two.
     includes_perturbed: bool = True
     includes_identity: bool = False
