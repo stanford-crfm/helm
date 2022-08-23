b84bebe89fbbaba35294eaa0085d4c06e0c9c167
Tony Lee <tonyh.lee@yahoo.com>
2022-08-23 15:04:06 -0700

    meddialog

--- src/benchmark/__init__.py
+++ src/benchmark/__init__.py
@@ -45,6 +45,7 @@ from .scenarios import pubmed_qa_scenario  # noqa
 from .scenarios import med_qa_scenario  # noqa
 from .scenarios import med_mcqa_scenario  # noqa
 from .scenarios import med_paragraph_simplification_scenario  # noqa
+from .scenarios import med_dialog_scenario  # noqa
 from .scenarios import covid_dialog_scenario  # noqa
 from .scenarios import me_q_sum_scenario  # noqa
 from .scenarios import bioleaflets_scenario  # noqa
