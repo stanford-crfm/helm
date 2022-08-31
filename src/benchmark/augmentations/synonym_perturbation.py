cec416d37451d677e2b096d9a99e7ff853f23b0e
Dimitris Tsipras <dtsip0@gmail.com>
2022-08-30 21:37:30 -0700

    fix wordnet

--- src/benchmark/augmentations/synonym_perturbation.py
+++ src/benchmark/augmentations/synonym_perturbation.py
@@ -1,13 +1,16 @@
 from dataclasses import dataclass
+import json
 import os
+from pathlib import Path
 import random
+from typing import Dict, List
 
 import nltk
 from nltk.corpus import wordnet
 import spacy
 
 from benchmark.scenarios.scenario import Instance
-from common.general import match_case
+from common.general import match_case, ensure_file_downloaded
 from .perturbation_description import PerturbationDescription
 from .perturbation import Perturbation
 
@@ -34,6 +37,12 @@ class SynonymPerturbation(Perturbation):
 
     name: str = "synonym"
 
+    # For downloading wordnet_synonyms.json
+    CODALAB_URI_TEMPLATE: str = "https://worksheets.codalab.org/rest/bundles/{bundle}/contents/blob/"
+    CODALAB_BUNDLE: str = "0x92a07ef9f82941e6a75b2f3b27961e8c"
+    FILE_NAME: str = "wordnet_synonyms.json"
+    SOURCE_URI: str = CODALAB_URI_TEMPLATE.format(bundle=CODALAB_BUNDLE)
+
     def __init__(self, prob: float):
         # Assign parameters to instance variables
         self.prob: float = prob
@@ -49,15 +58,21 @@ class SynonymPerturbation(Perturbation):
             spacy.cli.download("en_core_web_sm")  # type: ignore
             self.spacy_model = spacy.load("en_core_web_sm")
 
+        output_dir = os.path.join("benchmark_output", "perturbations", self.name)
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
         try:
-            _ = wordnet.synsets("test")
+            _ = wordnet._morphy("test", "n")
         except LookupError:
-            out_dir = "nltk_data"
-            nltk.data.path.append(out_dir)
-            if not os.path.exists(os.path.join(out_dir, "corpora/wordnet")):
-                nltk.download("wordnet", download_dir=out_dir)
-            if not os.path.exists(os.path.join(out_dir, "corpora/omw-1.4")):
-                nltk.download("omw-1.4", download_dir=out_dir)
+            nltk.data.path.append(output_dir)
+            if not os.path.exists(os.path.join(output_dir, "corpora/wordnet")):
+                nltk.download("wordnet", download_dir=output_dir)
+            if not os.path.exists(os.path.join(output_dir, "corpora/omw-1.4")):
+                nltk.download("omw-1.4", download_dir=output_dir)
+
+        target_path = os.path.join(output_dir, self.FILE_NAME)
+        ensure_file_downloaded(source_url=self.SOURCE_URI, target_path=target_path)
+        with open(target_path) as f:
+            self.wordnet_synonyms: Dict[str, List[str]] = json.load(f)
 
     @property
     def description(self) -> PerturbationDescription:
@@ -78,14 +93,15 @@ class SynonymPerturbation(Perturbation):
         for token in doc:
             word = token.text
             wordnet_pos = spacy_to_wordnet_pos.get(token.pos_)
+            synonyms = []
             if wordnet_pos:
-                synsets = wordnet.synsets(word, pos=wordnet_pos)
-                synonyms = [lemma.name() for synset in synsets for lemma in synset.lemmas()]
-                synonyms = [s for s in synonyms if s != word.lower()]
-                synonyms = list(dict.fromkeys(synonyms))  # Make the list unique while preserving the order
-                if synonyms and self.random.uniform(0, 1) < self.prob:
+                for base in wordnet._morphy(word.lower(), wordnet_pos):  # _morphy returns the base form of a word
+                    synonyms.extend(self.wordnet_synonyms.get(f"{base}:{wordnet_pos}", []))
+            synonyms = [s for s in synonyms if s != word.lower()]
+            synonyms = list(dict.fromkeys(synonyms))  # Make the list unique while preserving the order
+            if synonyms:
+                if self.random.uniform(0, 1) < self.prob:
                     synonym = self.random.choice(synonyms)
-                    synonym = synonym.replace("_", " ")  # We might get synonyms such as "passive_voice"
                     word = match_case(word, synonym)
             perturbed_text += word + token.whitespace_
 
