8211632a713f7f26121e6232f5dd0ce0aeed7360
Tony Lee <tonyh.lee@yahoo.com>
2022-08-31 16:57:59 -0700

    added todo

--- src/benchmark/presentation/summarize.py
+++ src/benchmark/presentation/summarize.py
@@ -188,6 +188,7 @@ def get_unique_stat_by_matcher(stats: List[Stat], matcher: MetricNameMatcher) ->
     matching_stats = [stat for stat in stats if matcher.matches(stat.name)]
     if len(matching_stats) == 0:
         return None
+    # TODO: remove this check once we dedupe metrics
     if len(matching_stats) > 1:
         hlog(f"WARNING: There are more than one matching stat: {matching_stats}")
         return matching_stats[0]
