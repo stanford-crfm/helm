c3fa529cbc3fff78df9259095ba894c7c1ecdae0
Tony Lee <tonyh.lee@yahoo.com>
2022-08-31 23:04:53 -0700

    quick truncate fix for Cohere

--- src/benchmark/window_services/cohere_window_service.py
+++ src/benchmark/window_services/cohere_window_service.py
@@ -80,3 +80,18 @@ class CohereWindowService(LocalWindowService):
         Note this logic currently only works with English text.
         """
         return "".join([token.value for token in tokens])
+
+    def truncate_from_right(self, text: str, expected_completion_token_length: int = 0) -> str:
+        """
+        Truncates text from the right to fit within the context window given by `max_request_length`
+        minus the expected completion length (defaults to 0).
+        """
+        max_length: int = self.max_request_length - expected_completion_token_length
+        result: str = self.decode(self.encode(text, truncation=True, max_length=max_length).tokens)
+
+        # HACK: For the vast majority of cases, the above logic works, but it sometimes doesn't work
+        # for non-English text, since Cohere technically only supports English at the moment.
+        while not self.fits_within_context_window(result, expected_completion_token_length):
+            result = result[:-1]
+
+        return result
