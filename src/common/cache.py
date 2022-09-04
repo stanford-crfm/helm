9a0b2d834095e0c9339c6171bc6c0b170f388ce6
Tony Lee <tonyh.lee@yahoo.com>
2022-09-04 08:59:49 -0700

    retry write

--- src/common/cache.py
+++ src/common/cache.py
@@ -1,6 +1,10 @@
 import json
+import time
 from typing import Dict, Callable, Tuple
+
 from sqlitedict import SqliteDict
+
+from common.general import hlog
 
 
 def request_to_key(request: Dict) -> str:
@@ -20,6 +24,8 @@ class Cache(object):
     We use sqlitedict to persist the cache: https://github.com/RaRe-Technologies/sqlitedict.
     """
 
+    MAX_WRITE_ATTEMPTS: int = 5
+
     def __init__(self, cache_path: str):
         self.cache_path = cache_path
         # Counters to keep track of progress
@@ -36,9 +42,15 @@ class Cache(object):
             if response:
                 cached = True
             else:
-                self.num_misses += 1
-                # Commit the request and response to SQLite
-                cache[key] = response = compute()
-                cache.commit()
                 cached = False
+                self.num_misses += 1
+                # Compute and commit the request/response to SQLite
+                response = compute()
+                for attempt in range(Cache.MAX_WRITE_ATTEMPTS):
+                    try:
+                        cache[key] = response
+                        cache.commit()
+                    except Exception as e:
+                        hlog(f"Write attempt #{attempt+1}: {e}")
+                        time.sleep(1)
         return response, cached
