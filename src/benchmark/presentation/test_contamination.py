e17cba4f64503e27e16fe9ea4ff247643025a8ea
Percy Liang <percyliang@gmail.com>
2022-09-10 10:34:49 -0700

    fix test

--- src/benchmark/presentation/test_contamination.py
+++ src/benchmark/presentation/test_contamination.py
@@ -7,5 +7,5 @@ def test_contamination_schema():
     contamination = read_contamination()
     validate_contamination(contamination, schema)
 
-    assert contamination.get_point("not_exists", "openai/davinci") is None
-    assert contamination.get_point("boolq", "openai/davinci") is not None
+    assert contamination.get_point("openai/davinci", "boolq") is not None
+    assert contamination.get_point("openai/davinci", "not_exists") is None
