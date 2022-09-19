148cc04c31eee8f9b125c9cc4094816334fc74e0
Tony Lee <tonyh.lee@yahoo.com>
2022-09-19 16:25:30 -0700

    Give --max-eval-instances a default of 100

--- src/benchmark/run.py
+++ src/benchmark/run.py
@@ -86,7 +86,8 @@ def add_run_args(parser: argparse.ArgumentParser):
         "-m",
         "--max-eval-instances",
         type=int,
-        help="Maximum number of instances to evaluate on, overrides adapter spec (for piloting)",
+        default=100,
+        help="Maximum number of instances to evaluate on, overrides the value in Adapter spec. Default is 100.",
     )
     parser.add_argument(
         "--suite", type=str, help="Name of the suite this run belongs to (default is today's date).", required=True,
