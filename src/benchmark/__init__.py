3b26eee69bd75c956be8e67421839721ccc9e175
Amelia Hardy <ameliahardy@MacBook-Pro-38.local>
2022-09-24 21:03:11 -0700

    add moderation metrics to init

--- src/benchmark/__init__.py
+++ src/benchmark/__init__.py
@@ -55,6 +55,7 @@ from .metrics import multiple_request_metrics  # noqa
 from .metrics import summarization_metrics  # noqa
 from .metrics import toxicity_metrics  # noqa
 from .metrics import tokens_metric  # noqa
+from .metrics import moderation_metrics # noqa
 
 # Perturbations for data augmentation
 from .augmentations.extra_space_perturbation import ExtraSpacePerturbation  # noqa
