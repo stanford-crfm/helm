2ce22d472eaee38767f5614537a25c9aa2ecfd52
Tony Lee <tonyh.lee@yahoo.com>
2022-10-28 14:34:18 -0700

    copy to helm

--- scripts/create-www.sh
+++ scripts/create-www.sh
@@ -22,6 +22,6 @@ ln -sf $suite www/benchmark_output/runs/latest || exit 1
 # Set permissions
 chmod -R og=u-w www || exit 1
 
-# Push to server (https://nlp.stanford.edu/helm/benchmraking)
+# Push to server (https://nlp.stanford.edu/helm/benchmarking)
 echo Pushing to server...
 rsync -arvz www/* scdt:/u/apache/htdocs/helm/benchmarking
