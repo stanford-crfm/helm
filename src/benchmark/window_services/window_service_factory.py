2fca87c549a17bb5f2fefe8678c3b9cd6684de61
Yifan Mai <yifan@cs.stanford.edu>
2022-11-10 11:49:39 -0800

    Refactor built in window service registration

--- src/benchmark/window_services/window_service_factory.py
+++ src/benchmark/window_services/window_service_factory.py
@@ -1,6 +1,6 @@
 from typing import Dict, Callable
 
-from proxy.models import get_model, Model, WIDER_CONTEXT_WINDOW_TAG
+from proxy.models import ALL_MODELS, WIDER_CONTEXT_WINDOW_TAG
 from .ai21_window_service import AI21WindowService
 from .anthropic_window_service import AnthropicWindowService
 from .cohere_window_service import CohereWindowService
@@ -32,26 +32,52 @@ def register_window_service_for_model(model_name: str, factory: WindowServiceFac
     _window_service_factory_registry[model_name] = factory
 
 
-# Register built-in window service factories
-register_window_service_for_model("huggingface/gpt2", GPT2WindowService)
-register_window_service_for_model("together/bloom", BloomWindowService)
-register_window_service_for_model(
-    "together/glm",
-    # From https://github.com/THUDM/GLM-130B, "the tokenizer is implemented based on
-    # icetk---a unified multimodal tokenizer for images, Chinese, and English."
-    ICEWindowService,
-)
-register_window_service_for_model("huggingface/gpt-j-6b", GPTJWindowService)
-register_window_service_for_model("together/gpt-j-6b", GPTJWindowService)
-register_window_service_for_model("gooseai/gpt-j-6b", GPTJWindowService)
-register_window_service_for_model("together/gpt-neox-20b", GPTNeoXWindowService)
-register_window_service_for_model("gooseai/gpt-neo-20b", GPTNeoXWindowService)
-register_window_service_for_model("together/opt-66b", OPTWindowService)
-register_window_service_for_model("together/opt-175b", OPTWindowService)
-register_window_service_for_model("together/t0pp", T0ppWindowService)
-register_window_service_for_model("together/t5-11b", T511bWindowService)
-register_window_service_for_model("together/ul2", UL2WindowService)
-register_window_service_for_model("together/yalm", YaLMWindowService)
+def _register_build_in_window_services():
+    for model in ALL_MODELS:
+        model_name: str = model.name
+        organization: str = model.organization
+        if WIDER_CONTEXT_WINDOW_TAG in model.tags:
+            register_window_service_for_model(model_name, WiderOpenAIWindowService)
+        elif organization == "openai" or organization == "simple":
+            register_window_service_for_model(model_name, OpenAIWindowService)
+        elif organization == "microsoft":
+            register_window_service_for_model(model_name, MTNLGWindowService)
+        elif organization == "anthropic":
+            register_window_service_for_model(model_name, AnthropicWindowService)
+        elif model_name == "huggingface/gpt2":
+            register_window_service_for_model(model_name, GPT2WindowService)
+        elif model_name == "together/bloom":
+            register_window_service_for_model(model_name, BloomWindowService)
+        elif model_name == "together/glm":
+            # From https://github.com/THUDM/GLM-130B, "the tokenizer is implemented based on
+            # icetk---a unified multimodal tokenizer for images, Chinese, and English."
+            register_window_service_for_model(model_name, ICEWindowService)
+        elif model_name in ["huggingface/gpt-j-6b", "together/gpt-j-6b", "gooseai/gpt-j-6b"]:
+            register_window_service_for_model(model_name, GPTJWindowService)
+        elif model_name in ["together/gpt-neox-20b", "gooseai/gpt-neo-20b"]:
+            register_window_service_for_model(model_name, GPTNeoXWindowService)
+        elif model_name in ["together/opt-66b", "together/opt-175b"]:
+            register_window_service_for_model(model_name, OPTWindowService)
+        elif model_name == "together/t0pp":
+            register_window_service_for_model(model_name, T0ppWindowService)
+        elif model_name == "together/t5-11b":
+            register_window_service_for_model(model_name, T511bWindowService)
+        elif model_name == "together/ul2":
+            register_window_service_for_model(model_name, UL2WindowService)
+        elif model_name == "together/yalm":
+            register_window_service_for_model(model_name, YaLMWindowService)
+        elif organization == "cohere":
+            register_window_service_for_model(model_name, CohereWindowService)
+        elif organization == "ai21":
+            register_window_service_for_model(
+                model_name,
+                lambda service: AI21WindowService(service=service, gpt2_window_service=GPT2WindowService(service)),
+            )
+        else:
+            raise ValueError(f"Unhandled model name: {model_name}")
+
+
+_register_build_in_window_services()
 
 
 class WindowServiceFactory:
@@ -61,27 +87,10 @@ class WindowServiceFactory:
         Returns a `WindowService` given the name of the model.
         Make sure this function returns instantaneously on repeated calls.
         """
-        wondow_service_factory_for_model = _window_service_factory_registry.get(model_name)
-        if wondow_service_factory_for_model is not None:
-            return wondow_service_factory_for_model(service)
-
-        model: Model = get_model(model_name)
-        organization: str = model.organization
-
-        window_service: WindowService
-        if WIDER_CONTEXT_WINDOW_TAG in model.tags:
-            window_service = WiderOpenAIWindowService(service)
-        elif organization == "openai" or organization == "simple":
-            window_service = OpenAIWindowService(service)
-        elif organization == "microsoft":
-            window_service = MTNLGWindowService(service)
-        elif organization == "anthropic":
-            window_service = AnthropicWindowService(service)
-        elif organization == "cohere":
-            window_service = CohereWindowService(service)
-        elif organization == "ai21":
-            window_service = AI21WindowService(service=service, gpt2_window_service=GPT2WindowService(service))
-        else:
-            raise ValueError(f"Unhandled model name: {model_name}")
-
-        return window_service
+        window_service_factory = _window_service_factory_registry.get(model_name)
+        if window_service_factory is None:
+            raise ValueError(
+                f"No registered window service found for model: {model_name} - "
+                "register a window service using register_window_service_for_model()"
+            )
+        return window_service_factory(service)
