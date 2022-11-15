578cd6c49bf889828f18d5e08e6eb3cb06c8581f
Dimitris Tsipras <dtsip0@gmail.com>
2022-11-14 23:59:21 -0800

    comment

--- src/benchmark/presentation/summarize.py
+++ src/benchmark/presentation/summarize.py
@@ -646,6 +646,8 @@ class Summarizer:
             assert len(group_names) == len(matchers)
             for group_name, matcher in zip(group_names, matchers):
                 group_runs = [run for run in runs if group_name in run.run_spec.groups]
+                # HACK: when looking at aggregate bAbi results (e.g., reasoning), we want to see only the `task: all`
+                # version and not the default aggregation across a sparse set of tasks, e.g., `task: {all, 3, 15, 19}`
                 if "babi" in group_name and "task:" not in name:
                     group_runs = [run for run in group_runs if "task=all" in run.run_spec.name]
                 point = self.contamination.get_point(model.name, group_name)
