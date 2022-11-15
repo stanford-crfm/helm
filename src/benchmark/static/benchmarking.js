e681a92dbd4ed9e9d2556592a1ca8f26784a45d5
Yifan Mai <yifan@cs.stanford.edu>
2022-11-15 11:46:20 -0800

    Fix bugs in table cell HTML escaping

--- src/benchmark/static/benchmarking.js
+++ src/benchmark/static/benchmarking.js
@@ -919,14 +919,17 @@ $(function () {
     if (typeof(value) === 'number') {
       value = Math.round(value * 1000) / 1000;
     }
+    const $value = $('<span>');
     if (cell.markdown && value) {
       value = renderMarkdown('' + value);
+      $value.append(value);
+    } else {
+      $value.text(value);
     }
-    const $value = $('<span>').text(value);
     if (cell.style) {
       $value.css(cell.style);
     }
-    if (!cell.description) {
+    if (cell.description) {
       $value.attr("title", cell.description)
     }
     const $linkedValue = cell.href ? $('<a>', {href: cell.href}).append($value) : $value;
