639580d79c7201ead1a426b57ae937aeecbf2f3e
Tony Lee <tonyh.lee@yahoo.com>
2022-12-30 09:55:30 -0800

    fixed typo

--- src/helm/benchmark/presentation/run_display.py
+++ src/helm/benchmark/presentation/run_display.py
@@ -8,7 +8,7 @@ import dacite
 
 from helm.benchmark.adapter import (
     AdapterSpec,
-    ADAPT_MUPLTIPLE_CHOICE_METHODS,
+    ADAPT_MULTIPLE_CHOICE_METHODS,
     ADAPT_MULTIPLE_CHOICE_SEPARATE_CALIBRATED,
 )
 from helm.benchmark.augmentations.dialect_perturbation import DialectPerturbation
@@ -129,7 +129,7 @@ def _read_per_instance_stats(run_path: str) -> List[Dict]:
 def _truncate_predicted_text(predicted_text: str, request_state: Dict, adapter_spec: AdapterSpec) -> Optional[str]:
     method = adapter_spec.method
     prefix = ""
-    if method in ADAPT_MUPLTIPLE_CHOICE_METHODS:
+    if method in ADAPT_MULTIPLE_CHOICE_METHODS:
         prefix = request_state["instance"]["input"]
     elif method == "language_modeling":
         if "result" in request_state and "completions" in request_state["result"]:
@@ -196,7 +196,7 @@ def write_run_display_json(run_path: str, run_spec: RunSpec, schema: Schema):
 
     metric_names = _get_metric_names_for_groups(run_spec.groups, schema)
 
-    if run_spec.adapter_spec.method in ADAPT_MUPLTIPLE_CHOICE_METHODS:
+    if run_spec.adapter_spec.method in ADAPT_MULTIPLE_CHOICE_METHODS:
         metric_names.add("predicted_index")
 
     stats_by_trial: Dict[Tuple[str, Optional[PerturbationDescription], int], Dict[str, float]] = defaultdict(dict)
@@ -247,7 +247,7 @@ def write_run_display_json(run_path: str, run_spec: RunSpec, schema: Schema):
         # For the multiple_choice_separate_* adapter methods,
         # only keep the prediction for the chosen reference and discard the rest.
         if (
-            run_spec.adapter_spec.method in ADAPT_MUPLTIPLE_CHOICE_METHODS
+            run_spec.adapter_spec.method in ADAPT_MULTIPLE_CHOICE_METHODS
             and "predicted_index" in trial_stats
             and trial_stats["predicted_index"] != request_state["reference_index"]
         ):
