edb70aafe4b8a08bc93783beacc87d76db095735
Tony Lee <tonyh.lee@yahoo.com>
2023-01-15 17:12:53 -0800

    truncate hack

--- src/helm/benchmark/window_services/clip_window_service.py
+++ src/helm/benchmark/window_services/clip_window_service.py
@@ -31,3 +31,13 @@ class CLIPWindowService(LocalWindowService, ABC):
     @property
     def tokenizer_name(self) -> str:
         return "openai/clip-vit-large-patch14"
+
+    def truncate_from_right(self, text: str, expected_completion_token_length: int = 0) -> str:
+        result: str = self.decode(self.encode(text, truncation=True, max_length=self.max_request_length).tokens)
+
+        # HACK: For the vast majority of cases, the above logic works, but there are a few where the
+        # token count exceeds `max_length` by 1.
+        while not self.fits_within_context_window(result):
+            result = result[:-1]
+
+        return result
