592c298f3394952cce79cba4fffb5f4cb8a06d58
Yifan Mai <yifan@cs.stanford.edu>
2023-02-16 11:15:11 -0800

    Documentation fixes

--- src/helm/benchmark/metrics/classification_metrics.py
+++ src/helm/benchmark/metrics/classification_metrics.py
@@ -1,4 +1,4 @@
-from typing import List, Set
+from typing import List
 
 from sklearn.metrics import f1_score
 
@@ -9,8 +9,27 @@ from helm.benchmark.metrics.statistic import Stat
 
 
 class ClassificationMetric(Metric):
+    """Defines metrics for multi-class classification using the generation adapter.
+
+    Currently provides `classification_macro_f1` and `classification_micro_f1`.
+    These are population-level F1 measures to measure classification performance where each
+    generation is a predicted class, and are different from the instance-level F1 measures
+    in `BasicMetrics` that are intended to measure word overlap between the correct references
+    and generations. The correct class should be provided by the normalized text of a correct
+    reference. The predicted class for each instance is the normalized text of the generation.
+
+    Note:
+    - The set of classes is derived from the correct references from all the instances.
+      This means that classes may be omitted if they never are never used as a correct
+      reference.
+    - Generations that are not in any of the known classes are counted as a
+      negative prediction for every class.
+    - Perturbed classes are considered different classes from unperturbed
+      classes.
+    - Currently, multi-label classification is not supported.
+    """
+
     def evaluate_instances(self, request_states: List[RequestState]) -> List[Stat]:
-        normalized_labels: Set[str] = set()
         y_pred: List[str] = []
         y_true: List[str] = []
         for request_state in request_states:
@@ -26,7 +45,6 @@ class ClassificationMetric(Metric):
 
             num_correct = 0
             for reference in request_state.instance.references:
-                normalized_labels.add(normalize_text(reference.output.text))
                 if reference.is_correct:
                     num_correct += 1
                     y_true.append(normalize_text(reference.output.text))
@@ -36,12 +54,12 @@ class ClassificationMetric(Metric):
             if request_state.output_mapping:
                 raise ValueError("ClassificationMetric does not support multiple choice adapters")
             y_pred.append(normalize_text(request_state.result.completions[0].text))
-        # TODO: Support F1 on perturbation slices.
+        labels = list(set(y_true))
         return [
             Stat(MetricName("classification_macro_f1")).add(
-                f1_score(y_pred=y_pred, y_true=y_true, labels=list(normalized_labels), average="macro")
+                f1_score(y_pred=y_pred, y_true=y_true, labels=list(labels), average="macro")
             ),
             Stat(MetricName("classification_micro_f1")).add(
-                f1_score(y_pred=y_pred, y_true=y_true, labels=list(normalized_labels), average="micro")
+                f1_score(y_pred=y_pred, y_true=y_true, labels=list(labels), average="micro")
             ),
         ]
