b2b189e10397b4f55bf3ac34dbcbcf8766f05e6b
Tony Lee <tonyh.lee@yahoo.com>
2023-02-26 14:49:43 -0800

    skip computing aesthetics score for blacked out images

--- src/helm/benchmark/metrics/images/aesthetics_metrics.py
+++ src/helm/benchmark/metrics/images/aesthetics_metrics.py
@@ -1,6 +1,7 @@
 from statistics import mean
 from typing import List, Optional
 
+from helm.common.images_utils import is_blacked_out_image
 from helm.common.request import RequestResult
 from helm.benchmark.adaptation.request_state import RequestState
 from helm.benchmark.adaptation.adapter_spec import AdapterSpec
@@ -35,9 +36,11 @@ class AestheticsMetric(Metric):
         if self._aesthetics_scorer is None:
             self._aesthetics_scorer = AestheticsScorer()
 
-        # Compute the aesthetics score for each generated image
+        # Compute the aesthetics score for each generated image. Skip blacked out images.
         scores: List[float] = [
-            self._aesthetics_scorer.compute_aesthetics_score(location) for location in image_locations
+            self._aesthetics_scorer.compute_aesthetics_score(location)
+            for location in image_locations
+            if not is_blacked_out_image(location)
         ]
         stats: List[Stat] = [
             Stat(MetricName("expected_aesthetics_score")).add(mean(scores) if len(scores) > 0 else 0),
