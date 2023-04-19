449b0ee928dc25a72931370731e82e336fb838bb
Yifan Mai <yifan@cs.stanford.edu>
2023-04-19 11:56:22 -0700

    Rename parameter

--- src/helm/benchmark/scenarios/imdb_scenario_pinned_file_order.py
+++ src/helm/benchmark/scenarios/imdb_scenario_pinned_file_order.py
@@ -15,14 +15,14 @@ _split_to_class_to_pinned_file_order: Dict[str, List[str]] = {}
 """Nested mapping of split to class to to pinned file order."""
 
 
-def get_split_to_class_to_pinned_file_order(target_path: str):
+def get_split_to_class_to_pinned_file_order(download_dir: str):
     """Lazily download and return a nested mapping of split to class to pinned file order."""
     global _split_to_class_to_pinned_file_order
     if not _split_to_class_to_pinned_file_order:
-        file_path: str = os.path.join(target_path, _PINNED_FILE_ORDER_FILENAME)
+        file_path: str = os.path.join(download_dir, _PINNED_FILE_ORDER_FILENAME)
         ensure_file_downloaded(
             source_url=_PINNED_FILE_ORDER_URL,
-            target_path=file_path,
+            download_dir=file_path,
             unpack=False,
         )
         with open(file_path) as f:
@@ -30,11 +30,11 @@ def get_split_to_class_to_pinned_file_order(target_path: str):
     return _split_to_class_to_pinned_file_order
 
 
-def listdir_with_pinned_file_order(target_path: str, split: str, class_name: str) -> List[str]:
+def listdir_with_pinned_file_order(download_dir: str, split: str, class_name: str) -> List[str]:
     """List files for the split in a pinned order for IMDB to ensure reproducibility.
 
     Unfortunately, the previous official HELM runs used the arbitrary file order
     produced by os.listdir() and did not sort the files, so future runs must use
     the same file order in order to sample the same instances to reproduce
     the official HELM runs."""
-    return get_split_to_class_to_pinned_file_order(target_path)[split][class_name]
+    return get_split_to_class_to_pinned_file_order(download_dir)[split][class_name]
