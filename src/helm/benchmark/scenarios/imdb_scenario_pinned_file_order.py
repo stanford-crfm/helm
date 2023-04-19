bac9eb6df799eb4622f67d04d0dccb0dbc6a3d94
Yifan Mai <yifan@cs.stanford.edu>
2023-04-19 11:17:17 -0700

    Update docs

--- src/helm/benchmark/scenarios/imdb_scenario_pinned_file_order.py
+++ src/helm/benchmark/scenarios/imdb_scenario_pinned_file_order.py
@@ -8,17 +8,18 @@ from helm.common.general import ensure_file_downloaded
 _PINNED_FILE_ORDER_URL: str = (
     "https://worksheets.codalab.org/rest/bundles/0xa75fa6eb8dc24e23a73d3a5ecb674b32/contents/blob/"
 )
-_PINNED_FILE_NAME: str = "imdb_scenario_pinned_file_order.json"
+_PINNED_FILE_ORDER_FILENAME: str = "imdb_scenario_pinned_file_order.json"
 
 
 _split_to_class_to_pinned_file_order: Dict[str, List[str]] = {}
+"""Nested mapping of split to class to to pinned file order."""
 
 
-def _get_split_to_class_to_pinned_file_order(target_path: str):
-    """Lazily download the pinned file order."""
+def get_split_to_class_to_pinned_file_order(target_path: str):
+    """Lazily download and return a nested mapping of split to class to pinned file order."""
     global _split_to_class_to_pinned_file_order
     if not _split_to_class_to_pinned_file_order:
-        file_path: str = os.path.join(target_path, _PINNED_FILE_NAME)
+        file_path: str = os.path.join(target_path, _PINNED_FILE_ORDER_FILENAME)
         ensure_file_downloaded(
             source_url=_PINNED_FILE_ORDER_URL,
             target_path=file_path,
@@ -36,4 +37,4 @@ def listdir_with_pinned_file_order(target_path: str, split: str, class_name: str
     produced by os.listdir() and did not sort the files, so future runs must use
     the same file order in order to sample the same instances to reproduce
     the official HELM runs."""
-    return _get_split_to_class_to_pinned_file_order(target_path)[split][class_name]
+    return get_split_to_class_to_pinned_file_order(target_path)[split][class_name]
