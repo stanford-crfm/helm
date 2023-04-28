5fc1b5da7bb520611dd4b6942c35cc709e5d4a73
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-04-27 19:35:41 -0700

    Update more tests and handle the ▁ character

--- src/helm/proxy/clients/yalm_tokenizer/test_yalm_tokenizer.py
+++ src/helm/proxy/clients/yalm_tokenizer/test_yalm_tokenizer.py
@@ -12,7 +12,7 @@ class TestYaLMTokenizer:
         text: str = "hello world!"  # Has 3 tokens
         token_ids: List[int] = self.tokenizer.tokenize(text)
         assert token_ids == [49524, 2175, 127679]
-        assert self.tokenizer.convert_ids_to_tokens(token_ids) == ["▁hello", "▁world", "!"]
+        assert self.tokenizer.convert_ids_to_tokens(token_ids) == ["hello", "world", "!"]
 
     def test_decode(self):
         text: str = "should be exactly the same"
