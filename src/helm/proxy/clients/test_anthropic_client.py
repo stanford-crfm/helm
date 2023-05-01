96ccc052d29f3d4d9f25695b5f03f2f8f1ccf729
Yifan Mai <yifan@cs.stanford.edu>
2023-05-01 16:24:04 -0700

    Fix test

--- src/helm/proxy/clients/test_anthropic_client.py
+++ src/helm/proxy/clients/test_anthropic_client.py
@@ -15,7 +15,7 @@ from .anthropic_client import AnthropicClient
 class TestAnthropicClient:
     TEST_PROMPT: str = "I am a computer scientist."
     TEST_ENCODED: List[int] = [45, 1413, 269, 6797, 22228, 18]
-    TEST_TOKENS: List[str] = ["I", "Ġam", "Ġa", "Ġcomputer", "Ġscientist", "."]
+    TEST_TOKENS: List[str] = ["I", " am", " a", " computer", " scientist", "."]
 
     def setup_method(self, method):
         cache_file = tempfile.NamedTemporaryFile(delete=False)
@@ -56,12 +56,3 @@ class TestAnthropicClient:
         result: DecodeRequestResult = self.client.decode(request)
         assert result.cached, "Result should be cached"
         assert result.text == self.TEST_PROMPT
-
-        # Checks that decode also works with a list of strings
-        request_str = DecodeRequest(tokens=self.TEST_TOKENS)
-        result_str: DecodeRequestResult = self.client.decode(request_str)
-        assert not result_str.cached, "First time making the decode request str. Result should not be cached"
-        assert result_str.text == self.TEST_PROMPT
-        result_str: DecodeRequestResult = self.client.decode(request_str)
-        assert result_str.cached, "Result should be cached"
-        assert result_str.text == self.TEST_PROMPT
