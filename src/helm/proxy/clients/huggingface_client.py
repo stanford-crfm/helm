74cbfebd38822ce666e9c9e5ad8c26fb661f0c96
julian-q <julianhquevedo@gmail.com>
2023-05-08 17:05:38 -0700

    add error message for assert

--- src/helm/proxy/clients/huggingface_client.py
+++ src/helm/proxy/clients/huggingface_client.py
@@ -47,10 +47,8 @@ class HuggingFaceServer:
         del raw_request["top_k_per_token"]
         if len(raw_request["stop_sequences"]) > 0:
             stop_sequence_ids = self.tokenizer(raw_request["stop_sequences"], return_token_type_ids=False)
-            # Total number of stop words should be 1.
-            assert len(stop_sequence_ids.input_ids) == 1
-            # Total number of tokens in each stop word should be 1.
-            assert len(stop_sequence_ids.input_ids[0]) == 1
+            assert len(stop_sequence_ids.input_ids) == 1, "Total number of stop words should be 1."
+            assert len(stop_sequence_ids.input_ids[0]) == 1, "Total number of tokens in each stop word should be 1."
             del raw_request["stop_sequences"]
             raw_request["eos_token_id"] = stop_sequence_ids.input_ids[0][0]
 
