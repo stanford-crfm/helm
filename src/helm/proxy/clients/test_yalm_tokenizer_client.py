decbc1e4e3af453a4590d76a489dbe870d09d1cc
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-05-10 15:05:51 -0700

    Fix yalm tokenizer and tests

--- src/helm/proxy/clients/test_yalm_tokenizer_client.py
+++ src/helm/proxy/clients/test_yalm_tokenizer_client.py
@@ -30,7 +30,7 @@ class TestYaLMTokenizerClient:
         assert not result.cached, "First time making the tokenize request. Result should not be cached"
         result: TokenizationRequestResult = self.client.tokenize(request)
         assert result.cached, "Result should be cached"
-        assert result.raw_tokens == ["▁The", "▁model", "▁lever", "ages", "▁100", "▁billion", "▁parameters", "."]
+        assert result.raw_tokens == [" The", " model", " lever", "ages", " 100", " billion", " parameters", "."]
 
     def test_encode(self):
         request = TokenizationRequest(text=self.test_prompt, encode=True)
--- src/helm/proxy/clients/yalm_tokenizer_client.py
+++ src/helm/proxy/clients/yalm_tokenizer_client.py
@@ -35,9 +35,11 @@ class YaLMTokenizerClient(Client):
                 token_ids = self.tokenizer.tokenize(request.text)
                 if request.truncation:
                     token_ids = token_ids[: request.max_length]
-                return {"tokens": token_ids if request.encode else self.tokenizer.convert_ids_to_tokens(token_ids)}
-                # TODO(1522): Check if we can reenable this to remove "▁"
-                # return {"tokens": token_ids if request.encode else self.tokenizer.convert_ids_to_string(token_ids)}
+                # We do not use: return {"tokens": token_ids if request.encode else self.tokenizer.convert_ids_to_string(token_ids)}
+                # as this replace "▁" with an empty string, which is not what we want.
+                # This is a problem because then tokenize(" Hello", encode=False) == tokenize("Hello", encode=False)
+                # That is why we manually replace "▁" with a space.
+                return {"tokens": token_ids if request.encode else [token.replace("▁", ' ') for token in self.tokenizer.convert_ids_to_tokens(token_ids)]}
 
             result, cached = self.cache.get(cache_key, wrap_request_time(do_it))
         except Exception as e:
