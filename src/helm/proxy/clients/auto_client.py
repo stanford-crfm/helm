eae1e6e727da8807f3db4de536a47db2fec1317c
Yifan Mai <yifan@cs.stanford.edu>
2023-05-16 11:26:09 -0700

    More minor fixes

--- src/helm/proxy/clients/auto_client.py
+++ src/helm/proxy/clients/auto_client.py
@@ -182,7 +182,7 @@ class AutoClient(Client):
                 "huggingface",
                 "microsoft",
                 "Writer",
-                "tatsu-lab",
+                "hf-internal-testing",
             ]:
                 client = HuggingFaceClient(cache_config=cache_config)
             elif organization == "openai":
