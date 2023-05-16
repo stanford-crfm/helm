6044b16d66611a74cd4abbc84a965e9b14c8ae78
Yifan Mai <yifan@cs.stanford.edu>
2023-05-16 10:20:06 -0700

    Moar window service fixes

--- src/helm/benchmark/window_services/llama_window_service.py
+++ src/helm/benchmark/window_services/llama_window_service.py
@@ -5,11 +5,9 @@ from helm.benchmark.window_services.tokenizer_service import TokenizerService
 
 class LlamaWindowService(HuggingFaceWindowService):
     def __init__(self, service: TokenizerService):
-        # Loading this Hugging FAce tokenizer takes 2 minutes, mostly due to convert_slow_tokenizer.
-        # TODO: Make this faster.
-        model_config = HuggingFaceModelConfig(namespace="tatsu-lab", model_name="alpaca-7b-wdiff", revision=None)
+        # Tokenizer name hf-internal-testing/llama-tokenizer is taken from:
+        # https://huggingface.co/docs/transformers/main/en/model_doc/llama#transformers.LlamaTokenizerFast.example
+        model_config = HuggingFaceModelConfig(
+            namespace="hf-internal-testing", model_name="llama-tokenizer", revision=None
+        )
         super().__init__(service, model_config)
-        # tatsu-lab/alpaca-7b-wdiff incorrectly gives a sequence length of 512.
-        # The LLaMA max sequence length is 2048.
-        # See: https://github.com/facebookresearch/llama/issues/16
-        self._max_sequence_length = 512
