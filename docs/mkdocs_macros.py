071a0efda9284d2540e591cd0e1d5a1bf1ef3783
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-05-30 15:51:41 -0700

    Fix models not displaying

--- docs/mkdocs_macros.py
+++ docs/mkdocs_macros.py
@@ -2,14 +2,32 @@ from collections import defaultdict
 
 from helm.benchmark.run_expander import RUN_EXPANDERS
 from helm.proxy.models import ALL_MODELS
+import yaml
 
 
 def define_env(env):
     @env.macro
     def models_by_organization():
+        with open("src/helm/benchmark/static/schema.yaml", "r") as f:
+            schema = yaml.safe_load(f)
+        models = list(schema["models"])
         result = defaultdict(list)
-        for model in ALL_MODELS:
-            result[model.creator_organization].append(model)
+
+        # Create dict name -> madel_object (ALL_MODELS)
+        name_to_model_object = {}
+        for model_object in ALL_MODELS:
+            name_to_model_object[model_object.name] = model_object
+
+        for model in models:
+            # Find the model in ALL_MODELS in an efficient way
+            model_object = name_to_model_object.get(model["name"], None)
+            if model_object is not None:
+                model["tags"] = model_object.tags
+                model["group"] = model_object.group
+            else:
+                model["tags"] = []
+                model["group"] = model["creator_organization"]
+            result[model["creator_organization"]].append(model)
         if "Simple" in result:
             del result["Simple"]
         return result
@@ -20,4 +38,4 @@ def define_env(env):
 
     @env.macro
     def render_model_tags(model):
-        return ", ".join([f"`{tag}`" for tag in model.tags])
+        return ", ".join([f"`{tag}`" for tag in model["tags"]])
--- docs/models.md
+++ docs/models.md
@@ -6,13 +6,18 @@
 
 {% for model in models %}
 
+
 ### {{ model.display_name }}
 
 - Name: `{{ model.name }}`
 - Group: `{{ model.group }}`
+- Access: `{{ model.access }}`
 - Tags: {{ render_model_tags(model) }}
 
 {{ model.description }}
+{% if model.todo %}
+<span style="color:red">This model is not supported yet.</span>
+{% endif %}
 
 {% endfor %}
 {% endfor %}
