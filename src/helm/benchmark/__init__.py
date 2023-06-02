bfe01e831dbd934fe874cbd079ea757cc0e85396
YianZhang <yianzh@stanford.edu>
2023-06-02 00:34:54 -0700

    Initial commit

--- src/helm/benchmark/__init__.py
+++ src/helm/benchmark/__init__.py
@@ -49,6 +49,7 @@ from .scenarios import self_instruct_scenario  # noqa
 from .scenarios import grammar_scenario  # noqa
 from .scenarios import opinions_qa_scenario  # noqa
 from .scenarios import open_assistant_scenario  # noqa
+from .scenarios import koala_scenario  # noqa
 
 # Biomedical
 from .scenarios import covid_dialog_scenario  # noqa
