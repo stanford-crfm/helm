3f1aad5d3d4db4dd33dcfe77561532f3b0769329
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-06-05 15:50:17 -0700

    Changes requested

--- docs/mkdocs_macros.py
+++ docs/mkdocs_macros.py
@@ -1,16 +1,33 @@
 from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import Optional, List
 
+from helm.benchmark.presentation.schema import read_schema, ModelField
 from helm.benchmark.run_expander import RUN_EXPANDERS
-from helm.proxy.models import ALL_MODELS
-import yaml
+from helm.proxy.models import ALL_MODELS, Model
+
+
+@dataclass(frozen=True)
+class ModelInfo(ModelField):
+    group: Optional[str] = None
+    tags: List[str] = field(default_factory=list)
+
+    @staticmethod
+    def from_model_field_and_model_object(model_field: ModelField, model_object: Optional[Model] = None):
+        # Copy all attributes from model_field
+        # and set group to model_field.creator_organization
+        # and tags to an empty list
+        if model_object is not None:
+            model_info = ModelInfo(**model_field.__dict__, group=model_object.group, tags=model_object.tags)
+        else:
+            model_info = ModelInfo(**model_field.__dict__, group=model_field.creator_organization, tags=[])
+        return model_info
 
 
 def define_env(env):
     @env.macro
     def models_by_organization():
-        with open("src/helm/benchmark/static/schema.yaml", "r") as f:
-            schema = yaml.safe_load(f)
-        models = list(schema["models"])
+        schema = read_schema()
         result = defaultdict(list)
 
         # Create dict name -> madel_object (ALL_MODELS)
@@ -18,16 +35,10 @@ def define_env(env):
         for model_object in ALL_MODELS:
             name_to_model_object[model_object.name] = model_object
 
-        for model in models:
-            # Find the model in ALL_MODELS in an efficient way
-            model_object = name_to_model_object.get(model["name"], None)
-            if model_object is not None:
-                model["tags"] = model_object.tags
-                model["group"] = model_object.group
-            else:
-                model["tags"] = []
-                model["group"] = model["creator_organization"]
-            result[model["creator_organization"]].append(model)
+        for model_field in schema.models:
+            model_object = name_to_model_object.get(model_field.name, None)
+            model_info: ModelInfo = ModelInfo.from_model_field_and_model_object(model_field, model_object)
+            result[model_info.creator_organization].append(model_info)
         if "Simple" in result:
             del result["Simple"]
         return result
@@ -38,4 +49,4 @@ def define_env(env):
 
     @env.macro
     def render_model_tags(model):
-        return ", ".join([f"`{tag}`" for tag in model["tags"]])
+        return ", ".join([f"`{tag}`" for tag in model.tags])
