c85a2761133e72c6b511e54fb7a8ef938683a694
Tony Lee <tonyh.lee@yahoo.com>
2023-06-04 17:39:41 -0700

    efficiency metric

--- src/helm/benchmark/run_specs.py
+++ src/helm/benchmark/run_specs.py
@@ -2536,7 +2536,10 @@ def get_mscoco_spec(
 
     if for_efficiency:
         adapter_spec = get_image_generation_adapter_spec(num_outputs=1)
-        metric_specs = [MetricSpec(class_name="helm.benchmark.denoised_runtime_metric.DenoisedRuntimeMetric", args={})]
+        metric_specs = [
+            MetricSpec(class_name="helm.benchmark.denoised_runtime_metric.DenoisedRuntimeMetric", args={}),
+            MetricSpec(class_name="helm.benchmark.efficiency_metrics.EfficiencyMetric", args={}),
+        ]
         run_spec_name = "mscoco_efficiency"
     elif compute_fid:
         adapter_spec = get_image_generation_adapter_spec(num_outputs=1)
