5d528999cbb74315bcdfb477e58b287f9be73f26
YianZhang <yianzh@stanford.edu>
2023-06-09 16:43:51 -0700

    Switch metric to critique for instruction-following scenarios

--- src/helm/benchmark/run_specs.py
+++ src/helm/benchmark/run_specs.py
@@ -2075,7 +2075,7 @@ def get_self_instruct_spec(num_respondents: int) -> RunSpec:
 
 
 @run_spec_function("vicuna")
-def get_vicuna_spec(category: str = "all") -> RunSpec:
+def get_vicuna_spec(num_respondents: int, category: str = "all") -> RunSpec:
     scenario_spec = ScenarioSpec(
         class_name="helm.benchmark.scenarios.vicuna_scenario.VicunaScenario",
         args={"category": category},
@@ -2087,13 +2087,13 @@ def get_vicuna_spec(category: str = "all") -> RunSpec:
         name=f"vicuna:category={category}",  # TODO: add args
         scenario_spec=scenario_spec,
         adapter_spec=adapter_spec,
-        metric_specs=get_open_ended_generation_metric_specs() + get_generative_harms_metric_specs(),
+        metric_specs=get_instruction_following_critique_metric_specs(num_respondents),
         groups=["vicuna"],
     )
 
 
 @run_spec_function("grammar")
-def get_grammar_spec(path: str, tags: str) -> RunSpec:
+def get_grammar_spec(num_respondents: int, path: str, tags: str) -> RunSpec:
     scenario_spec = ScenarioSpec(
         class_name="helm.benchmark.scenarios.grammar_scenario.GrammarScenario",
         args={"path": path, "tags": tags},
@@ -2105,7 +2105,7 @@ def get_grammar_spec(path: str, tags: str) -> RunSpec:
         name=f"grammar:path={path},tags={tags}",
         scenario_spec=scenario_spec,
         adapter_spec=adapter_spec,
-        metric_specs=get_open_ended_generation_metric_specs() + get_generative_harms_metric_specs(),
+        metric_specs=get_instruction_following_critique_metric_specs(num_respondents),
         groups=["grammar"],
     )
 
@@ -2173,7 +2173,7 @@ def get_opinions_qa_spec(
 
 
 @run_spec_function("open_assistant")
-def get_open_assistant_spec(language: str) -> RunSpec:
+def get_open_assistant_spec(num_respondents: int, language: str) -> RunSpec:
     scenario_spec = ScenarioSpec(
         class_name="helm.benchmark.scenarios.open_assistant_scenario.OpenAssistantScenario",
         args={"language": language},
@@ -2185,13 +2185,13 @@ def get_open_assistant_spec(language: str) -> RunSpec:
         name=f"open_assistant:language={language}",
         scenario_spec=scenario_spec,
         adapter_spec=adapter_spec,
-        metric_specs=get_open_ended_generation_metric_specs() + get_generative_harms_metric_specs(),
+        metric_specs=get_instruction_following_critique_metric_specs(num_respondents),
         groups=["open_assistant"],
     )
 
 
 @run_spec_function("koala")
-def get_koala_spec() -> RunSpec:
+def get_koala_spec(num_respondents: int) -> RunSpec:
     scenario_spec = ScenarioSpec(
         class_name="helm.benchmark.scenarios.koala_scenario.KoalaScenario",
         args={},
@@ -2203,13 +2203,13 @@ def get_koala_spec() -> RunSpec:
         name="koala",
         scenario_spec=scenario_spec,
         adapter_spec=adapter_spec,
-        metric_specs=get_open_ended_generation_metric_specs() + get_generative_harms_metric_specs(),
+        metric_specs=get_instruction_following_critique_metric_specs(num_respondents),
         groups=["koala"],
     )
 
 
 @run_spec_function("anthropic_hh_rlhf")
-def get_anthropic_hh_rlhf_spec(subset: str) -> RunSpec:
+def get_anthropic_hh_rlhf_spec(num_respondents: int, subset: str) -> RunSpec:
     scenario_spec = ScenarioSpec(
         class_name="helm.benchmark.scenarios.anthropic_hh_rlhf_scenario.AnthropicHHRLHFScenario",
         args={"subset": subset},
@@ -2221,7 +2221,7 @@ def get_anthropic_hh_rlhf_spec(subset: str) -> RunSpec:
         name=f"anthropic_hh_rlhf:subset={subset}",
         scenario_spec=scenario_spec,
         adapter_spec=adapter_spec,
-        metric_specs=get_open_ended_generation_metric_specs() + get_generative_harms_metric_specs(),
+        metric_specs=get_instruction_following_critique_metric_specs(num_respondents),
         groups=["anthropic_hh_rlhf"],
     )
 
