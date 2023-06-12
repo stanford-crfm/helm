9cda6d648eed5d05d77c60fbe8bed13cef5b2683
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-06-12 16:56:24 -0700

    Fix tests

--- src/helm/proxy/clients/test_yalm_tokenizer_client.py
+++ src/helm/proxy/clients/test_yalm_tokenizer_client.py
@@ -25,7 +25,7 @@ class TestYaLMTokenizerClient:
         os.remove(self.cache_path)
 
     def test_tokenize(self):
-        request = TokenizationRequest(text=self.test_prompt)
+        request = TokenizationRequest(tokenizer="Yandex/yalm", text=self.test_prompt)
         result: TokenizationRequestResult = self.client.tokenize(request)
         assert not result.cached, "First time making the tokenize request. Result should not be cached"
         result: TokenizationRequestResult = self.client.tokenize(request)
@@ -33,18 +33,20 @@ class TestYaLMTokenizerClient:
         assert result.raw_tokens == [" The", " model", " lever", "ages", " 100", " billion", " parameters", "."]
 
     def test_encode(self):
-        request = TokenizationRequest(text=self.test_prompt, encode=True)
+        request = TokenizationRequest(tokenizer="Yandex/yalm", text=self.test_prompt, encode=True)
         result: TokenizationRequestResult = self.client.tokenize(request)
         assert result.raw_tokens == self.encoded_test_prompt
 
     def test_encode_with_truncation(self):
         max_length: int = 6
-        request = TokenizationRequest(text=self.test_prompt, encode=True, truncation=True, max_length=max_length)
+        request = TokenizationRequest(
+            tokenizer="Yandex/yalm", text=self.test_prompt, encode=True, truncation=True, max_length=max_length
+        )
         result: TokenizationRequestResult = self.client.tokenize(request)
         assert result.raw_tokens == self.encoded_test_prompt[:max_length]
 
     def test_decode(self):
-        request = DecodeRequest(tokens=self.encoded_test_prompt)
+        request = DecodeRequest(tokenizer="Yandex/yalm", tokens=self.encoded_test_prompt)
         result: DecodeRequestResult = self.client.decode(request)
         assert not result.cached, "First time making the decode request. Result should not be cached"
         result: DecodeRequestResult = self.client.decode(request)
