0cd4edd9fdb13e1a8e15d0738977e3a41b352ada
Yifan Mai <yifan@cs.stanford.edu>
2023-07-26 02:23:56 +0800

    Fix test

--- src/helm/proxy/clients/test_together_client.py
+++ src/helm/proxy/clients/test_together_client.py
@@ -42,7 +42,7 @@ class TestTogetherClient:
             ),
             (
                 Request(
-                    model="together/llama-7b",
+                    model="meta/llama-7b",
                     prompt="I am a computer scientist.",
                     temperature=0,
                     num_completions=4,
