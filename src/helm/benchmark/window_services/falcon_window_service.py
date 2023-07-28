4dd40a0cb52d3e654e07cb3907b924e252629119
Pratik Budruk <pratikbudruk@Pratiks-MacBook-Pro.local>
2023-07-29 01:22:50 +0530

    added falcon window service

---
+++ src/helm/benchmark/window_services/falcon_window_service.py
@@ -0,0 +1,36 @@
+from .local_window_service import LocalWindowService
+from .tokenizer_service import TokenizerService
+
+
+class FalconWindowService(LocalWindowService):
+    def __init__(self, service: TokenizerService):
+        super().__init__(service)
+
+    @property
+    def max_sequence_length(self) -> int:
+        """
+        The max length of the model input. The max sequence length for the OPT models is 2048.
+        Source: https://arxiv.org/pdf/2205.01068.pdf
+        """
+        return 3584
+
+    @property
+    def max_request_length(self) -> int:
+        """Return the max request length."""
+        return self.max_sequence_length - 1
+
+    @property
+    def end_of_text_token(self) -> str:
+        """The end of text token."""
+        return "<|endoftext|>"
+
+    @property
+    def prefix_token(self) -> str:
+        """The prefix token is the same as the end of text token."""
+        return self.end_of_text_token
+
+    @property
+    def tokenizer_name(self) -> str:
+        """Name of the tokenizer to use when sending a request."""
+        return "tiiuae/falcon-40b"
+
