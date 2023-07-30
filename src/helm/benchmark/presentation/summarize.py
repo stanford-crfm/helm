73afd69dcbb06b42962710263823e345b85a4cf0
Andy Z <andy@DN0a1f89ea.SUNet>
2023-07-30 16:56:35 -0700

    Black

--- src/helm/benchmark/presentation/summarize.py
+++ src/helm/benchmark/presentation/summarize.py
@@ -716,12 +716,12 @@ class Summarizer:
                     num_instances = group_overlap_stats.num_instances
                     num_overlapping_inputs = group_overlap_stats.num_overlapping_inputs
                     num_overlapping_references = group_overlap_stats.num_overlapping_references
-                    overlapping_input_ratio = num_overlapping_inputs / num_instances 
-                    overlapping_reference_ratio = num_overlapping_references / num_instances 
+                    overlapping_input_ratio = num_overlapping_inputs / num_instances
+                    overlapping_reference_ratio = num_overlapping_references / num_instances
                     description = (
-                        f'Overlapping input ratio: {overlapping_input_ratio:.3f}\n'
-                        f'Overlapping reference ratio: {overlapping_reference_ratio:.3f}\n'
-                        f'{description}'
+                        f"Overlapping input ratio: {overlapping_input_ratio:.3f}\n"
+                        f"Overlapping reference ratio: {overlapping_reference_ratio:.3f}\n"
+                        f"{description}"
                     )
 
                 # HACK: we want to hide stats for the following model-metric combinations:
