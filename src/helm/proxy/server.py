b27a37e1f0cfeed8e6f7356588b2fdd5fb6a5407
AndrewJGaut <ajg@umail.ucsb.edu>
2023-08-19 14:55:08 -0700

    fmt

--- src/helm/proxy/server.py
+++ src/helm/proxy/server.py
@@ -8,7 +8,6 @@ Look at `index.js` to see how the functionality is invoked.
 from urllib.parse import unquote_plus
 import argparse
 import dataclasses
-import json
 import os
 import sys
 import time
