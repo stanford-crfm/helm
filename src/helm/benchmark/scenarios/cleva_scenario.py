c1bb31c8c9e5438ad84d2df244bf2e9e1c91149a
Yanyang Li <yyli21@cse.cuhk.edu.hk>
2023-08-20 00:54:13 +0800

    Fix Python style

--- src/helm/benchmark/scenarios/cleva_scenario.py
+++ src/helm/benchmark/scenarios/cleva_scenario.py
@@ -15,10 +15,10 @@ CLEVA_DATA_PATH = "benchmark_output/scenarios/cleva"
 @dataclass(frozen=True)
 class PromptSetting:
     instructions: str
-    input_noun: Optional[str] = None,
-    newline_after_input_noun: bool = False,
-    output_noun: Optional[str] = None,
-    newline_after_output_noun: bool = False,
+    input_noun: Optional[str] = None
+    newline_after_input_noun: bool = False
+    output_noun: Optional[str] = None
+    newline_after_output_noun: bool = False
 
 
 class CLEVAScenario(Scenario):
@@ -57,7 +57,7 @@ class CLEVAScenario(Scenario):
         ensure_directory_exists(data_dir)
         ensure_file_downloaded(source_url=download_url, target_path=os.path.join(data_dir, "data.zip"))
 
-        with zipfile.ZipFile(os.path.join(data_dir, "data.zip"), 'r') as zip_ref:
+        with zipfile.ZipFile(os.path.join(data_dir, "data.zip"), "r") as zip_ref:
             zip_ref.extractall(data_dir)
 
     def load_dataset(self) -> Dict[str, List[Dict[str, Any]]]:
@@ -92,12 +92,12 @@ class CLEVAScenario(Scenario):
         if "choices" in row.keys():
             answers: List[str] = row["choices"]
             correct_choice: List[int] = row["label"]
-            
+
             correct_answer: List[str] = [answers[idx] for idx in correct_choice]
 
             def answer_to_reference(answer: str) -> Reference:
                 return Reference(Output(text=answer), tags=[CORRECT_TAG] if answer in correct_answer else [])
-            
+
             references: list[Instance] = list(map(answer_to_reference, answers))
         else:
             answers: List[str] = row["label"]
@@ -109,7 +109,7 @@ class CLEVAScenario(Scenario):
             split=split,
         )
         return instance
-    
+
     @classmethod
     def get_prompt_setting(cls, task: str, subtask: str, version: str) -> PromptSetting:
         # TODO: get prompt setting online
@@ -227,7 +227,7 @@ class CLEVAOpinionMiningScenario(CLEVAScenario):
 
         陈述: 从亚龙湾出发，大概40分钟左右的车程即可到达码头，转乘轮渡抵达蜈支洲岛。
         主体: 蜈支洲岛
-        
+
         陈述: 这是一座被称为最美大学的校园，座山面海是厦门大学得天独厚的自然条件。
         主体:
 
@@ -251,7 +251,7 @@ class CLEVAPinyinTransliterationScenario(CLEVAScenario):
 
         拼音：wǒ men shǒu tóu mù qián dōu bǐ jiào kuān yù
         汉字：
-        
+
     Target: 我们手头目前都比较宽裕
 
     An example of zh2pinyin subtask is:
@@ -265,6 +265,7 @@ class CLEVAPinyinTransliterationScenario(CLEVAScenario):
 
     Target: zhè shì qiú lèi bǐ sài
     """
+
     # name = "cleva_pinyin_transliteration"
     description = "Pinyin transliteration task in CLEVA benchmark"
     tags = ["pinyin_transliteration"]
@@ -335,11 +336,17 @@ class CLEVAInstructionFollowingScenario(CLEVAScenario):
 
     Target: A
     """
+
     # name = "cleva_instruction_following"
     description = "Instruction following task in CLEVA benchmark"
     tags = ["instruction_following"]
 
-    def __init__(self, version: str, task: str, subtask: str, ):
+    def __init__(
+        self,
+        version: str,
+        task: str,
+        subtask: str,
+    ):
         super().__init__(version, task, subtask)
         self.splits: Dict[str, str] = {
             "test": TEST_SPLIT,
