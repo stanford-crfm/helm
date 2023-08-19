cfb9abed364569eada8a66ff5e299d518a685bf3
Andy Z <andy@DNa1d819b.SUNet>
2023-08-18 21:35:18 -0700

    Implement rabin karp

---
+++ scripts/data_overlap/ngram_hasher.py
@@ -0,0 +1,95 @@
+from typing import List, Tuple
+
+class RabinKarpHash:
+    """
+    (ignoring mods here for simplicity)
+    Initial hash:
+        for a List[int] i_0...i_n with a window w s.t. w <= n
+        hash is i_0 * 256 ^ {w-1} + i_1 * 256 ^ {w-2} + ... + i_{w-1}
+
+    high_coefficient:
+        256 * {w-1}
+    
+    Is it 256 because 2^8 = 256, and the expected input is UTF-8 chars?
+
+    In our case, it probably makes more sense to use a large prime p
+
+    and convert words/tokens into ints (probably by multiplying chars by a prime or 256)
+    """
+    def __init__(self, int_sequence: List[int], window_size: int, mod: int):
+        self.window_start = 0
+        self.window_end = window_size
+        self.mod = mod
+        self.high_coefficient = pow(256, window_size - 1, mod)
+        self.current_hash = 0
+        self.int_sequence = int_sequence
+
+        """
+        Is window_size the number of n-grams (i.e., tokens) or is it the number of characters? The comment says:
+
+        # for i in range(window_size):
+        #     self.hash = (self.hash * 256 + ord(self.text[i])) % mod
+
+        ord only acts at a character level, but the logic only makes sense for n-grams/tokens (otherwise we have no way to configure N, the length of n-grams)
+        """
+        for i in range(window_size):
+            self.current_hash = (self.current_hash * 256 + int_sequence[i]) % mod
+        if self.current_hash < 0:
+            self.current_hash += mod
+
+    def mult_mod(self, a: int, b: int, k: int) -> int:
+        return ((a * b) % k)
+
+    """
+    Initial hash:
+        for a List[int] i_0...i_n with a window w s.t. w <= n
+        hash is i_0 * 256 ^ {w-1} + i_1 * 256 ^ {w-2} + ... + i_{w-1}
+    
+    start_piece = i_0 * 256 ^ {w-1}
+
+    subtract the start piece and multiply by 256, then add the current value, so we have
+        hash is i_1 * 256 ^ {w-1} + i_2 * 256 ^ {w-2} + ... + i_{w}
+
+    """
+    def update(self) -> None:
+        start_piece = self.mult_mod(self.int_sequence[self.window_start], self.high_coefficient, self.mod)
+        self.current_hash = self.mult_mod(self.current_hash - start_piece, 256, self.mod)
+        self.current_hash = (self.current_hash + self.int_sequence[self.window_end]) % self.mod
+        if self.current_hash < 0:
+            self.current_hash += self.mod
+        self.window_start += 1
+        self.window_end += 1
+
+def compute_hashes(int_sequence: List[int], window_size: int, mod: int) -> List[Tuple[int, int, int]]:
+    """
+    Previously was 
+    rk_hash = RabinKarpHash(
+        int_sequence,
+        min(window_size, len(int_sequence))
+    )
+    but we don't want anything to happen when len(int_sequence) < window_size
+    """
+    if len(int_sequence) < window_size:
+        return []
+    rk_hash = RabinKarpHash(
+        int_sequence,
+        window_size,
+        mod
+    )
+    hashes = [(rk_hash.current_hash, rk_hash.window_start, rk_hash.window_end)]
+    for i in range(len(int_sequence) - window_size):
+        rk_hash.update()
+        hash_info = (rk_hash.current_hash, rk_hash.window_start, rk_hash.window_end)
+        hashes.append(hash_info)
+
+    return hashes
+
+def hash_token(token: str, mod: int):
+    hash_value = 0
+    for ch in token:
+        hash_value = (hash_value * 256 + ord(ch)) % mod
+    return hash_value
+
+def get_ngram_hashes(tokens: List[str], n: int, mod: int = 8554560727166512717):
+    hashed_tokens = [hash_token(token, mod) for token in tokens]
+    return compute_hashes(hashed_tokens, n, mod)
