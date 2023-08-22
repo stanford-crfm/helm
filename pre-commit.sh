5e0dae2cb34b65cdb96464f757300daa045c15cb
Yifan Mai <yifan@cs.stanford.edu>
2023-08-22 10:04:54 -0700

    Speed up pre-commit by moving `pip install` to a separate script (#1800)

--- pre-commit.sh
+++ pre-commit.sh
@@ -11,16 +11,6 @@ if [ "$valid_version" == "False" ]; then
   exit 1
 fi
 
-# On Mac OS, skip installing pytorch with CUDA because CUDA is not supported
-if [[ $OSTYPE != 'darwin'* ]]; then
-  # Manually install pytorch to avoid pip getting killed: https://stackoverflow.com/a/54329850
-  pip install --no-cache-dir --find-links https://download.pytorch.org/whl/torch_stable.html torch==1.12.1+cu113 torchvision==0.13.1+cu113
-fi
-# Manually install protobuf to workaround issue: https://github.com/protocolbuffers/protobuf/issues/6550
-pip install --no-binary=protobuf protobuf==3.20.2
-# Install all pinned dependencies
-pip install -r requirements-freeze.txt
-pip install -e .
 pip check
 
 # Python style checks and linting
