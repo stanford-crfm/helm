003a3c0a2b65cfda4af3624dc2b13ac8f67e8342
Andy Z <andy@DN51v4ii.SUNet>
2023-08-28 21:14:21 -0700

    Update comment

--- scripts/data_overlap/compute_data_overlap_metrics.py
+++ scripts/data_overlap/compute_data_overlap_metrics.py
@@ -109,7 +109,7 @@ def create_ngram_index(
                         )
 
                 # concatenate the last n-1 tokens of input and the first n-1 tokens 
-                # of reference and compute n-grams on this "interesection token" sequence
+                # of reference and compute n-grams on this "interesection token sequence"
                 # for instance: input = ["is 2+2 4 true or false"] reference = ["true"]
                 # the intersection is the 5-gram ["4 true or false true"]
                 # (which is formed from the input 4-gram [4 true or false] and the reference 1-gram [true])
