69e0a9ec6a4d6e02dbd968959c2bfd69d4085e6c
Weixin Chen <48148205+chenweixin107@users.noreply.github.com>
2023-08-30 18:20:43 -0500

    Update __init__.py

--- src/helm/benchmark/__init__.py
+++ src/helm/benchmark/__init__.py
@@ -55,6 +55,7 @@ from .scenarios import koala_scenario  # noqa
 from .scenarios import decodingtrust_adv_robustness_scenario
 from .scenarios import decodingtrust_ood_robustness_scenario
 from .scenarios import decodingtrust_fairness_scenario
+from .scenarios import decodingtrust_machine_ethics_scenario
 
 
 # Biomedical
