4876f6a424abd506412e138f479560445fc991c3
Andy Z <andy@DNa811b82.SUNet>
2023-09-24 21:25:45 -0700

    Script to compute metrics

---
+++ scripts/data_overlap/run_metrics.sh
@@ -0,0 +1,11 @@
+for x in {a..r}
+do
+    echo $x
+    python3 compute_metrics_from_ngrams.py --ngrams-path "pile_ngrams/output_stats_pi..gram_xa${x}_ngrams" --scenario-path "filtered_scenario_data_new" --out-path "new_filtered_pile_metrics_${x}" --filter-path "scenario_spec_i.._ids_1000.jsonl"
+done
+
+# for x in {a..r}
+# do
+#     echo $x
+#     python3 compute_metrics_from_ngrams.py --ngrams-path "pile_ngrams\/output_stats_pi..gram_xa${x}_ngrams"  —-scenario-path "filtered_scenario_data_new" --out-path "new_filtered_pile_metrics_${x}" --filter-path "scenario_spec_i.._ids_1000.jsonl"
+# done
