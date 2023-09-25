2011e13323af1a6c3b1813ea9f57d756dd235a0a
Andy Z <andy@DNa811b45.SUNet>
2023-09-25 07:38:40 -0700

    Update

--- scripts/data_overlap/combine_ngrams.sh
+++ scripts/data_overlap/combine_ngrams.sh
@@ -9,7 +9,6 @@ combined_output="combined_ngrams"
 # Loop through all files with "_ngrams" in the name and append to the combined output
 for file in output_stats/*_ngrams; do
     cat "$file" >> $combined_output
-    echo -e "\n" >> $combined_output # add a newline for separation (optional)
 done
 
 echo "All ngrams files have been combined into $combined_output"
