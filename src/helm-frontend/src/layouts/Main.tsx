8e0368cdbb31a2aa86d495bd529080c05cb022cf
farzaank <fkaiyom@gmail.com>
2023-10-12 10:40:41 -0700

    missing import

--- src/helm-frontend/src/layouts/Main.tsx
+++ src/helm-frontend/src/layouts/Main.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Nav from "@/components/Nav/Nav";
+import Nav from "@/components/NavComponents/Nav";
 
 export default function Main() {
   return (
