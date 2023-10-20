6bc7e4a8cc283c52835ee5075807ae330f2b5c69
Brian Goldman <auxy@google.com>
2023-10-20 15:12:52 -0600

    Simplify call structure when updating Stats member variables.
    Before, a method called "_update_stddev" called "_update_variance" which called "_update_mean". Beyond being confusingly named (update_stddv doesn't tell you it updates mean), there were no use cases where we'd want to update just one of the three.

--- src/helm/benchmark/metrics/test_statistic.py
+++ src/helm/benchmark/metrics/test_statistic.py
@@ -1,7 +1,24 @@
 from typing import Dict
 
+import pytest
+import statistics
+
 from .metric_name import MetricName
 from .statistic import Stat, merge_stat
+
+
+def test_stat_add():
+    stat = Stat(MetricName("some_metric"))
+    population = list(range(10))
+    for i in population:
+        stat.add(i)
+    assert stat.sum == sum(population)
+    assert stat.count == 10
+    assert stat.min == 0
+    assert stat.max == 9
+    assert stat.mean == sum(population) / 10
+    assert stat.variance == pytest.approx(statistics.pvariance(population))
+    assert stat.stddev == pytest.approx(statistics.pstdev(population))
 
 
 def test_merge_stat():
@@ -12,3 +29,15 @@ def test_merge_stat():
 
     assert len(stats) == 1
     assert stats[metric_name].sum == 2
+    assert stats[metric_name].mean == 1
+
+
+def test_merge_empty_stat():
+    # This test ensures we guard against division by zero.
+    metric_name = MetricName("some_metric")
+    empty_1 = Stat(metric_name)
+    empty_2 = Stat(metric_name)
+    merged = empty_1.merge(empty_2)
+
+    assert merged.count == 0
+    assert merged.stddev is None
