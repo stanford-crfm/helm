02f3ec491f36dca2166bff5aa869169036b450b5
farzaank <fkaiyom@gmail.com>
2023-10-25 12:40:20 -0700

    fixed fmt

--- src/helm-frontend/src/App.tsx
+++ src/helm-frontend/src/App.tsx
@@ -10,20 +10,20 @@ import Runs from "@/routes/Runs";
 import Run from "@/routes/Run";
 import LegacyLanding from "@/routes/LegacyLanding";
 export default function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route path={"/"} element={<Home />} />
-					<Route path="/models" element={<Models />} />
-					<Route path="/landing" element={<LegacyLanding />} />
-					<Route path="/scenarios" element={<Scenarios />} />
-					<Route path="/groups" element={<Groups />} />
-					<Route path="/groups/:groupName" element={<Group />} />
-					<Route path="/runs" element={<Runs />} />
-					<Route path="/runs/:runName" element={<Run />} />
-				</Route>
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={"/"} element={<Home />} />
+          <Route path="/models" element={<Models />} />
+          <Route path="/landing" element={<LegacyLanding />} />
+          <Route path="/scenarios" element={<Scenarios />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/groups/:groupName" element={<Group />} />
+          <Route path="/runs" element={<Runs />} />
+          <Route path="/runs/:runName" element={<Run />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
--- src/helm-frontend/src/routes/LegacyLanding.tsx
+++ src/helm-frontend/src/routes/LegacyLanding.tsx
@@ -25,136 +25,136 @@ import tsinghuaKeg from "@/assets/logos/tsinghua-keg.png";
 import yandex from "@/assets/logos/yandex.png";
 
 const logos = [
-	ai21,
-	anthropic,
-	bigscience,
-	cohere,
-	eleutherai,
-	google,
-	meta,
-	microsoft,
-	nvidia,
-	openai,
-	together,
-	tsinghuaKeg,
-	yandex,
+  ai21,
+  anthropic,
+  bigscience,
+  cohere,
+  eleutherai,
+  google,
+  meta,
+  microsoft,
+  nvidia,
+  openai,
+  together,
+  tsinghuaKeg,
+  yandex,
 ];
 
 export default function LegacyLanding() {
-	const [schema, setSchema] = useState<Schema | undefined>(undefined);
+  const [schema, setSchema] = useState<Schema | undefined>(undefined);
 
-	useEffect(() => {
-		const controller = new AbortController();
-		async function fetchData() {
-			const schema = await getSchema(controller.signal);
-			setSchema(schema);
-		}
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchData() {
+      const schema = await getSchema(controller.signal);
+      setSchema(schema);
+    }
 
-		void fetchData();
-		return () => controller.abort();
-	}, []);
+    void fetchData();
+    return () => controller.abort();
+  }, []);
 
-	if (!schema) {
-		return null;
-	}
+  if (!schema) {
+    return null;
+  }
 
-	return (
-		<>
-			<Hero />
-			<div className="flex flex-col sm:flex-row justify-center mt-10 mb-10 flex gap-2 sm:gap-8 md:gap-32">
-				{" "}
-				<h1 className="text-4xl mb-4 mx-4">
-					<strong>About HELM</strong>
-				</h1>
-			</div>
-			<div className="flex flex-col sm:flex-row justify-center mt-16 mb-32 flex gap-2 sm:gap-8 md:gap-32">
-				<Link to="https://crfm.stanford.edu/2022/11/17/helm.html">
-					<button className="px-10 btn btn-grey rounded-md">Blog post</button>
-				</Link>
-				<Link to="https://arxiv.org/pdf/2211.09110.pdf">
-					<button className="px-10 btn btn-gray rounded-md">Paper</button>
-				</Link>
-				<Link to="https://github.com/stanford-crfm/helm">
-					<button className="px-10 btn btn-gray rounded-md">Github</button>
-				</Link>
-			</div>
-			<div className="container mx-auto text-lg">
-				<p>
-					A language model takes in text and produces text:
-					<img
-						src={languageModelHelm}
-						alt="Language model diagram"
-						className="mx-auto block w-[800px] max-w-full h-auto"
-					/>
-				</p>
+  return (
+    <>
+      <Hero />
+      <div className="flex flex-col sm:flex-row justify-center mt-10 mb-10 flex gap-2 sm:gap-8 md:gap-32">
+        {" "}
+        <h1 className="text-4xl mb-4 mx-4">
+          <strong>About HELM</strong>
+        </h1>
+      </div>
+      <div className="flex flex-col sm:flex-row justify-center mt-16 mb-32 flex gap-2 sm:gap-8 md:gap-32">
+        <Link to="https://crfm.stanford.edu/2022/11/17/helm.html">
+          <button className="px-10 btn btn-grey rounded-md">Blog post</button>
+        </Link>
+        <Link to="https://arxiv.org/pdf/2211.09110.pdf">
+          <button className="px-10 btn btn-gray rounded-md">Paper</button>
+        </Link>
+        <Link to="https://github.com/stanford-crfm/helm">
+          <button className="px-10 btn btn-gray rounded-md">Github</button>
+        </Link>
+      </div>
+      <div className="container mx-auto text-lg">
+        <p>
+          A language model takes in text and produces text:
+          <img
+            src={languageModelHelm}
+            alt="Language model diagram"
+            className="mx-auto block w-[800px] max-w-full h-auto"
+          />
+        </p>
 
-				<p className="mb-32">
-					Despite their simplicity, language models are increasingly functioning
-					as the foundation for almost all language technologies from question
-					answering to summarization. But their immense capabilities and risks
-					are not well understood. Holistic Evaluation of Language Models (HELM)
-					is a living benchmark that aims to improve the transparency of
-					language models.
-				</p>
+        <p className="mb-32">
+          Despite their simplicity, language models are increasingly functioning
+          as the foundation for almost all language technologies from question
+          answering to summarization. But their immense capabilities and risks
+          are not well understood. Holistic Evaluation of Language Models (HELM)
+          is a living benchmark that aims to improve the transparency of
+          language models.
+        </p>
 
-				<ol className="mt-12 flex flex-col gap-32">
-					<li>
-						<strong>Broad coverage and recognition of incompleteness.</strong>{" "}
-						We define a taxonomy over the scenarios we would ideally like to
-						evaluate, select scenarios and metrics to cover the space and make
-						explicit what is missing.
-						<img
-							src={taxonomyScenarios}
-							alt="Taxonomy scenarios chart"
-							className="mx-auto block w-[800px] max-w-full h-auto"
-						/>
-					</li>
-					<li>
-						<strong>Multi-metric measurement.</strong> Rather than focus on
-						isolated metrics such as accuracy, we simultaneously measure
-						multiple metrics (e.g., accuracy, robustness, calibration,
-						efficiency) for each scenario, allowing analysis of tradeoffs.
-						<img
-							src={scenariosByMetrics}
-							alt="Scenarios by metrics table"
-							className="mx-auto block w-[800px] max-w-full h-auto"
-						/>
-					</li>
-					<li>
-						<strong>Standardization.</strong> We evaluate all the models that we
-						have access to on the same scenarios with the same adaptation
-						strategy (e.g., prompting), allowing for controlled comparisons.
-						Thanks to all the companies for providing API access to the
-						limited-access and closed models and{" "}
-						<a target="_black" href="https://together.xyz/">
-							Together
-						</a>{" "}
-						for providing the infrastructure to run the open models.
-						<div className="flex flex-wrap justify-center max-w-[1100px] mx-auto w-auto">
-							{logos.map((logo, idx) => (
-								<div className="w-24 h-24 flex items-center m-6" key={idx}>
-									<img
-										src={logo}
-										alt="Logo"
-										className="mx-auto block"
-										sizes="100vw"
-									/>
-								</div>
-							))}
-						</div>
-					</li>
-				</ol>
-			</div>
-			<div className="container mx-auto">
-				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-					<ModelsList models={schema.models} />
-					<ScenariosList runGroups={schema.run_groups} />
-					<MetricsList
-						metrics={schema.metrics}
-						metricGroups={schema.metric_groups}
-					/>
-				</div>
-			</div>
-		</>
-	);
+        <ol className="mt-12 flex flex-col gap-32">
+          <li>
+            <strong>Broad coverage and recognition of incompleteness.</strong>{" "}
+            We define a taxonomy over the scenarios we would ideally like to
+            evaluate, select scenarios and metrics to cover the space and make
+            explicit what is missing.
+            <img
+              src={taxonomyScenarios}
+              alt="Taxonomy scenarios chart"
+              className="mx-auto block w-[800px] max-w-full h-auto"
+            />
+          </li>
+          <li>
+            <strong>Multi-metric measurement.</strong> Rather than focus on
+            isolated metrics such as accuracy, we simultaneously measure
+            multiple metrics (e.g., accuracy, robustness, calibration,
+            efficiency) for each scenario, allowing analysis of tradeoffs.
+            <img
+              src={scenariosByMetrics}
+              alt="Scenarios by metrics table"
+              className="mx-auto block w-[800px] max-w-full h-auto"
+            />
+          </li>
+          <li>
+            <strong>Standardization.</strong> We evaluate all the models that we
+            have access to on the same scenarios with the same adaptation
+            strategy (e.g., prompting), allowing for controlled comparisons.
+            Thanks to all the companies for providing API access to the
+            limited-access and closed models and{" "}
+            <a target="_black" href="https://together.xyz/">
+              Together
+            </a>{" "}
+            for providing the infrastructure to run the open models.
+            <div className="flex flex-wrap justify-center max-w-[1100px] mx-auto w-auto">
+              {logos.map((logo, idx) => (
+                <div className="w-24 h-24 flex items-center m-6" key={idx}>
+                  <img
+                    src={logo}
+                    alt="Logo"
+                    className="mx-auto block"
+                    sizes="100vw"
+                  />
+                </div>
+              ))}
+            </div>
+          </li>
+        </ol>
+      </div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <ModelsList models={schema.models} />
+          <ScenariosList runGroups={schema.run_groups} />
+          <MetricsList
+            metrics={schema.metrics}
+            metricGroups={schema.metric_groups}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
