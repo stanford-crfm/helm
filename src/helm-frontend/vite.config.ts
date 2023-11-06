961091f6aacc2a4ed921615e1ef345bb7c12ddcb
farzaank <fkaiyom@gmail.com>
2023-11-06 13:02:46 -0800

    remove base path

--- src/helm-frontend/vite.config.ts
+++ src/helm-frontend/vite.config.ts
@@ -17,5 +17,5 @@ export default defineConfig({
   build: {
     outDir: `${__dirname}/../helm/benchmark/static_build`,
   },
-  base: "/helm/" // can't add process.env.HELM_SUITE here with GH pages 
+  //base: "/helm/" // can't add process.env.HELM_SUITE here with GH pages 
 });
