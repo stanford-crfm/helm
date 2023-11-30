1467ca04b3ba01000a07990bfc8921ca6fdb4ce8
Tony Lee <tonyh.lee@yahoo.com>
2023-11-30 12:00:08 -0800

    simplify installation

--- install-heim-extras.sh
+++ install-heim-extras.sh
@@ -19,19 +19,8 @@ fi
 
 # For CogView2, manually install apex and Image-Local-Attention. NOTE: need to run this on a GPU machine
 echo "Installing CogView2 dependencies..."
-if (nvcc --version > /dev/null 2>&1); then
-    ROOT=$(exec pwd)
-    mkdir -p tmp && chmod -R 777 tmp && rm -r tmp
-    mkdir -p tmp && cd tmp && git clone https://github.com/Sleepychord/Image-Local-Attention && cd Image-Local-Attention && git checkout 43fee310cb1c6f64fb0ed77404ba3b01fa586026 && python setup.py install
-    cd "$ROOT"
-    mkdir -p tmp && cd tmp && git clone https://github.com/michiyasunaga/apex && cd apex && git checkout 9395ba2aab3c05e0e36ef0b7fe48d42de9f10bcf && pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-    cd "$ROOT"
-else
-    ROOT=$(exec pwd)
-    mkdir -p tmp && chmod -R 777 tmp && rm -r tmp
-    mkdir -p tmp && cd tmp && git clone https://github.com/michiyasunaga/apex && cd apex && git checkout 9395ba2aab3c05e0e36ef0b7fe48d42de9f10bcf && pip install -v --disable-pip-version-check --no-cache-dir ./
-    cd "$ROOT"
-fi
+pip install localAttention@git+https://github.com/Sleepychord/Image-Local-Attention.git@43fee310cb1c6f64fb0ed77404ba3b01fa586026
+pip install --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" apex@git+https://github.com/michiyasunaga/apex.git@9395ba2aab3c05e0e36ef0b7fe48d42de9f10bcf
 
 # For Detectron2. Following https://detectron2.readthedocs.io/en/latest/tutorials/install.html
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
