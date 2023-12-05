1927cdbe47a6ee309d6ff6fa21e1e8fb664485b7
Yifan Mai <yifan@cs.stanford.edu>
2023-12-05 15:01:22 -0800

    Hold a lock when using thread-hostile Hugging Face tokenizers

--- src/helm/proxy/tokenizers/test_huggingface_tokenizer.py
+++ src/helm/proxy/tokenizers/test_huggingface_tokenizer.py
@@ -93,12 +93,13 @@ class TestHuggingFaceTokenizer:
     def verify_get_tokenizer(
         tokenizer_name: str, expected_num_tokens: int, pretrained_model_name_or_path: Optional[str] = None
     ):
-        tokenizer = HuggingFaceTokenizer.get_tokenizer(
+        wrapped_tokenizer = HuggingFaceTokenizer.get_tokenizer(
             helm_tokenizer_name=tokenizer_name,
             pretrained_model_name_or_path=pretrained_model_name_or_path or tokenizer_name,
         )
         assert tokenizer_name in HuggingFaceTokenizer._tokenizers, "Tokenizer should be cached"
-        assert len(tokenizer.encode(TestHuggingFaceTokenizer.TEST_PROMPT)) == expected_num_tokens
+        with wrapped_tokenizer as tokenizer:
+            assert len(tokenizer.encode(TestHuggingFaceTokenizer.TEST_PROMPT)) == expected_num_tokens
 
     def test_get_tokenizer_gpt2(self):
         TestHuggingFaceTokenizer.verify_get_tokenizer("huggingface/gpt2", 51, pretrained_model_name_or_path="gpt2")
@@ -126,7 +127,8 @@ class TestHuggingFaceTokenizer:
 
     def test_gpt2_tokenize_eos(self):
         eos_token: str = "<|endoftext|>"
-        tokenizer = HuggingFaceTokenizer.get_tokenizer("huggingface/gpt2", pretrained_model_name_or_path="gpt2")
-        token_ids = tokenizer.encode(eos_token)
-        assert singleton(token_ids) == 50256
-        assert tokenizer.decode(token_ids) == eos_token
+        wrapped_tokenizer = HuggingFaceTokenizer.get_tokenizer("huggingface/gpt2", pretrained_model_name_or_path="gpt2")
+        with wrapped_tokenizer as tokenizer:
+            token_ids = tokenizer.encode(eos_token)
+            assert singleton(token_ids) == 50256
+            assert tokenizer.decode(token_ids) == eos_token
