6912756319b42e216722dc678ae1fe3f5d2d2155
Yifan Mai <yifan@cs.stanford.edu>
2023-12-06 08:41:58 -0800

    Add retrying to AutoTokenizer (#2090)

--- src/helm/proxy/retry.py
+++ src/helm/proxy/retry.py
@@ -87,3 +87,7 @@ def retry_if_request_failed(result: Union[RequestResult, TokenizationRequestResu
 retry_request: Callable = get_retry_decorator(
     "Request", max_attempts=5, wait_exponential_multiplier_seconds=5, retry_on_result=retry_if_request_failed
 )
+
+retry_tokenizer_request: Callable = get_retry_decorator(
+    "Request", max_attempts=5, wait_exponential_multiplier_seconds=1, retry_on_result=retry_if_request_failed
+)
