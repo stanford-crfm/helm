19608284e86986f3cbda0fb81ec6af845c57c258
Yifan Mai <yifan@cs.stanford.edu>
2023-12-07 20:23:50 -0800

    Add lock to AnthropicTokenizer

--- src/helm/proxy/tokenizers/anthropic_tokenizer.py
+++ src/helm/proxy/tokenizers/anthropic_tokenizer.py
@@ -26,21 +26,24 @@ class AnthropicTokenizer(CachingTokenizer):
     def _tokenize_do_it(self, request: Dict[str, Any]) -> Dict[str, Any]:
         if request["encode"]:
             if request["truncation"]:
-                tokens = self._tokenizer.encode(
-                    request["text"],
-                    truncation=request["truncation"],
-                    max_length=request["max_length"],
-                    add_special_tokens=False,
-                )
+                with AnthropicTokenizer.LOCK:
+                    tokens = self._tokenizer.encode(
+                        request["text"],
+                        truncation=request["truncation"],
+                        max_length=request["max_length"],
+                        add_special_tokens=False,
+                    )
             else:
-                tokens = self._tokenizer.encode(request["text"], add_special_tokens=False)
+                with AnthropicTokenizer.LOCK:
+                    tokens = self._tokenizer.encode(request["text"], add_special_tokens=False)
         else:
             # No encoding, just return the token strings
             tokens = [self._tokenizer.convert_tokens_to_string([i]) for i in self._tokenizer.tokenize(request["text"])]
         return {"tokens": tokens}
 
     def _decode_do_it(self, request: Dict[str, Any]) -> Dict[str, Any]:
-        text = self._tokenizer.decode(
-            request["tokens"], clean_up_tokenization_spaces=request["clean_up_tokenization_spaces"]
-        )
+        with AnthropicTokenizer.LOCK:
+            text = self._tokenizer.decode(
+                request["tokens"], clean_up_tokenization_spaces=request["clean_up_tokenization_spaces"]
+            )
         return {"text": text}
