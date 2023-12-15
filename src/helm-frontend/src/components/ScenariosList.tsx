fe1d543b46064b87849cabacb81cac4d2faf7841
Yifan Mai <yifan@cs.stanford.edu>
2023-12-14 18:17:36 -0800

    Fix scenarios list

--- src/helm-frontend/src/components/ScenariosList.tsx
+++ src/helm-frontend/src/components/ScenariosList.tsx
@@ -6,77 +6,82 @@ interface Props {
 }
 
 export default function ScenariosList({ runGroups }: Props) {
-  const { topLevelGroups, subGroups } = runGroups.reduce(
-    (acc, cur) => {
-      if (cur.category !== undefined) {
-        acc.topLevelGroups.push(cur);
-      } else {
-        acc.subGroups.push(cur);
-      }
-
-      return acc;
-    },
-    { topLevelGroups: [], subGroups: [] } as {
-      topLevelGroups: RunGroup[];
-      subGroups: RunGroup[];
-    },
+  console.log(runGroups);
+  // A run group is a scenario if it has metric groups but no subgroups.
+  const scenariosByName = new Map<string, RunGroup>(
+    runGroups
+      .filter(
+        (runGroup) =>
+          runGroup.metric_groups !== undefined &&
+          (runGroup.subgroups === undefined || runGroup.subgroups.length === 0),
+      )
+      .map((runGroup) => [runGroup.name, runGroup]),
   );
+  console.log(scenariosByName);
+
+  // Only count scenarios that have a category and are displayed
+  // i.e. don't count "orphaned" scenarios
+  // Also, don't double-count scenarios that appear in multiple categories
+  const categorizedScenarioNames = new Set<string>();
+
+  const categoriesWithScenarios: [RunGroup, RunGroup[]][] = [];
+  runGroups.forEach((runGroup) => {
+    const subgroups: string[] = runGroup.subgroups ? runGroup.subgroups : [];
+    const groupScenarios: RunGroup[] = [];
+    subgroups.forEach((subgroup) => {
+      const maybeScenario = scenariosByName.get(subgroup);
+      if (maybeScenario) {
+        groupScenarios.push(maybeScenario);
+        categorizedScenarioNames.add(maybeScenario.name);
+      }
+    });
+    if (groupScenarios.length > 0) {
+      categoriesWithScenarios.push([runGroup, groupScenarios]);
+    }
+  });
 
   return (
     <section>
-      <h3 className="text-3xl">{runGroups.length} scenarios</h3>
+      <h3 className="text-3xl">{categorizedScenarioNames.size} scenarios</h3>
       <ul>
-        {topLevelGroups
-          .filter((topLevelGroup) =>
-            subGroups.some((subGroup) =>
-              (topLevelGroup.subgroups || []).includes(subGroup.name),
-            ),
-          )
-          .map((topLevelGroup, idx) => (
-            <li key={idx} className="my-3">
-              <ReactRouterLink
-                className="text-black"
-                to={"groups/" + topLevelGroup.name}
-              >
-                <h2>{topLevelGroup.display_name}</h2>
-              </ReactRouterLink>
-              <ul className="list-disc list-inside">
-                {subGroups
-                  .filter((subGroup) =>
-                    (topLevelGroup.subgroups || []).includes(subGroup.name),
-                  )
-                  .map((subGroup, idx) =>
-                    subGroup.todo || subGroup.name.includes("CLEVA") ? (
-                      <li
-                        key={idx}
-                        className={`${
-                          subGroup.todo ? "ml-4 text-slate-300" : "ml-4"
-                        }`}
-                      >
-                        {subGroup.display_name}
-                      </li>
-                    ) : (
-                      <ReactRouterLink
-                        className="text-black"
-                        to={"groups/" + subGroup.name}
-                      >
-                        <li
-                          key={idx}
-                          className={`${
-                            subGroup.todo ||
-                            subGroup.display_name.includes("CLEVA")
-                              ? "ml-4 text-slate-300"
-                              : "ml-4"
-                          }`}
-                        >
-                          {subGroup.display_name}
-                        </li>
-                      </ReactRouterLink>
-                    ),
-                  )}
-              </ul>
-            </li>
-          ))}
+        {categoriesWithScenarios.map(([category, scenarios]) => (
+          <li key={category.name} className="my-3">
+            <ReactRouterLink
+              className="text-black"
+              to={"groups/" + category.name}
+            >
+              <h2>{category.display_name}</h2>
+            </ReactRouterLink>
+            <ul className="list-disc list-inside">
+              {scenarios.map((scenario) =>
+                scenario.todo ? (
+                  <li
+                    key={scenario.name}
+                    className={`${
+                      scenario.todo ? "ml-4 text-slate-300" : "ml-4"
+                    }`}
+                  >
+                    {scenario.display_name}
+                  </li>
+                ) : (
+                  <ReactRouterLink
+                    className="text-black"
+                    to={"groups/" + scenario.name}
+                  >
+                    <li
+                      key={scenario.name}
+                      className={`${
+                        scenario.todo ? "ml-4 text-slate-300" : "ml-4"
+                      }`}
+                    >
+                      {scenario.display_name}
+                    </li>
+                  </ReactRouterLink>
+                ),
+              )}
+            </ul>
+          </li>
+        ))}
       </ul>
     </section>
   );
