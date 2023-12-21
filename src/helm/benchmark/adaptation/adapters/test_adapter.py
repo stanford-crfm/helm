e4c8a12b8acb1f0b38be6cc04e52b7632b0d2a18
Yifan Mai <yifan@cs.stanford.edu>
2023-12-20 21:49:51 -0800

    Fix test

--- src/helm/benchmark/adaptation/adapters/test_adapter.py
+++ src/helm/benchmark/adaptation/adapters/test_adapter.py
@@ -1,6 +1,7 @@
 import shutil
 import tempfile
 
+from helm.benchmark.config_registry import register_builtin_configs_from_helm_package
 from helm.common.authentication import Authentication
 from helm.proxy.services.server_service import ServerService
 from helm.benchmark.window_services.tokenizer_service import TokenizerService
@@ -12,6 +13,7 @@ class TestAdapter:
     """
 
     def setup_method(self):
+        register_builtin_configs_from_helm_package()
         self.path: str = tempfile.mkdtemp()
         service = ServerService(base_path=self.path, root_mode=True)
         self.tokenizer_service = TokenizerService(service, Authentication("test"))
