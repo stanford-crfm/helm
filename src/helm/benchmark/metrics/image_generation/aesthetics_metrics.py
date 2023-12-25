95c5026cf3dba9016b2b80247c72441c50fe6b0c
Tony Lee <tonyh.lee@yahoo.com>
2023-12-25 12:39:37 +0900

    aesthetics v2

--- src/helm/benchmark/metrics/image_generation/aesthetics_metrics.py
+++ src/helm/benchmark/metrics/image_generation/aesthetics_metrics.py
@@ -1,6 +1,7 @@
 from statistics import mean
 from typing import List, Optional
 
+from helm.common.gpu_utils import is_cuda_available
 from helm.common.images_utils import is_blacked_out_image
 from helm.common.request import RequestResult
 from helm.benchmark.adaptation.request_state import RequestState
@@ -9,18 +10,18 @@ from helm.benchmark.metrics.statistic import Stat
 from helm.benchmark.metrics.metric import Metric
 from helm.benchmark.metrics.metric_name import MetricName
 from helm.benchmark.metrics.metric_service import MetricService
-from .aesthetics_scorer import AestheticsScorer
+from .aesthetics_scorer import AestheticsScorer, AestheticsScorerV1, AestheticsScorerV2
 from helm.common.multimodal_request_utils import gather_generated_image_locations
 
 
 class AestheticsMetric(Metric):
     """
-    Defines metrics for LAION's CLIP-based aesthetics predictor for images
-    (https://github.com/LAION-AI/aesthetic-predictor).
+    Defines metrics for LAION's CLIP-based aesthetics predictor for images.
     """
 
     def __init__(self):
         self._aesthetics_scorer: Optional[AestheticsScorer] = None
+        self._aesthetics_scorer_v2: Optional[AestheticsScorer] = None
 
     def __repr__(self):
         return "AestheticsMetric()"
@@ -38,17 +39,36 @@ class AestheticsMetric(Metric):
         if len(image_locations) == 0:
             return []
 
-        if self._aesthetics_scorer is None:
-            self._aesthetics_scorer = AestheticsScorer()
-
         # Compute the aesthetics score for each generated image. Skip blacked out images.
-        scores: List[float] = [
-            self._aesthetics_scorer.compute_aesthetics_score(location)
-            for location in image_locations
-            if not is_blacked_out_image(location)
-        ]
+        scores: List[float] = self._compute_aesthetics_scores(image_locations)
         stats: List[Stat] = [
             Stat(MetricName("expected_aesthetics_score")).add(mean(scores) if len(scores) > 0 else 0),
             Stat(MetricName("max_aesthetics_score")).add(max(scores) if len(scores) > 0 else 0),
         ]
+        if is_cuda_available():
+            scores_v2: List[float] = self._compute_aesthetics_scores(image_locations, use_v2=True)
+            stats.extend(
+                [
+                    Stat(MetricName("expected_aesthetics_score_v2")).add(mean(scores_v2) if len(scores_v2) > 0 else 0),
+                    Stat(MetricName("max_aesthetics_score_v2")).add(max(scores_v2) if len(scores_v2) > 0 else 0),
+                ]
+            )
+
         return stats
+
+    def _compute_aesthetics_scores(self, image_locations: List[str], use_v2: bool = False) -> List[float]:
+        scorer: AestheticsScorer
+        if use_v2:
+            if self._aesthetics_scorer_v2 is None:
+                self._aesthetics_scorer_v2 = AestheticsScorerV2()
+            scorer = self._aesthetics_scorer_v2
+        else:
+            if self._aesthetics_scorer is None:
+                self._aesthetics_scorer = AestheticsScorerV1()
+            scorer = self._aesthetics_scorer
+
+        return [
+            scorer.compute_aesthetics_score(location)
+            for location in image_locations
+            if not is_blacked_out_image(location)
+        ]
--- src/helm/benchmark/metrics/image_generation/aesthetics_scorer.py
+++ src/helm/benchmark/metrics/image_generation/aesthetics_scorer.py
@@ -1,16 +1,31 @@
+from abc import ABC, abstractmethod
 from urllib.request import urlretrieve
 import os
 
 import torch
 
 from helm.common.general import ensure_directory_exists
+from helm.common.hierarchical_logger import hlog
 from helm.common.gpu_utils import get_torch_device
 from helm.common.images_utils import open_image
 from helm.common.optional_dependencies import handle_module_not_found_error
 from helm.benchmark.runner import get_cached_models_path
 
 
-class AestheticsScorer:
+class AestheticsScorer(ABC):
+    """
+    Abstract class for aesthetics scorers.
+    """
+
+    @abstractmethod
+    def compute_aesthetics_score(self, image_location: str) -> float:
+        """
+        Compute the aesthetics score. Returns a value between 1 and 10.
+        """
+        pass
+
+
+class AestheticsScorerV1(AestheticsScorer):
     """
     LAION's CLIP-based aesthetics predictor for images (https://github.com/LAION-AI/aesthetic-predictor).
     Adapted from
@@ -29,7 +44,7 @@ class AestheticsScorer:
         model_path: str = os.path.join(cache_folder, f"sa_0_4_{clip_model}_linear.pth")
 
         if not os.path.exists(model_path):
-            model_url: str = os.path.join(AestheticsScorer.MODEL_URL_TEMPLATE.format(clip_model=clip_model))
+            model_url: str = os.path.join(AestheticsScorerV1.MODEL_URL_TEMPLATE.format(clip_model=clip_model))
             urlretrieve(model_url, model_path)
 
         if clip_model == "vit_l_14":
@@ -64,3 +79,107 @@ class AestheticsScorer:
             image_features = self._model.encode_image(image)
             image_features /= image_features.norm(dim=-1, keepdim=True)
             return self._aesthetics_model(image_features.float()).detach().item()
+
+
+class AestheticsScorerV2(AestheticsScorer):
+    """
+    LAION's CLIP-based Aesthetics Predictor v2: https://laion.ai/blog/laion-aesthetics
+
+    Adapted from
+    https://github.com/christophschuhmann/improved-aesthetic-predictor/blob/main/simple_inference.py
+    """
+
+    MODEL_URL: str = (
+        "https://github.com/christophschuhmann/improved-aesthetic-predictor/raw/main/sac+logos+ava1-l14-linearMSE.pth"
+    )
+
+    def __init__(self):
+        """Load the aesthetics model."""
+        import pytorch_lightning as pl
+        import torch.nn as nn
+        import torch.nn.functional as F
+        import clip
+
+        class MLP(pl.LightningModule):
+            def __init__(self, input_size, xcol="emb", ycol="avg_rating"):
+                super().__init__()
+                self.input_size = input_size
+                self.xcol = xcol
+                self.ycol = ycol
+                self.layers = nn.Sequential(
+                    nn.Linear(self.input_size, 1024),
+                    # nn.ReLU(),
+                    nn.Dropout(0.2),
+                    nn.Linear(1024, 128),
+                    # nn.ReLU(),
+                    nn.Dropout(0.2),
+                    nn.Linear(128, 64),
+                    # nn.ReLU(),
+                    nn.Dropout(0.1),
+                    nn.Linear(64, 16),
+                    # nn.ReLU(),
+                    nn.Linear(16, 1),
+                )
+
+            def forward(self, x):
+                return self.layers(x)
+
+            def training_step(self, batch, batch_idx):
+                x = batch[self.xcol]
+                y = batch[self.ycol].reshape(-1, 1)
+                x_hat = self.layers(x)
+                loss = F.mse_loss(x_hat, y)
+                return loss
+
+            def validation_step(self, batch, batch_idx):
+                x = batch[self.xcol]
+                y = batch[self.ycol].reshape(-1, 1)
+                x_hat = self.layers(x)
+                loss = F.mse_loss(x_hat, y)
+                return loss
+
+            def configure_optimizers(self):
+                optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+                return optimizer
+
+        # Load the CLIP and aesthetics model
+        self._device: torch.device = get_torch_device()
+
+        self._model = MLP(768)  # CLIP embedding dim is 768 for CLIP ViT L 14
+
+        cache_folder: str = os.path.join(get_cached_models_path(), "aesthetics_v2")
+        ensure_directory_exists(cache_folder)
+        model_path: str = os.path.join(cache_folder, "sac+logos+ava1-l14-linearMSE.pth")
+        if not os.path.exists(model_path):
+            hlog(f"Downloading the model to {model_path}...")
+            urlretrieve(self.MODEL_URL, model_path)
+
+        s = torch.load(model_path)
+        self._model.load_state_dict(s)
+        self._model.to(self._device)
+        self._model.eval()
+
+        self._model2, self._preprocess = clip.load("ViT-L/14", device=self._device)  # RN50x64
+
+    def compute_aesthetics_score(self, image_location: str) -> float:
+        """
+        Compute the aesthetics score. Returns a value between 1 and 10.
+        """
+        import numpy as np
+
+        def normalized(a, axis=-1, order=2):
+            l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+            l2[l2 == 0] = 1
+            return a / np.expand_dims(l2, axis)
+
+        pil_image = open_image(image_location)
+        image = self._preprocess(pil_image).unsqueeze(0).to(self._device)
+        with torch.no_grad():
+            image_features = self._model2.encode_image(image)
+
+        im_emb_arr = normalized(image_features.cpu().detach().numpy())
+        prediction = self._model(
+            torch.from_numpy(im_emb_arr).to(self._device).type(torch.cuda.FloatTensor)  # type: ignore
+        )
+        hlog(f"location: {image_location} score: {float(prediction)}")
+        return float(prediction)
