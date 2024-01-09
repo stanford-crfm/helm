stanford-crfm/helm

commit 69c583ff2d8a3e071b0808dca8e5be1a991b6245

Call compute_all_general_metrics on all requests, not just the last one (#2172)

src/helm/benchmark/metrics/basic_metrics.py
@@ -848,7 +848,8 @@ class BasicMetric(Metric):
             raise ValueError(f"Unknown adapter method: {adapter_spec.method}")
 
         stats: List[Stat] = []
-        stats.extend(self.compute_all_general_metrics(adapter_spec, request_state, metric_service))
+        for request_state in reference_request_states:
+            stats.extend(self.compute_all_general_metrics(adapter_spec, request_state, metric_service))
 
         max_prob = np.max(scipy.special.softmax(reference_scores))
 

commit 02982a47edd6e14d3e4c2b4ab222a61d24187c60

Fix breakage caused by #2172 (#2194)

src/helm/benchmark/metrics/basic_metrics.py
@@ -243,11 +243,14 @@ class BasicMetric(Metric):
             raise ValueError(f"Unknown adapter method: {adapter_spec.method}")
 
         stats: List[Stat] = []
-        for request_state in reference_request_states:
-            stats.extend(
-                compute_request_state_metrics(self.efficiency_metric, adapter_spec, request_state, metric_service)
-            )
 
+        general_metrics: Dict[MetricName, Stat] = {}
+        for request_state in reference_request_states:
+            for stat in compute_request_state_metrics(
+                self.efficiency_metric, adapter_spec, request_state, metric_service
+            ):
+                merge_stat(general_metrics, stat)
+        stats.extend(general_metrics.values())
         max_prob = np.max(scipy.special.softmax(reference_scores))
 
         # Multiple references may attain the same maximal score; in such cases,

src/helm/benchmark/metrics/metric.py
@@ -288,7 +288,9 @@ class Metric(MetricInterface, ABC):
             for stat in stats:  # go through all the perturbations of the instance and merge relevant stats
                 perturbation = stat.name.perturbation
                 if perturbation is None:
-                    assert original_stat is None  # we should only have one original stat
+                    assert (
+                        original_stat is None
+                    ), f"For {metric_name} got both {original_stat} and {stat}"  # we should only have one original stat
                     original_stat = stat
                 else:
                     if perturbation.robustness: