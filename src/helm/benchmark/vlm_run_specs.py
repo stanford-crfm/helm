e29784d430d3c26e2323eda3c8c6ab081a08a2be
Tony Lee <tonyh.lee@yahoo.com>
2024-01-13 13:44:44 -0800

    Hateful  Memes Scenario (#1992)

--- src/helm/benchmark/vlm_run_specs.py
+++ src/helm/benchmark/vlm_run_specs.py
@@ -43,6 +43,26 @@ def get_vlm_generation_adapter_spec(
 # VHELM run specs
 
 
+@run_spec_function("hateful_memes")
+def get_hateful_memes_spec() -> RunSpec:
+    scenario_spec = ScenarioSpec(
+        class_name="helm.benchmark.scenarios.vision_language.hateful_memes_scenario.HatefulMemesScenario", args={}
+    )
+    adapter_spec: AdapterSpec = get_vlm_generation_adapter_spec(
+        instructions="Answer Yes or No.",
+    )
+    metric_specs: List[MetricSpec] = get_exact_match_metric_specs()
+
+    run_spec_name: str = "hateful_memes"
+    return RunSpec(
+        name=run_spec_name,
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=metric_specs,
+        groups=[run_spec_name],
+    )
+
+
 @run_spec_function("viz_wiz")
 def get_viz_wiz_spec() -> RunSpec:
     scenario_spec = ScenarioSpec(
