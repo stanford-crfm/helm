0792a8400bfb0fa5a04aa09a0e62b11b7a597c29
farzaank <fkaiyom@gmail.com>
2024-01-23 10:12:26 -0800

    finalized component

--- helm-frontend/src/components/ReleaseDropdown.tsx
+++ helm-frontend/src/components/ReleaseDropdown.tsx
@@ -1,9 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import getBenchmarkRelease from "@/utils/getBenchmarkRelease";
+import getReleaseSummary from "@/services/getReleaseSummary";
+import ReleaseSummary from "@/types/ReleaseSummary";
 
 function ReleaseDropdown() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [summary, setSummary] = useState<ReleaseSummary>({
+    release: "",
+    suites: [],
+    date: "",
+  });
   const release = getBenchmarkRelease();
+
+  function reformatDate(date: string): string {
+    const [year, day, month] = date.split("-");
+    const formattedDate = `${day}/${month}/${year.slice(-2)}`;
+    return formattedDate;
+  }
+
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchData() {
+      const summ = await getReleaseSummary(controller.signal);
+      setSummary(summ);
+    }
+
+    void fetchData();
+    return () => controller.abort();
+  }, []);
 
   const accessibleReleases = ["v0.4.0", "v0.3.0", "v0.2.2"]; // this could also read from a config file in the future
 
@@ -15,7 +39,7 @@ function ReleaseDropdown() {
           onClick={() => setDropdownOpen(!dropdownOpen)}
           className="inline-flex items-center justify-center focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
-          <div> Release: {release} </div>
+          <div> Release: {release + " - " + reformatDate(summary.date)} </div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-4 w-4 ml-2"
