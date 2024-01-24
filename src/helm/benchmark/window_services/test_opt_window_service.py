054ddd27ba6e3cda9ffc71cb7ee06606ba94d11d
Yifan Mai <yifan@cs.stanford.edu>
2024-01-24 14:19:47 -0800

    Register configs at the start of test session (#2265)

--- src/helm/benchmark/window_services/test_opt_window_service.py
+++ src/helm/benchmark/window_services/test_opt_window_service.py
@@ -1,7 +1,6 @@
 import shutil
 import tempfile
 
-from helm.benchmark.config_registry import register_builtin_configs_from_helm_package
 from .test_utils import get_tokenizer_service, TEST_PROMPT
 from .tokenizer_service import TokenizerService
 from .window_service_factory import WindowServiceFactory
@@ -9,7 +8,6 @@ from .window_service_factory import WindowServiceFactory
 
 class TestOPTWindowService:
     def setup_method(self):
-        register_builtin_configs_from_helm_package()
         self.path: str = tempfile.mkdtemp()
         service: TokenizerService = get_tokenizer_service(self.path)
         self.window_service = WindowServiceFactory.get_window_service("huggingface/opt-175b", service)
