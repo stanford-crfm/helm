f0bd55df6cd675a3c426c865e0be05376c8134eb
Tony Lee <tonyh.lee@yahoo.com>
2024-02-07 14:54:40 -0800

    test

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -22,7 +22,7 @@ DEFAULT_NLP_RUN: str = "-a helm -c 4 --memory 64g -w /nlp/scr4/nlp/crfm/benchmar
 DEFAULT_NLP_RUN_CPU_ARGS: str = f"{DEFAULT_NLP_RUN} -g 0 --exclude john17"
 # jag 27, 29 started the run, but did nothing. Saw CUDA OOM with 28.
 DEFAULT_NLP_RUN_GPU_ARGS: str = f"{DEFAULT_NLP_RUN} -g 1 --exclude jagupard[18-31]"
-MONGODB_MACHINE: str = "john10"
+MONGODB_MACHINE: str = "john13"
 # -m controls number of instances
 DEFAULT_HELM_ARGS: str = (
     f"--num-train-trials 1 --local -n 1 --mongo-uri='mongodb://crfm-benchmarking:kindling-vespers"
