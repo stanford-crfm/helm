7125dfca7756967477dfe38c3b14ec0740dbade8
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-21 23:34:52 -0800

    Fix Latex Scenario

---
+++ src/helm/benchmark/scenarios/vision_language/image2structure/image2structure_scenario.py
@@ -0,0 +1,120 @@
+import os.path
+from typing import List
+from abc import abstractmethod
+
+from datasets import load_dataset
+from tqdm import tqdm
+
+from helm.benchmark.scenarios.scenario import (
+    CORRECT_TAG,
+    TEST_SPLIT,
+    VALID_SPLIT,
+    Instance,
+    Input,
+    Output,
+    Reference,
+    Scenario,
+)
+from helm.common.media_object import MediaObject, MultimediaObject
+from helm.common.general import ensure_directory_exists
+from helm.common.optional_dependencies import handle_module_not_found_error
+from helm.common.hierarchical_logger import hlog
+
+try:
+    from PIL import Image
+except ModuleNotFoundError as e:
+    handle_module_not_found_error(e, suggestions=["image2structure"])
+
+
+class Image2StructureScenario(Scenario):
+    BASE_PROMPT: str
+    HUGGINGFACE_DATASET_NAME: str
+    SUBSETS: List[str]
+
+    name: str
+    description: str
+    tags = ["vision-language"]
+
+    helm_split_to_huggingface_split = {
+        TEST_SPLIT: "test",
+        VALID_SPLIT: "validation",
+    }
+
+    def __init__(self, subset: str, recompile_prompt: bool = True, split: str = VALID_SPLIT):
+        super().__init__()
+        assert subset in self.SUBSETS, f"Invalid subset: {subset}"
+        self._subset: str = subset
+        self._recompile_prompt: bool = recompile_prompt
+        self._split: str = split
+
+    @abstractmethod
+    def compile(self, structure: str, assets_path: str) -> Image.Image:
+        pass
+
+    def get_instances(self, output_path: str) -> List[Instance]:
+        images_path: str = os.path.join(output_path, "data/images", self._subset)
+        assets_path: str = os.path.join(output_path, "data/assets", self._subset)
+        ensure_directory_exists(images_path)
+
+        instances: List[Instance] = []
+
+        # Process the desired set of instances
+        for row in tqdm(
+            load_dataset(
+                self.HUGGINGFACE_DATASET_NAME,
+                self._subset,
+                split=self.helm_split_to_huggingface_split[self._split],
+                cache_dir=output_path,
+            )
+        ):
+            question_id: str = row["num_id"]
+            if row["category"][1:-1] != self._subset:
+                hlog(
+                    f"Skipping instance {question_id} as it belong in category"
+                    f" {row['category']} and not {self._subset}"
+                )
+                continue
+
+            # Save the image locally
+            image_path: str = os.path.join(images_path, f"{question_id}.png")
+            if not os.path.exists(image_path):
+                if not self._recompile_prompt:
+                    row["image"].save(image_path)
+                else:
+                    structure: str = row["structure"]
+                    image: Image.Image = self.compile(structure, assets_path)
+                    image.save(image_path)
+
+            # Create the multimedia content
+            prompt: str = self.BASE_PROMPT
+            image_object = MediaObject(location=image_path, content_type="image/png")
+            content: List[MediaObject] = [
+                MediaObject(text=prompt, content_type="text/plain"),
+                image_object,
+            ]
+
+            # Create the reference
+            reference: Reference = Reference(
+                output=Output(
+                    text=row["structure"],
+                    multimedia_content=MultimediaObject(
+                        [
+                            image_object,
+                            # TODO: Change this once we have a better way to pass the assets_path
+                            # to the evaluation
+                            MediaObject(text=f"assets_path={assets_path}", content_type="text/plain"),
+                        ]
+                    ),
+                ),
+                tags=[CORRECT_TAG],  # TODO: Add assets
+            )
+
+            # Finalize the Instance
+            instances.append(
+                Instance(
+                    input=Input(multimedia_content=MultimediaObject(content)), references=[reference], split=self._split
+                )
+            )
+
+        assert len(instances) > 0, f"No instances found for subject {self._subset}"
+        return instances
