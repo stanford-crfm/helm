f4e8072d3a9e47dd20da89c95b2022f8791e68ef
farzaank <fkaiyom@gmail.com>
2024-02-28 21:39:56 -0800

    comments for consideration

--- helm-frontend/src/routes/Home.tsx
+++ helm-frontend/src/routes/Home.tsx
@@ -2,11 +2,13 @@ import InstructLanding from "@/components/InstructLanding";
 import LiteLanding from "@/components/LiteLanding";
 
 export default function Home() {
+  // TODO consider a more streamlined way to do this?
   if (window.RELEASE_INDEX_ID === "lite") {
     return <LiteLanding />;
   } else if (window.RELEASE_INDEX_ID === "instruct") {
     return <InstructLanding />;
   } else {
+    // TODO: better global/default landing page
     return <LiteLanding />;
   }
 }
