cd8d3a9697ce2a81c1cb0e388a4c2407afe0e428
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-03-04 14:46:02 -0800

    Changing image metrics to run in parallel

--- src/helm/benchmark/metrics/vision_language/image_metrics.py
+++ src/helm/benchmark/metrics/vision_language/image_metrics.py
@@ -1,13 +1,14 @@
 from typing import List, Dict, Optional
 import numpy as np
 from torchvision import transforms, models
-from tqdm import tqdm
 from skimage.metrics import structural_similarity as ssim
 import os
 import torch
 import warnings
 
-from helm.benchmark.metrics.evaluate_instances_metric import EvaluateInstancesMetric
+from helm.benchmark.metrics.metric import Metric
+from helm.benchmark.metrics.metric_service import MetricService
+from helm.benchmark.adaptation.adapter_spec import AdapterSpec
 from helm.common.images_utils import open_image
 from helm.common.gpu_utils import get_torch_device
 from helm.common.cache import Cache, SqliteCacheConfig
@@ -15,9 +16,14 @@ from helm.benchmark.adaptation.request_state import RequestState
 from helm.common.media_object import MediaObject
 from helm.common.optional_dependencies import handle_module_not_found_error
 from helm.common.hierarchical_logger import hlog
-from ..metric_name import MetricName
-from ..statistic import Stat
-from .image_utils import preprocess_image, earth_mover_similarity, pixel_similarity, sift_similarity
+from helm.benchmark.metrics.metric_name import MetricName
+from helm.benchmark.metrics.statistic import Stat
+from helm.benchmark.metrics.vision_language.image_utils import (
+    preprocess_image,
+    earth_mover_similarity,
+    pixel_similarity,
+    sift_similarity,
+)
 
 try:
     from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
@@ -30,7 +36,7 @@ class CompilationError(Exception):
     pass
 
 
-class AnnotatedImageMetrics(EvaluateInstancesMetric):
+class AnnotatedImageMetrics(Metric):
     """Abstract class for image metrics.
 
     This class is designed to evaluate metrics on images that should be generated using the text
@@ -88,14 +94,19 @@ class AnnotatedImageMetrics(EvaluateInstancesMetric):
             "completion": completion,
         }
 
-    def evaluate_instances(self, request_states: List[RequestState], eval_cache_path: str) -> List[Stat]:
+    def evaluate_generation(
+        self,
+        adapter_spec: AdapterSpec,
+        request_state: RequestState,
+        metric_service: MetricService,
+        eval_cache_path: str,
+    ) -> List[Stat]:
         if self._cache is None:
             self._cache = self._get_cache(eval_cache_path)
 
         # TODO: Remove this debugging saving
-        assert len(request_states) > 0
         debug_save_path = os.path.join(
-            eval_cache_path, f"debug/{self.generation_type}/{request_states[0].request.model_deployment}"
+            eval_cache_path, f"debug/{self.generation_type}/{request_state.request.model_deployment}"
         )
         os.makedirs(debug_save_path, exist_ok=True)
         # count the number of files in the directory
@@ -105,144 +116,143 @@ class AnnotatedImageMetrics(EvaluateInstancesMetric):
             name: Stat(MetricName(name)) for name in (self._metric_names + [self.COMPILE_METRIC])
         }
 
-        for request_state in tqdm(request_states, desc="Evaluating images"):
-            if (
-                request_state.annotations is None
-                or request_state.result is None
-                or len(request_state.annotations) != len(request_state.result.completions)
-            ):
-                raise ValueError(
-                    "Annotations and results should be present and have the same length.",
-                    " Please make sure to add a compiler annotator to the run spec.",
-                )
-            reference = request_state.instance.references[0]
-            assert reference.output.multimedia_content is not None
-            assert len(reference.output.multimedia_content.media_objects) > 0
-            ref_media_object: MediaObject = reference.output.multimedia_content.media_objects[0]
-            assert ref_media_object.type == "image"
-            ref_image: Image.Image
-            rgb_ref_image: np.ndarray
-            gray_ref_image: np.ndarray
-            if ref_media_object.is_local_file and ref_media_object.location is not None:
-                ref_image = open_image(ref_media_object.location)
-                rgb_ref_image = np.array(ref_image)
-                gray_ref_image = preprocess_image(ref_image)
-            else:
-                raise Exception(
-                    "Remote images are not supported in metrics. "
-                    "Images should be downloaded when constructing the instance."
-                )
-            white_image: Optional[Image.Image] = None
-            rgb_white_image: Optional[np.ndarray] = None
-            gray_white_image: Optional[np.ndarray] = None
-            if self._normalize_by_white_score:
-                white_image = Image.new("RGB", ref_image.size, (255, 255, 255))
-                rgb_white_image = np.array(white_image)
-                gray_white_image = preprocess_image(white_image)
+        if (
+            request_state.annotations is None
+            or request_state.result is None
+            or len(request_state.annotations) != len(request_state.result.completions)
+        ):
+            raise ValueError(
+                "Annotations and results should be present and have the same length.",
+                " Please make sure to add a compiler annotator to the run spec.",
+            )
+        reference = request_state.instance.references[0]
+        assert reference.output.multimedia_content is not None
+        assert len(reference.output.multimedia_content.media_objects) > 0
+        ref_media_object: MediaObject = reference.output.multimedia_content.media_objects[0]
+        assert ref_media_object.type == "image"
+        ref_image: Image.Image
+        rgb_ref_image: np.ndarray
+        gray_ref_image: np.ndarray
+        if ref_media_object.is_local_file and ref_media_object.location is not None:
+            ref_image = open_image(ref_media_object.location)
+            rgb_ref_image = np.array(ref_image)
+            gray_ref_image = preprocess_image(ref_image)
+        else:
+            raise Exception(
+                "Remote images are not supported in metrics. "
+                "Images should be downloaded when constructing the instance."
+            )
+        white_image: Optional[Image.Image] = None
+        rgb_white_image: Optional[np.ndarray] = None
+        gray_white_image: Optional[np.ndarray] = None
+        if self._normalize_by_white_score:
+            white_image = Image.new("RGB", ref_image.size, (255, 255, 255))
+            rgb_white_image = np.array(white_image)
+            gray_white_image = preprocess_image(white_image)
 
-            assert request_state.result is not None
-            assert len(request_state.annotations) == len(request_state.result.completions)
-            for annotation in request_state.annotations:
-                if "error" in annotation:
-                    stats_dict[self.COMPILE_METRIC].add(0)  # Did not compile
-                    # For all other metrics, we set the value to zero
-                    for metric_name in self._metric_names:
-                        stats_dict[metric_name].add(0)
+        assert request_state.result is not None
+        assert len(request_state.annotations) == len(request_state.result.completions)
+        for annotation in request_state.annotations:
+            if "error" in annotation:
+                stats_dict[self.COMPILE_METRIC].add(0)  # Did not compile
+                # For all other metrics, we set the value to zero
+                for metric_name in self._metric_names:
+                    stats_dict[metric_name].add(0)
+                continue
+
+            assert "media_object" in annotation, "No media object in the annotation"
+            assert isinstance(annotation["media_object"], MediaObject)
+            media_object: MediaObject = annotation["media_object"]
+            assert media_object.type == "image"
+            assert media_object.is_local_file and media_object.location is not None
+            image: Image.Image = Image.open(media_object.location).convert("RGB")
+
+            # TODO: Remove this debugging saving
+            image.save(os.path.join(debug_save_path, f"{save_id}_pred.png"))
+            ref_image.save(os.path.join(debug_save_path, f"{save_id}_ref.png"))
+
+            # Handle difference in size
+            if image.size != ref_image.size:
+                if self._size_handling_method == "none":
+                    raise ValueError(
+                        "Compiled image and reference image should have the same size"
+                        " when the size handling method is none."
+                    )
+                elif self._size_handling_method == "resize":
+                    image = image.resize(ref_image.size)
+                else:
+                    raise ValueError(f"size handling method {self._size_handling_method} not recognized.")
+            assert image.size == ref_image.size
+
+            rgb_image: np.ndarray = np.array(image)
+            gray_image: np.ndarray = preprocess_image(image)
+
+            # List of metrics and arguments to evaluate
+            # The arguments are as follows:
+            # 1. Name of the metric
+            # 2. Function to compute the metric
+            # 3. The generated image obhect (type can depend on the metric)
+            # 4. The reference image object (type can depend on the metric)
+            # 5. The white image object (type can depend on the metric) - may be used to normalize the metric
+            # 6. Whether the metric can be computed on the white image - if not, the metric is not normalized
+            metric_runs: list = [
+                [self.PIXEL_SIMILARITY, pixel_similarity, gray_image, gray_ref_image, gray_white_image, True],
+                [self.SIFT_SIMILARITY, sift_similarity, rgb_image, rgb_ref_image, rgb_white_image, False],
+                [
+                    self.EARTH_MOVER_SIMILARITY,
+                    earth_mover_similarity,
+                    gray_image,
+                    gray_ref_image,
+                    gray_white_image,
+                    True,
+                ],
+                [self.LPIPS_SIMILARITY, self.lpips_similarity, image, ref_image, white_image, True],
+                [self.FID_SIMILARITY, self.fid_similarity, image, ref_image, white_image, True],
+                [self.SSIM_SIMILARITY, self.compute_ssim, gray_image, gray_ref_image, gray_white_image, False],
+            ]
+
+            for metric_name, metric_fn, image1, image2, white_image, can_compute_on_white in metric_runs:
+                if metric_name not in self._metric_names:
                     continue
 
-                assert "media_object" in annotation, "No media object in the annotation"
-                assert isinstance(annotation["media_object"], MediaObject)
-                media_object: MediaObject = annotation["media_object"]
-                assert media_object.type == "image"
-                assert media_object.is_local_file and media_object.location is not None
-                image: Image.Image = Image.open(media_object.location).convert("RGB")
+                def do_it():
+                    try:
+                        value = metric_fn(image1, image2)
+                        if self._normalize_by_white_score and can_compute_on_white:
+                            assert white_image is not None
+                            value_white: float = metric_fn(image2, white_image)
+                            value = (value - value_white) / (1.0 - value_white)
+                        return {"value": value}
+                    except Exception as e:
+                        return {"error": str(e)}
 
-                # TODO: Remove this debugging saving
-                image.save(os.path.join(debug_save_path, f"{save_id}_pred.png"))
-                ref_image.save(os.path.join(debug_save_path, f"{save_id}_ref.png"))
-
-                # Handle difference in size
-                if image.size != ref_image.size:
-                    if self._size_handling_method == "none":
-                        raise ValueError(
-                            "Compiled image and reference image should have the same size"
-                            " when the size handling method is none."
-                        )
-                    elif self._size_handling_method == "resize":
-                        image = image.resize(ref_image.size)
-                    else:
-                        raise ValueError(f"size handling method {self._size_handling_method} not recognized.")
-                assert image.size == ref_image.size
-
-                rgb_image: np.ndarray = np.array(image)
-                gray_image: np.ndarray = preprocess_image(image)
-
-                # List of metrics and arguments to evaluate
-                # The arguments are as follows:
-                # 1. Name of the metric
-                # 2. Function to compute the metric
-                # 3. The generated image obhect (type can depend on the metric)
-                # 4. The reference image object (type can depend on the metric)
-                # 5. The white image object (type can depend on the metric) - may be used to normalize the metric
-                # 6. Whether the metric can be computed on the white image - if not, the metric is not normalized
-                metric_runs: list = [
-                    [self.PIXEL_SIMILARITY, pixel_similarity, gray_image, gray_ref_image, gray_white_image, True],
-                    [self.SIFT_SIMILARITY, sift_similarity, rgb_image, rgb_ref_image, rgb_white_image, False],
-                    [
-                        self.EARTH_MOVER_SIMILARITY,
-                        earth_mover_similarity,
-                        gray_image,
-                        gray_ref_image,
-                        gray_white_image,
-                        True,
-                    ],
-                    [self.LPIPS_SIMILARITY, self.lpips_similarity, image, ref_image, white_image, True],
-                    [self.FID_SIMILARITY, self.fid_similarity, image, ref_image, white_image, True],
-                    [self.SSIM_SIMILARITY, self.compute_ssim, gray_image, gray_ref_image, gray_white_image, False],
-                ]
-
-                for metric_name, metric_fn, image1, image2, white_image, can_compute_on_white in metric_runs:
-                    if metric_name not in self._metric_names:
-                        continue
-
-                    def do_it():
-                        try:
-                            value = metric_fn(image1, image2)
-                            if self._normalize_by_white_score and can_compute_on_white:
-                                assert white_image is not None
-                                value_white: float = metric_fn(image2, white_image)
-                                value = (value - value_white) / (1.0 - value_white)
-                            return {"value": value}
-                        except Exception as e:
-                            return {"error": str(e)}
-
-                    response_metric, _ = self._cache.get(
-                        {
-                            "metric_name": metric_name,
-                            "ref_image_path": ref_media_object.location,
-                            "generated_image_path": media_object.location,
-                            "normalize_by_white_score": self._normalize_by_white_score,
-                        },
-                        do_it,
-                    )
-                    value: float
-                    if "error" in response_metric:
-                        hlog(f"Error in metric {metric_name}: {response_metric['error']}")
-                        value = 0
-                    else:
-                        value = response_metric["value"]
-                    stats_dict[metric_name].add(value)
-
-                    # TODO: Remove this debugging saving
-                    # Save metric values in a txt file
-                    with open(os.path.join(debug_save_path, f"{save_id}_metrics.txt"), "a") as f:
-                        f.write(f"{metric_name}: {value}\n")
-
-                stats_dict[self.COMPILE_METRIC].add(1)  # Compiled
+                response_metric, _ = self._cache.get(
+                    {
+                        "metric_name": metric_name,
+                        "ref_image_path": ref_media_object.location,
+                        "generated_image_path": media_object.location,
+                        "normalize_by_white_score": self._normalize_by_white_score,
+                    },
+                    do_it,
+                )
+                value: float
+                if "error" in response_metric:
+                    hlog(f"Error in metric {metric_name}: {response_metric['error']}")
+                    value = 0
+                else:
+                    value = response_metric["value"]
+                stats_dict[metric_name].add(value)
 
                 # TODO: Remove this debugging saving
                 # Save metric values in a txt file
-                save_id += 1
+                with open(os.path.join(debug_save_path, f"{save_id}_metrics.txt"), "a") as f:
+                    f.write(f"{metric_name}: {value}\n")
+
+            stats_dict[self.COMPILE_METRIC].add(1)  # Compiled
+
+            # TODO: Remove this debugging saving
+            # Save metric values in a txt file
+            save_id += 1
 
         return list(stats_dict.values())
 
