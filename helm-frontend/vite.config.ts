6f325b63869c255de38fc7d21495e88688f0e60b
Yifan Mai <yifan@cs.stanford.edu>
2024-03-12 13:22:50 -0700

    Bleagh

--- helm-frontend/vite.config.ts
+++ helm-frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, ViteDevServer } from "vite";
 import serveStatic from "serve-static";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 const ServeBenchmarkOutputPlugin = {
   name: 'serve-benchmark-output-plugin',
-  configureServer(server) {
+  configureServer(server: ViteDevServer) {
     server.middlewares.use(
       "/benchmark_output",
       // TODO: Make the path configurable via environment variable
@@ -41,36 +41,3 @@ export default defineConfig({
     }
   },
 });
-
-
-// import { defineConfig } from "vitest/config";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// const ServeBenchmarkOutputPlugin = {
-//   name: 'serve-benchmark-output-plugin',
-//   configureServer(server) {
-//     server.middlewares.use(
-//       "/benchmark_output",
-//       // TODO: Make the path configurable via environment variable
-//       serveStatic("../../benchmark_output", {fallthrough: false, index: false})
-//     )
-//   }
-// }
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), ServeBenchmarkOutputPlugin],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   test: {
-//     globals: true,
-//     environment: "jsdom",
-//   },
-//   build: {
-//     outDir: `${__dirname}/../helm/benchmark/static_build`,
-//   },
-// });
