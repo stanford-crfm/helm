fe47fd08c7cdbc796f5a24f66131e8366c9711c5
Yifan Mai <yifan@cs.stanford.edu>
2024-03-13 12:42:05 -0700

    Add benchmark_output serving to the vite config (#2454)

--- helm-frontend/vite.config.ts
+++ helm-frontend/vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vitest/config";
+import { ViteDevServer } from "vite";
+import serveStatic from "serve-static";
 import react from "@vitejs/plugin-react";
 import path from "path";
+
+const ServeBenchmarkOutputPlugin = {
+  name: 'serve-benchmark-output-plugin',
+  configureServer(server: ViteDevServer) {
+    server.middlewares.use(
+      "/benchmark_output",
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
+      serveStatic("../benchmark_output", {fallthrough: false, index: false})
+    )
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react()],
+  plugins: [react(), ServeBenchmarkOutputPlugin],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
