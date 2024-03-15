4e9d006ad1431f3d5b9732172c79adbc0224685b
farzaank <fkaiyom@gmail.com>
2024-03-14 18:06:46 -0700

    revert config

--- helm-frontend/public/config.js
+++ helm-frontend/public/config.js
@@ -1,5 +1,5 @@
-window.SUITE = "latest";
-window.RELEASE = null;
+window.SUITE = null;
+window.RELEASE = "v1.0.0";
 window.BENCHMARK_OUTPUT_BASE_URL =
-	"https://nlp.stanford.edu/helm/vhelm/benchmark_output";
+	"https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/";
 window.RELEASE_INDEX_ID = "lite";
