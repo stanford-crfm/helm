68e5b7ad80fb15f7608a1aa6625dec9c279261ad
farzaank <fkaiyom@gmail.com>
2024-03-15 10:19:44 -0700

    clean up

--- helm-frontend/src/components/InstanceData.tsx
+++ helm-frontend/src/components/InstanceData.tsx
@@ -21,31 +21,29 @@ export default function InstanceData({
 }: Props) {
   return (
     <div className="border p-4">
-      <div>
-        <h3 className="text-xl mb-4">
-          {`Instance id: ${instance.id} [split:  ${instance.split}]`}
-        </h3>
-        <h3>Input</h3>
-        {instance.input.text.includes("<img") ? (
-          <div dangerouslySetInnerHTML={{ __html: instance.input.text }} />
-        ) : (
-          <Preview value={instance.input.text} />
-        )}
+      <h3 className="text-xl mb-4">
+        {`Instance id: ${instance.id} [split:  ${instance.split}]`}
+      </h3>
+      <h3>Input</h3>
+      {instance.input.text.includes(`<br><img src="data:image;base64`) ? (
+        <div dangerouslySetInnerHTML={{ __html: instance.input.text }} />
+      ) : (
+        <Preview value={instance.input.text} />
+      )}
 
-        <div>
-          {instance.references && instance.references.length > 0 ? (
-            <References references={instance.references} />
-          ) : null}
-        </div>
-        <div>
-          {predictions && requests ? (
-            <Predictions
-              predictions={predictions}
-              requests={requests}
-              metricFieldMap={metricFieldMap}
-            />
-          ) : null}
-        </div>
+      <div>
+        {instance.references && instance.references.length > 0 ? (
+          <References references={instance.references} />
+        ) : null}
+      </div>
+      <div>
+        {predictions && requests ? (
+          <Predictions
+            predictions={predictions}
+            requests={requests}
+            metricFieldMap={metricFieldMap}
+          />
+        ) : null}
       </div>
     </div>
   );
