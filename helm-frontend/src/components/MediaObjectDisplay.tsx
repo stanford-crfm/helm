5a31dfd83a6931b24f7c3d7d17ee347bb2a8ef3f
Yifan Mai <yifan@cs.stanford.edu>
2024-03-15 18:38:10 -0700

    Add todos

--- helm-frontend/src/components/MediaObjectDisplay.tsx
+++ helm-frontend/src/components/MediaObjectDisplay.tsx
@@ -5,6 +5,10 @@ interface Props {
 }
 
 export default function MediaObjectDisplay({ mediaObject }: Props) {
+  // TODO: Actually render the object.
+  // NOTE: If mediaObject.location has the prefix "benchmark_output/",
+  // this prefix should be stripped before appending the location to
+  // `window.BENCHMARK_OUTPUT_BASE_URL`.
   return (
     <div>
       content_type: {mediaObject.content_type}, text:{" "}
