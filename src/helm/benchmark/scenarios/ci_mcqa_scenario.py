138699ad9bbb5de855b92e8467e921e4e4931357
Murtaza Ali <murtaza5152.ali@gmail.com>
2024-03-17 05:08:21 -0700

    pushing minor changes to cenario

--- src/helm/benchmark/scenarios/ci_mcqa_scenario.py
+++ src/helm/benchmark/scenarios/ci_mcqa_scenario.py
@@ -2,7 +2,7 @@ import json
 import os
 from typing import List
 
-from .scenario import Scenario, Instance, Reference, CORRECT_TAG, VALID_SPLIT, Input, Output
+from .scenario import Scenario, Instance, Reference, CORRECT_TAG, TRAIN_SPLIT, VALID_SPLIT, TEST_SPLIT, Input, Output
 
 
 class CIMCQAScenario(Scenario):
@@ -19,7 +19,7 @@ class CIMCQAScenario(Scenario):
     tags = ["question_answering"]
 
     def get_instances(self, output_path: str) -> List[Instance]:
-        data_path: str = os.path.join("restricted", "scs1.json")
+        data_path: str = os.path.join("restricted", "bdsi_multiple_answers_removed.json")
         assert os.path.exists(data_path)
 
         with open(data_path, "r", encoding="utf8") as f:
@@ -29,25 +29,25 @@ class CIMCQAScenario(Scenario):
         instances: List[Instance] = list()
 
         # UNCOMMENT BELOW FOR FEW-SHOT RUN
-        # training_data_path: str = os.path.join("restricted", "mock_scs1.json")
-        # assert os.path.exists(training_data_path)
+        training_data_path: str = os.path.join("restricted", "mock_bdsi_multiple_answers_removed.json")
+        assert os.path.exists(training_data_path)
 
-        # with open(training_data_path, "r", encoding="utf8") as f:
-        #     training_data = json.load(f)
-        # for question in training_data:
-        #     question_text = question['question']
-        #     references = list()
-        #     for index, answer in enumerate(question['options']):
-        #         reference_answer = Output(text=answer)
-        #         # Correct option offset by 1 due to zero-indexing
-        #         tag = [CORRECT_TAG] if index == question['correct_option'] - 1 else []
-        #         references.append(Reference(reference_answer, tags=tag))
-        #     instance: Instance = Instance(
-        #         input=Input(text=question_text),
-        #         references=references,
-        #         split=TRAIN_SPLIT,
-        #     )
-        #     instances.append(instance)
+        with open(training_data_path, "r", encoding="utf8") as f:
+            training_data = json.load(f)
+        for question in training_data:
+            question_text = question['question']
+            references = list()
+            for index, answer in enumerate(question['options']):
+                reference_answer = Output(text=answer)
+                # Correct option offset by 1 due to zero-indexing
+                tag = [CORRECT_TAG] if index == question['correct_option'] - 1 else []
+                references.append(Reference(reference_answer, tags=tag))
+            instance: Instance = Instance(
+                input=Input(text=question_text),
+                references=references,
+                split=TRAIN_SPLIT,
+            )
+            instances.append(instance)
 
         for question in data:
             question_text = question["question"]
@@ -60,8 +60,7 @@ class CIMCQAScenario(Scenario):
             instance: Instance = Instance(
                 input=Input(text=question_text),
                 references=references,
-                split=VALID_SPLIT,  # Just doing zero shot to start
+                split=TEST_SPLIT,  # Just doing zero shot to start
             )
             instances.append(instance)
-
         return instances
