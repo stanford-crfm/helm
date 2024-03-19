64b2c629d12a6e94514885a3f5d6143d80d8e349
farzaank <fkaiyom@gmail.com>
2024-03-18 18:19:55 -0700

    fix format

--- helm-frontend/src/components/MediaObjectDisplay.tsx
+++ helm-frontend/src/components/MediaObjectDisplay.tsx
@@ -2,36 +2,36 @@ import MediaObject from "@/types/MediaObject";
 import getBenchmarkEndpoint from "@/utils/getBenchmarkEndpoint";
 
 interface Props {
-	mediaObject: MediaObject;
+  mediaObject: MediaObject;
 }
 
 export default function MediaObjectDisplay({ mediaObject }: Props) {
-	if (mediaObject.content_type.includes("image")) {
-		const url = getBenchmarkEndpoint(
-			String(mediaObject.location?.replace("benchmark_output/", ""))
-		);
-		return (
-			<div>
-				<img src={url}></img>
-				<br />
-			</div>
-		);
-	} else {
-		if (
-			mediaObject.text &&
-			mediaObject.content_type &&
-			mediaObject.content_type === "text/plain" &&
-			mediaObject.text.length > 1
-		) {
-			return (
-				<div>
-					{mediaObject.text}
-					<br />
-					<br />
-				</div>
-			);
-		} else {
-			return <div></div>;
-		}
-	}
+  if (mediaObject.content_type.includes("image")) {
+    const url = getBenchmarkEndpoint(
+      String(mediaObject.location?.replace("benchmark_output/", "")),
+    );
+    return (
+      <div>
+        <img src={url}></img>
+        <br />
+      </div>
+    );
+  } else {
+    if (
+      mediaObject.text &&
+      mediaObject.content_type &&
+      mediaObject.content_type === "text/plain" &&
+      mediaObject.text.length > 1
+    ) {
+      return (
+        <div>
+          {mediaObject.text}
+          <br />
+          <br />
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
 }
