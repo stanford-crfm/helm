f65e87f4c3b9c7f4b7cc5e2b14c46bf390672804
Yifan Mai <yifan@cs.stanford.edu>
2024-03-19 09:34:51 -0700

    Simplify SimpleClient (#2475)

--- src/helm/clients/simple_client.py
+++ src/helm/clients/simple_client.py
@@ -1,75 +1,64 @@
-from typing import List, Dict
+import itertools
+from typing import List, TypedDict
+from typing import Dict, Any
 
 from helm.common.cache import CacheConfig
 from helm.common.request import wrap_request_time, Request, RequestResult, Sequence, Token
-from helm.tokenizers.simple_tokenizer import SimpleTokenizer
-from .client import CachingClient
+from helm.clients.client import CachingClient
+
+
+class SimpleClientRequest(TypedDict):
+    engine: str
+    prompt: str
+    num_completions: int
 
 
 class SimpleClient(CachingClient):
-    """Implements some "models" that just generate silly things quickly just to debug the infrastructure."""
+    """Simple client for tutorials and for debugging."""
 
     def __init__(self, cache_config: CacheConfig):
         super().__init__(cache_config=cache_config)
 
     def make_request(self, request: Request) -> RequestResult:
-        raw_request = {
+        raw_request: SimpleClientRequest = {
             "engine": request.model_engine,
             "prompt": request.prompt,
-            "n": request.num_completions,
+            "num_completions": request.num_completions,
         }
 
-        if request.model_engine in ["model1", "tutorial"]:
+        def do_it() -> Dict[str, Any]:
+            return self.invoke_model(raw_request)
 
-            def do_it():
-                if request.model_engine == "model1":
-                    return self.invoke_model1(raw_request)
-                elif request.model_engine == "tutorial":
-                    return self.invoke_model_tutorial(raw_request)
-
-            cache_key = CachingClient.make_cache_key(raw_request, request)
-            response, cached = self.cache.get(cache_key, wrap_request_time(do_it))
-            completions = [
-                Sequence(
-                    text=text,
-                    logprob=logprob,
-                    tokens=[Token(text=text, logprob=logprob)],
-                )
-                for text, logprob in response["completions"].items()
-            ]
-        else:
-            raise ValueError(f"Invalid model: {request.model}")
+        cache_key = CachingClient.make_cache_key(raw_request, request)
+        response, cached = self.cache.get(cache_key, wrap_request_time(do_it))
+        logprob = 0
+        completions = [
+            Sequence(
+                text=text,
+                logprob=logprob,
+                tokens=[Token(text=text, logprob=logprob)],
+            )
+            for text in response["completions"]
+        ]
 
         return RequestResult(
             success=True,
-            cached=False,
-            request_time=0,
+            cached=cached,
+            request_time=response["request_time"],
             request_datetime=response.get("request_datetime"),
             completions=completions,
             embedding=[],
         )
 
-    def invoke_model1(self, raw_request: Dict) -> Dict:
+    def invoke_model(self, raw_request: SimpleClientRequest) -> Dict[str, Any]:
         """
-        Example: 7 2 4 6
+        Example:
+        Prompt: 7 2 4 6
         Completions (num_completions = 3):
         - 6
         - 4
         - 2
         """
-        prompt_tokens: List[str] = SimpleTokenizer.tokenize_by_space(raw_request["prompt"])
-        choices = reversed(prompt_tokens[-raw_request["n"] :])
-        response = {"completions": dict((text, -i) for i, text in enumerate(choices))}
-        return response
-
-    def invoke_model_tutorial(self, raw_request: Dict) -> Dict:
-        """Always returns: 'The model is generating some text. Hooray, the tutorial works! (Completion {i})'.
-        This supports multiple completions.
-        """
-        response = {
-            "completions": dict(
-                (f"The model is generating some text. Hooray, the tutorial works! (Completion {i})", -i)
-                for i in range(raw_request["n"])
-            )
-        }
-        return response
+        prompt_words: List[str] = raw_request["prompt"].split()
+        completions = list(itertools.islice(itertools.cycle(reversed(prompt_words)), raw_request["num_completions"]))
+        return {"completions": completions}
