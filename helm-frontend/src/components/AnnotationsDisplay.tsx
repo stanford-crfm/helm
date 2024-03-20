e3e31dc409967026397df7fbbb57813582fd85ad
farzaank <fkaiyom@gmail.com>
2024-03-19 20:14:02 -0700

    necessary changes

--- helm-frontend/src/components/AnnotationsDisplay.tsx
+++ helm-frontend/src/components/AnnotationsDisplay.tsx
@@ -1,5 +1,6 @@
 import CompletionAnnotation from "@/types/CompletionAnnotation";
 import Preview from "./Preview";
+import MediaObjectDisplay from "./MediaObjectDisplay";
 
 type Props = {
   predictionAnnotations:
@@ -7,14 +8,25 @@ type Props = {
     | undefined;
 };
 
-export default function AnnotationDsisplay({ predictionAnnotations }: Props) {
+export default function AnnotationDisplay({ predictionAnnotations }: Props) {
   return (
     <div>
       {predictionAnnotations && predictionAnnotations !== undefined
         ? Object.entries(predictionAnnotations).map(([key, value]) => (
             <div key={key}>
               <h3>{key}</h3>
-              <Preview value={JSON.stringify(value[0])} />
+              {value.map((annotation, idx) => (
+                <div key={idx}>
+                  {annotation.error && <Preview value={annotation["error"]} />}
+                  {annotation.text && <Preview value={annotation["text"]} />}
+                  {/* TODO remove this impossible condition, once Yifan's PR is merged. */}
+                  {annotation.media_object && !annotation.media_object && (
+                    <MediaObjectDisplay
+                      mediaObject={annotation["media_object"]}
+                    />
+                  )}
+                </div>
+              ))}
             </div>
           ))
         : null}
