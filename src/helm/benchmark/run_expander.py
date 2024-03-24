b75b5517dc7b58b888185691e99f6dd9d7f539dc
Tony Lee <tonyh.lee@yahoo.com>
2024-03-24 02:11:44 -0700

    update instructions plus more models

--- src/helm/benchmark/run_expander.py
+++ src/helm/benchmark/run_expander.py
@@ -513,7 +513,7 @@ class MaxTrainInstancesRunExpander(ReplaceValueRunExpander):
         "one": [1],
         "all": [0, 1, 2, 4, 8, 16],  # Cap at 16 due to limited context length
         "big_bench_few_shot_setting": [0, 1, 2, 3],  # Commonly used few-shot setting in BIG-bench
-        "vhelm": [0, 1, 2, 4, 8],
+        "vhelm": [1, 2, 4, 8],
     }
 
 
