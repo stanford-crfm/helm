2dba53ffd5da512719343120270334ce901ab74b
Yifan Mai <yifan@cs.stanford.edu>
2024-03-26 10:10:15 -0700

    Fixes

--- helm-frontend/public/config.js
+++ helm-frontend/public/config.js
@@ -1,4 +1,4 @@
-window.SUITE = "v1.0.0";
+window.RELEASE = "v1.0.0";
 window.BENCHMARK_OUTPUT_BASE_URL =
-  "https://storage.googleapis.com/crfm-helm-public/vhelm/benchmark_output/";
-window.PROJECT_ID = "vhelm";
+  "https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/";
+window.PROJECT_ID = "lite";
