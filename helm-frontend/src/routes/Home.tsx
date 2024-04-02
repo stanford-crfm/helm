7be76928a05f509299347113531064b339fcebdf
farzaank <fkaiyom@gmail.com>
2024-04-02 16:17:27 -0700

    fix fmt

--- helm-frontend/src/routes/Home.tsx
+++ helm-frontend/src/routes/Home.tsx
@@ -6,20 +6,20 @@ import VHELMLanding from "@/components/VHELMLanding";
 import GlobalLanding from "@/components/Landing/GlobalLanding";
 
 export default function Home() {
-	// TODO consider a more streamlined way to do this?
-	if (window.PROJECT_ID === "lite") {
-		return <LiteLanding />;
-	} else if (window.PROJECT_ID === "instruct") {
-		return <InstructLanding />;
-	} else if (window.PROJECT_ID === "heim") {
-		return <HEIMLanding />;
-	} else if (window.PROJECT_ID === "mmlu") {
-		return <MMLULanding />;
-	} else if (window.PROJECT_ID === "vhelm") {
-		return <VHELMLanding />;
-	} else if (window.PROJECT_ID === "global") {
-		return <GlobalLanding />;
-	} else {
-		return <LiteLanding />;
-	}
+  // TODO consider a more streamlined way to do this?
+  if (window.PROJECT_ID === "lite") {
+    return <LiteLanding />;
+  } else if (window.PROJECT_ID === "instruct") {
+    return <InstructLanding />;
+  } else if (window.PROJECT_ID === "heim") {
+    return <HEIMLanding />;
+  } else if (window.PROJECT_ID === "mmlu") {
+    return <MMLULanding />;
+  } else if (window.PROJECT_ID === "vhelm") {
+    return <VHELMLanding />;
+  } else if (window.PROJECT_ID === "global") {
+    return <GlobalLanding />;
+  } else {
+    return <LiteLanding />;
+  }
 }
