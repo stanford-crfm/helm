82398734dbf42d413161fc05628ae910926e7908
Yifan Mai <yifan@cs.stanford.edu>
2024-04-03 16:45:34 -0700

    Minor unitxt fixes

---
+++ src/helm/benchmark/run_specs/unitxt_run_specs.py
@@ -0,0 +1,28 @@
+from helm.benchmark.adaptation.common_adapter_specs import (
+    get_generation_adapter_spec,
+)
+from helm.benchmark.metrics.metric import MetricSpec
+from helm.benchmark.metrics.common_metric_specs import get_basic_metric_specs
+from helm.benchmark.run_spec import RunSpec, run_spec_function
+from helm.benchmark.scenarios.scenario import ScenarioSpec
+
+
+@run_spec_function("unitxt")
+def get_unitxt_spec(**kwargs) -> RunSpec:
+    card = kwargs.get("card")
+    if not card:
+        raise Exception("Unitxt card must be specified")
+    name_suffix = ",".join([f"{key}={value}" for key, value in kwargs.items()])
+    name = f"unitxt:{name_suffix}"
+    scenario_spec = ScenarioSpec(class_name="helm.benchmark.scenarios.unitxt_scenario.UnitxtScenario", args=kwargs)
+    adapter_spec = get_generation_adapter_spec(max_train_instances=0)
+    return RunSpec(
+        name=name,
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=[
+            MetricSpec(class_name="helm.benchmark.metrics.unitxt_metrics.UnitxtMetric", args=kwargs),
+        ]
+        + get_basic_metric_specs([]),
+        groups=[f"unitxt_{card}"],
+    )
