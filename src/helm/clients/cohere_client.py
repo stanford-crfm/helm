a355e4e73999ddc9a69efea28b46acd9274a6c1b
andyt-cohere <andy@cohere.com>
2024-04-04 19:27:43 +0000

    Some cleanup to the Cohere client. More documentation and better checks.

--- src/helm/clients/cohere_client.py
+++ src/helm/clients/cohere_client.py
@@ -16,6 +16,11 @@ from .cohere_utils import get_cohere_url
 
 
 class CohereClient(CachingClient):
+    """
+    Leverages the chat endpoint: https://docs.cohere.com/reference/chat
+
+    Cohere models will only support chat soon: https://docs.cohere.com/docs/migrating-from-cogenerate-to-cochat
+    """
     ORGANIZATION: str = "cohere"
     CHAT_ENDPOINT: str = "chat"
 
@@ -23,28 +28,30 @@ class CohereClient(CachingClient):
         super().__init__(cache_config=cache_config)
         self.api_key: str = api_key
 
-    def make_request(self, request: Request) -> RequestResult:
-        if request.embedding:
-            return EMBEDDING_UNAVAILABLE_REQUEST_RESULT
-
-        # Validate `Request` according to the rules here: https://docs.cohere.com/reference/chat.
-
-        assert 0.0 <= request.temperature <= 1.0, f"Invalid temperature: {request.temperature}. Valid range: [0, 1]"
-
+    def _validate_request(self, request: Request):
         assert (
             1 <= request.num_completions <= 1
-        ), f"Invalid num_completions: {request.num_completions}. Only 1 can be requested at a time."
-
+        ), f"Invalid num_completions: {request.num_completions}. Cohere chat only supports 1 completion at a time."
+        assert 0.0 <= request.temperature <= 1.0, f"Invalid temperature: {request.temperature}. Valid range: [0, 1]"
         assert (
             0 <= request.top_k_per_token <= 500
         ), f"Invalid top_k_per_token: {request.top_k_per_token}. Valid range: [0..500]"
-
         assert 0.0 <= request.top_p <= 1.0, f"Invalid top_p: {request.top_p}. Valid range: [0,1]"
+        assert 0.0 <= request.frequency_penalty <= 1.0, f"Invalid frequency_penalty: {request.frequency_penalty}. Valid range: [0,1]"
+        assert 0.0 <= request.presence_penalty <= 1.0, f"Invalid presence_penalty: {request.presence_penalty}. Valid range: [0,1]"
+        assert 0 <= len(request.stop_sequences) <= 5, f"Invalid length of stop_sequences: {request.stop_sequences}. Up to 5 strings permitted."
+
+    def make_request(self, request: Request) -> RequestResult:
+        if request.embedding:
+            return EMBEDDING_UNAVAILABLE_REQUEST_RESULT
+        self._validate_request(request)
     
         raw_request = {
             "model": request.model_engine,
             "message": request.prompt,
             "max_tokens": request.max_tokens,
+            # Setting prompt truncation to off will throw an error if a prompt with too many tokens is passed in
+            # This will avoid silent, unexpected behaviour with truncation
             "prompt_truncation": "OFF",
             "temperature": request.temperature,
             "k": request.top_k_per_token,
@@ -58,7 +65,7 @@ class CohereClient(CachingClient):
 
             def do_it():
                 """
-                Send the request to the Cohere Generate API. Responses will be structured like this:
+                Send the request to the Cohere Chat API. Responses will be structured like this:
                 cohere.Chat {
                     message: What's up?
                     text: Hey there! How's it going? I'm doing well, thank you for asking 😊.
@@ -77,7 +84,7 @@ class CohereClient(CachingClient):
                     data=json.dumps(raw_request),
                 )
                 result = json.loads(response.text)
-                assert "text" in result, f"Invalid response: {result}"
+                assert "text" in result, f"Response does not contain text: {result}"
                 return result
 
             response, cached = self.cache.get(raw_request, wrap_request_time(do_it))
@@ -85,8 +92,13 @@ class CohereClient(CachingClient):
             error: str = f"CohereClient error: {e}"
             return RequestResult(success=False, cached=False, error=error, completions=[], embedding=[])
 
+        # Cohere chat only supports 1 completion at a time
+        # Furthermore, it does not support likelihoods, or return tokens (just text)
+        dummy_log_prob = 0.0
+        dummy_tokens = []
+
         completions: List[Sequence] = []
-        completion: Sequence = Sequence(text=response["text"], logprob=1.0, tokens=[])
+        completion: Sequence = Sequence(text=response["text"], logprob=dummy_log_prob, tokens=dummy_tokens)
         completion = truncate_sequence(completion, request)
         completions.append(completion)
 
