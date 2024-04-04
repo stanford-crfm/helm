48fa67ee581590a6f9fce6f41067c8631ae42bba
shenmishajing <shenmishajing@gmail.com>
2024-04-04 20:31:48 +0800

    feat(scenario): add mementos scenario

--- src/helm/benchmark/run_specs/vlm_run_specs.py
+++ src/helm/benchmark/run_specs/vlm_run_specs.py
@@ -390,3 +390,22 @@ def get_heim_human_eval_spec(question_type: str) -> RunSpec:
         metric_specs=metric_specs,
         groups=[run_spec_name],
     )
+
+
+@run_spec_function("mementos")
+def get_mementos_spec(subject: str) -> RunSpec:
+    scenario_spec = ScenarioSpec(
+        class_name="helm.benchmark.scenarios.vision_language.mementos_scenario.MementosScenario",
+        args={"subject": subject},
+    )
+    adapter_spec: AdapterSpec = get_short_answer_generation_adapter_spec()
+    metric_specs: List[MetricSpec] = get_open_ended_generation_metric_specs()
+
+    run_spec_name: str = "mementos"
+    return RunSpec(
+        name=run_spec_name,
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=metric_specs,
+        groups=[run_spec_name],
+    )
