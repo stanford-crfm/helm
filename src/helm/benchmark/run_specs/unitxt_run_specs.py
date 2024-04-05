f33d1c183bfc505f5b6dcb4cdf8421bbd6c36853
Yifan Mai <yifan@cs.stanford.edu>
2024-04-04 22:24:35 -0700

    Minor unitxt fixes (#2553)

--- src/helm/benchmark/run_specs/unitxt_run_specs.py
+++ src/helm/benchmark/run_specs/unitxt_run_specs.py
@@ -1,6 +1,4 @@
-from helm.benchmark.adaptation.common_adapter_specs import (
-    get_generation_adapter_spec,
-)
+from helm.benchmark.adaptation.adapter_spec import ADAPT_GENERATION, AdapterSpec
 from helm.benchmark.metrics.metric import MetricSpec
 from helm.benchmark.metrics.common_metric_specs import get_basic_metric_specs
 from helm.benchmark.run_spec import RunSpec, run_spec_function
@@ -15,7 +13,23 @@ def get_unitxt_spec(**kwargs) -> RunSpec:
     name_suffix = ",".join([f"{key}={value}" for key, value in kwargs.items()])
     name = f"unitxt:{name_suffix}"
     scenario_spec = ScenarioSpec(class_name="helm.benchmark.scenarios.unitxt_scenario.UnitxtScenario", args=kwargs)
-    adapter_spec = get_generation_adapter_spec()
+    adapter_spec = AdapterSpec(
+        method=ADAPT_GENERATION,
+        global_prefix="",
+        global_suffix="",
+        instructions="",
+        input_prefix="",
+        input_suffix="",
+        reference_prefix="A. ",
+        reference_suffix="",
+        output_prefix="",
+        output_suffix="",
+        instance_prefix="",
+        max_train_instances=0,
+        num_outputs=1,
+        temperature=0.0,
+        stop_sequences=["\n\n"],
+    )
     return RunSpec(
         name=name,
         scenario_spec=scenario_spec,
