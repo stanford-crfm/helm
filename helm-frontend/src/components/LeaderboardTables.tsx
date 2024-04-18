7c69a337a5a198a6a77b4a8d80e99b6ddaeccd53
farzaank <fkaiyom@gmail.com>
2024-04-18 13:58:25 -0700

    decompose

--- helm-frontend/src/components/LeaderboardTables.tsx
+++ helm-frontend/src/components/LeaderboardTables.tsx
@@ -21,13 +21,8 @@ interface Props {
 export default function LeaderboardTables({
   groupsTables,
   activeGroup,
-  ignoreHref = false,
   sortable = true,
   sortFirstMetric = true,
-  filtered = false,
-  filteredCols = [],
-  modelsToFilter = [],
-  numModelsToAutoFilter = 0, // if non-zero, sets how many models to filter down to (ranked by first column)
 }: Props) {
   const [activeSortColumn, setActiveSortColumn] = useState<number | undefined>(
     sortFirstMetric ? 1 : undefined,
@@ -36,8 +31,6 @@ export default function LeaderboardTables({
     ...groupsTables[activeGroup],
   });
   const [sortDirection, setSortDirection] = useState<number>(1);
-  const [filteredModels, setFilteredModels] =
-    useState<string[]>(modelsToFilter);
 
   interface HeaderValueObject {
     value: string;
@@ -134,21 +127,7 @@ export default function LeaderboardTables({
 
   useEffect(() => {
     setActiveGroupsTable({ ...groupsTables[activeGroup] });
-    // upon receiving and setting data for current table, use sort to figure out n top models
-    if (numModelsToAutoFilter) {
-      const activeRows = groupsTables[0].rows;
-      const sortedRows = activeRows.sort((a, b) => {
-        // assumes we sort by column 1, which represents Mean Win Rate in the Core Scenarios table
-        // this assumption works as numModelsToAutoFilter is only used in mini leaderboards
-        // which always have one main scenario we sort by
-        return Number(b[1].value) - Number(a[1].value);
-      });
-      // Get the top ModelsToAutoFilter
-      const topNumRows = sortedRows.slice(0, numModelsToAutoFilter);
-      const topNumRowNames = topNumRows.map((row) => String(row[0].value));
-      setFilteredModels(topNumRowNames);
-    }
-  }, [activeGroup, groupsTables, numModelsToAutoFilter]);
+  }, [activeGroup, groupsTables]);
 
   const handleSort = (columnIndex: number) => {
     let sort = sortDirection;
@@ -196,223 +175,115 @@ export default function LeaderboardTables({
 
   return (
     <>
-      {filtered ? (
-        <div
-          className="rounded-2xl overflow-hidden border-2 bg-white p-1 mx-2 my-0"
-          style={{ overflow: "auto", justifyContent: "space-between" }}
-        >
-          <div className="overflow-x-auto">
-            <table className="table w-full">
-              <thead>
-                <tr>
-                  {activeGroupsTable.header
-                    .filter(
-                      (_, cellIdx) =>
-                        filteredCols.length === 0 ||
-                        filteredCols.includes(cellIdx),
-                    )
-                    .map((headerValue, idx) => (
-                      <th
-                        key={`${activeGroup}-${idx}`}
-                        className={`${
-                          idx === activeSortColumn ? "bg-gray-100" : ""
-                        } ${
-                          headerValue.description
-                            ? "underline decoration-dashed"
-                            : ""
-                        } whitespace-nowrap px-4 `}
-                        title={
-                          headerValue.description ? headerValue.description : ""
-                        }
-                      >
-                        <div className="flex gap-2 items-center">
-                          <span>{getHeaderValue(headerValue)}</span>
-                          {sortable ? (
-                            <button
-                              className="link"
-                              onClick={() => handleSort(idx)}
-                            >
-                              <ChevronUpDownIcon className="w-6 h-6" />
-                            </button>
-                          ) : null}
-                        </div>
-                      </th>
-                    ))}
-                </tr>
-              </thead>
-              <tbody>
-                {activeGroupsTable.rows
-                  .filter((row) =>
-                    filteredModels.includes(String(row[0].value)),
-                  )
-                  .map((row, idx) => (
-                    <tr
-                      key={`${activeGroup}-${idx}`}
-                      className={`${idx % 2 === 0 ? "bg-gray-50" : ""}`}
-                    >
-                      {row
-                        .filter(
-                          (_, cellIdx) =>
-                            filteredCols.length === 0 ||
-                            filteredCols.includes(cellIdx),
-                        )
-                        .map((rowValue, cellIdx) => (
-                          <td
-                            key={`${activeGroup}-${cellIdx}`}
-                            className={`${cellIdx === 0 ? "text-lg" : ""}`}
-                          >
-                            <div
-                              className={
-                                rowValue &&
-                                rowValue.style &&
-                                rowValue.style["font-weight"] &&
-                                rowValue.style["font-weight"] === "bold"
-                                  ? "font-bold"
-                                  : ""
-                              }
-                            >
-                              {cellIdx === 0 ? (
-                                <RowValue
-                                  value={{
-                                    ...rowValue,
-                                  }}
-                                  title={getModelDesc(String(row[0].value))}
-                                  hideIcon
-                                />
-                              ) : (
-                                <RowValue
-                                  value={{
-                                    ...rowValue,
-                                    href:
-                                      "/runs/?q=" +
-                                      getModelForRunName(String(row[0].value)),
-                                  }}
-                                  title={`Click value to see all predictions for: ${getModelForRunName(
-                                    String(row[0].value),
-                                  )}`}
-                                  ignoreHref={ignoreHref}
-                                />
-                              )}
-                            </div>
-                          </td>
-                        ))}
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      ) : (
+      <div>
         <div>
-          <div>
-            <table className="rounded-lg shadow-md table">
-              <thead>
-                <tr>
-                  {activeGroupsTable.header.map((headerValue, idx) => (
-                    <th
-                      key={`${activeGroup}-${idx}`}
-                      className={`${
-                        idx === activeSortColumn ? "bg-gray-100" : "bg-white"
-                      } ${idx === 0 ? "left-0 z-10" : ""} ${
-                        headerValue.description
-                          ? "underline decoration-dashed decoration-gray-300	"
-                          : ""
-                      }  whitespace-nowrap px-4 sticky top-0`}
-                      title={
-                        headerValue.description ? headerValue.description : ""
-                      }
-                    >
-                      <div className="flex justify-between items-center min-w-48 w-48 max-w-48 text-wrap">
-                        <span className={`inline-block w-full break-words`}>
-                          {getHeaderValue(headerValue)}
-                        </span>
+          <table className="rounded-lg shadow-md table">
+            <thead>
+              <tr>
+                {activeGroupsTable.header.map((headerValue, idx) => (
+                  <th
+                    key={`${activeGroup}-${idx}`}
+                    className={`${
+                      idx === activeSortColumn ? "bg-gray-100" : "bg-white"
+                    } ${idx === 0 ? "left-0 z-10" : ""} ${
+                      headerValue.description
+                        ? "underline decoration-dashed decoration-gray-300	"
+                        : ""
+                    }  whitespace-nowrap px-4 sticky top-0`}
+                    title={
+                      headerValue.description ? headerValue.description : ""
+                    }
+                  >
+                    <div className="flex justify-between items-center min-w-48 w-48 max-w-48 text-wrap">
+                      <span className={`inline-block w-full break-words`}>
+                        {getHeaderValue(headerValue)}
+                      </span>
 
-                        {sortable ? (
-                          <button
-                            className="link"
-                            onClick={() => handleSort(idx)}
-                          >
-                            <ChevronUpDownIcon className="w-6 h-6" />
-                          </button>
-                        ) : null}
-                      </div>
-                    </th>
+                      {sortable ? (
+                        <button
+                          className="link"
+                          onClick={() => handleSort(idx)}
+                        >
+                          <ChevronUpDownIcon className="w-6 h-6" />
+                        </button>
+                      ) : null}
+                    </div>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {activeGroupsTable.rows.map((row, idx) => (
+                <tr key={`${activeGroup}-${idx}`}>
+                  {row.map((rowValue, cellIdx) => (
+                    <td
+                      key={`${activeGroup}-${cellIdx}`}
+                      className={`${
+                        cellIdx === 0 ? "text-lg sticky left-0" : ""
+                      } ${idx % 2 === 0 ? "bg-gray-50" : "bg-white"}`}
+                    >
+                      {cellIdx == 1 ? (
+                        <div
+                          className={`${
+                            rowValue &&
+                            rowValue.style &&
+                            rowValue.style["font-weight"] &&
+                            rowValue.style["font-weight"] === "bold"
+                              ? "font-bold"
+                              : ""
+                          }`}
+                        >
+                          <RowValue
+                            value={{
+                              ...rowValue,
+                              href:
+                                "/runs/?q=" +
+                                getModelForRunName(String(row[0].value)),
+                            }}
+                            title={`Click value to see all predictions for: ${getModelForRunName(
+                              String(row[0].value),
+                            )}`}
+                          />
+                        </div>
+                      ) : (
+                        <div
+                          className={`${
+                            rowValue &&
+                            rowValue.style &&
+                            rowValue.style["font-weight"] &&
+                            rowValue.style["font-weight"] === "bold"
+                              ? "font-bold"
+                              : ""
+                          } ${
+                            cellIdx === 0
+                              ? "underline decoration-dashed decoration-gray-300"
+                              : ""
+                          }`}
+                        >
+                          <RowValue
+                            value={{ ...rowValue }}
+                            title={
+                              String(row[0].value) === rowValue.value
+                                ? getModelDesc(String(row[0].value))
+                                : `Click value to see predictions for ${getGroupForRunName(
+                                    getHeaderValue(
+                                      activeGroupsTable.header[cellIdx],
+                                    ),
+                                  )}: ${getModelForRunName(
+                                    String(row[0].value),
+                                  )}`
+                            }
+                          />
+                        </div>
+                      )}
+                    </td>
                   ))}
                 </tr>
-              </thead>
-              <tbody>
-                {activeGroupsTable.rows.map((row, idx) => (
-                  <tr key={`${activeGroup}-${idx}`}>
-                    {row.map((rowValue, cellIdx) => (
-                      <td
-                        key={`${activeGroup}-${cellIdx}`}
-                        className={`${
-                          cellIdx === 0 ? "text-lg sticky left-0" : ""
-                        } ${idx % 2 === 0 ? "bg-gray-50" : "bg-white"}`}
-                      >
-                        {cellIdx == 1 ? (
-                          <div
-                            className={`${
-                              rowValue &&
-                              rowValue.style &&
-                              rowValue.style["font-weight"] &&
-                              rowValue.style["font-weight"] === "bold"
-                                ? "font-bold"
-                                : ""
-                            }`}
-                          >
-                            <RowValue
-                              value={{
-                                ...rowValue,
-                                href:
-                                  "/runs/?q=" +
-                                  getModelForRunName(String(row[0].value)),
-                              }}
-                              title={`Click value to see all predictions for: ${getModelForRunName(
-                                String(row[0].value),
-                              )}`}
-                            />
-                          </div>
-                        ) : (
-                          <div
-                            className={`${
-                              rowValue &&
-                              rowValue.style &&
-                              rowValue.style["font-weight"] &&
-                              rowValue.style["font-weight"] === "bold"
-                                ? "font-bold"
-                                : ""
-                            } ${
-                              cellIdx === 0
-                                ? "underline decoration-dashed decoration-gray-300"
-                                : ""
-                            }`}
-                          >
-                            <RowValue
-                              value={{ ...rowValue }}
-                              title={
-                                String(row[0].value) === rowValue.value
-                                  ? getModelDesc(String(row[0].value))
-                                  : `Click value to see predictions for ${getGroupForRunName(
-                                      getHeaderValue(
-                                        activeGroupsTable.header[cellIdx],
-                                      ),
-                                    )}: ${getModelForRunName(
-                                      String(row[0].value),
-                                    )}`
-                              }
-                            />
-                          </div>
-                        )}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
-      )}
+      </div>
     </>
   );
   // TODO: Remove unnecessary divs
