43153fd11db06c49ef0774afd7a1a2c035522acf
farzaank <fkaiyom@gmail.com>
2024-05-24 11:19:41 -0700

    finalize changes

--- src/helm/benchmark/annotation/model_as_judge/model_as_judge_annotator.py
+++ src/helm/benchmark/annotation/model_as_judge/model_as_judge_annotator.py
@@ -18,22 +18,29 @@ class ModelAsJudgeAnnotator(Annotator, ABC):
         self.model_name = model_name
         self.model_deployment = model_deployment
 
-    def generate_annotation(self, prompt) -> str:
+    def generate_annotation(self, prompt: str) -> str:
         """Generates an annotation from the model specified in AutoClient using it."""
-        # TODO: Fix this method
+        # TODO: Check that this method does the right thing
         annotation_request: Request = {
             "model_deployment": self.model_deployment,
             "model_name": self.model_name,
             "prompt": prompt,
         }
-        return self.auto_client.make_request(annotation_request)
+        request_result = self.auto_client.make_request(annotation_request)
+        if request_result.success:
+            return request_result.completions[0].text
+        else:
+            return "Error generating annotation"
 
-    def annotate(self, request_state: RequestState) -> List[Dict[str, Any]]:
+    def annotate(self, request_state: RequestState, prompt: str) -> List[Dict[str, Any]]:
         """Fills the annotations field of the request state using the supplied model."""
 
         assert request_state.result is not None, "Annotator can only be used after the request has been processed."
         annotations: List[Dict[str, Any]] = []
 
-        # TODO: iterate through request state to generate annotations per example by calling generate_annotation
+        # TODO check that this is right
+        annotation = self.generate_annotation(prompt)
+        if annotation != "Error generating annotation":
+            annotations.append({request_state.request.prompt: annotation})
 
-        return None
+        return annotations
