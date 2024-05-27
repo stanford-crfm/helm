22c4e00ffdf79c6171474600169559ed48f4b245
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-05-27 15:47:56 -0700

    Fix cutlassF

--- src/helm/clients/vision_language/paligemma_client.py
+++ src/helm/clients/vision_language/paligemma_client.py
@@ -22,6 +22,10 @@ try:
 except ModuleNotFoundError as e:
     handle_module_not_found_error(e, ["images"])
 
+# Added to solve: cutlassF: no kernel found to launch!
+torch.backends.cuda.enable_mem_efficient_sdp(False)
+torch.backends.cuda.enable_flash_sdp(False)
+
 
 @dataclass(frozen=True)
 class LoadedPaliGemmaForConditionalGeneration:
