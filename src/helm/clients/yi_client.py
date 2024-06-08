07dc7592a6f79618a9c11b8661d813c3c8eebc22
Yifan Mai <yifan@cs.stanford.edu>
2024-06-07 17:28:41 -0700

    Add Yi Large (#2723)

---
+++ src/helm/clients/yi_client.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from helm.clients.openai_client import OpenAIClient
+from helm.common.cache import CacheConfig
+from helm.tokenizers.tokenizer import Tokenizer
+
+
+class YiChatClient(OpenAIClient):
+
+    BASE_URL = "http://api.01ww.xyz/v1"
+
+    def __init__(
+        self,
+        tokenizer: Tokenizer,
+        tokenizer_name: str,
+        cache_config: CacheConfig,
+        api_key: Optional[str] = None,
+    ):
+        self.tokenizer = tokenizer
+        self.tokenizer_name = tokenizer_name
+        super().__init__(
+            tokenizer=tokenizer,
+            tokenizer_name=tokenizer_name,
+            cache_config=cache_config,
+            api_key=api_key,
+            org_id=None,
+            base_url=YiChatClient.BASE_URL,
+        )
+
+    def _is_chat_model_engine(self, model_engine: str) -> bool:
+        return True
