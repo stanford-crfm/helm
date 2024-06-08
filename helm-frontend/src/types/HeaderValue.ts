41d5314261c60d42a93467d9fb35a677b21d6a8c
farzaank <fkaiyom@gmail.com>
2024-06-07 21:40:35 -0700

    fix for lowerIsBetter sorting

--- helm-frontend/src/types/HeaderValue.ts
+++ helm-frontend/src/types/HeaderValue.ts
@@ -2,6 +2,7 @@ export default interface HeaderValue {
   value: string;
   markdown: boolean;
   description?: string;
+  lower_is_better?: boolean;
   metadata: {
     [key: string]: unknown;
   };
