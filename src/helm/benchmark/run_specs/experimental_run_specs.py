ddeec6cc489fed236a166f5c71e9689b41ff1b6a
Yifan Mai <yifan@cs.stanford.edu>
2024-07-08 16:04:00 -0700

    Add verifiability_judgment_with_explanation run spec function

--- src/helm/benchmark/run_specs/experimental_run_specs.py
+++ src/helm/benchmark/run_specs/experimental_run_specs.py
@@ -3,8 +3,8 @@
 These run specs are not intended for use with public leaderboards."""
 
 from helm.benchmark.adaptation.adapters.adapter_factory import ADAPT_MULTIPLE_CHOICE_JOINT
-from helm.benchmark.adaptation.common_adapter_specs import get_multiple_choice_adapter_spec
-from helm.benchmark.metrics.common_metric_specs import get_exact_match_metric_specs
+from helm.benchmark.adaptation.common_adapter_specs import get_generation_adapter_spec, get_multiple_choice_adapter_spec
+from helm.benchmark.metrics.common_metric_specs import get_basic_metric_specs, get_exact_match_metric_specs
 from helm.benchmark.run_spec import RunSpec, run_spec_function
 from helm.benchmark.scenarios.scenario import ScenarioSpec
 
@@ -30,4 +30,40 @@ def get_ci_mcqa_spec() -> RunSpec:
         adapter_spec=adapter_spec,
         metric_specs=get_exact_match_metric_specs(),
         groups=["CIMCQA"],
+    )
+
+
+_VERIFIABILITY_JUDGMENT_WITH_EXPLANATION = """
+
+"""
+
+
+@run_spec_function("verifiability_judgment_with_explanation")
+def get_verifiability_judgment_spec() -> RunSpec:
+    scenario_spec = ScenarioSpec(
+        class_name="helm.benchmark.scenarios.verifiability_judgment_scenario.VerifiabilityJudgementScenario", args={}
+    )
+
+    instructions = (
+        "You will be provided with a document delimited by triple quotes and a statement. "
+        'Your task is to judge whether the provided document "fully supports", "partially supports" or "does not support" the statement. '  # noqa: E501
+        'If the answer is "fully supports" or "partially supports", it must be annotated with direct citation from the document and/or explanation. '  # noqa: E501
+        'Use the following format for output: {"answer": "..." , "citation": "..." , "explanation": "..."}'
+    )
+    adapter_spec = get_generation_adapter_spec(
+        instructions=instructions,
+        input_noun="Statement",
+        # Add another new line before the output noun, since the source might have
+        # newlines embedded in it.
+        output_noun="\nJudgment",
+        max_tokens=300,
+        max_train_instances=0,
+    )
+
+    return RunSpec(
+        name="verifiability_judgment_with_explanation",
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=get_basic_metric_specs([]),
+        groups=["verifiability_judgment_with_explanation"],
     )
