fa61a2b0dbbe27caaac7b735e4f3aa9051cd8447
farzaank <fkaiyom@gmail.com>
2024-07-22 11:31:24 -0700

    remove other scenarios

--- src/helm/benchmark/run_specs/safety_run_specs.py
+++ src/helm/benchmark/run_specs/safety_run_specs.py
@@ -9,47 +9,6 @@ from helm.benchmark.scenarios.scenario import ScenarioSpec
 from helm.benchmark.metrics.metric import MetricSpec
 
 
-@run_spec_function("simple_safety_tests")
-def get_simple_safety_tests_spec() -> RunSpec:
-    adapter_spec = AdapterSpec(
-        method=ADAPT_GENERATION,
-        global_prefix="",
-        global_suffix="",
-        instructions="",
-        input_prefix="",
-        input_suffix="",
-        output_prefix="",
-        output_suffix="",
-        instance_prefix="",
-        max_train_instances=0,
-        num_outputs=1,
-        max_tokens=512,
-        temperature=0.0,
-        stop_sequences=[],
-    )
-    scenario_spec = ScenarioSpec(
-        class_name="helm.benchmark.scenarios.simple_safety_tests_scenario.SimpleSafetyTestsScenario"
-    )
-    annotator_specs = [
-        AnnotatorSpec(class_name="helm.benchmark.annotation.simple_safety_tests_annotator.SimpleSafetyTestsAnnotator")
-    ]
-    metric_specs = [
-        MetricSpec(class_name="helm.benchmark.metrics.simple_safety_tests_metrics.SimpleSafetyTestsScoreMetric"),
-        MetricSpec(
-            class_name="helm.benchmark.metrics.simple_safety_tests_metrics.SimpleSafetyTestsBasicGenerationMetric"
-        ),
-        MetricSpec(class_name="helm.benchmark.metrics.basic_metrics.InstancesPerSplitMetric"),
-    ]
-    return RunSpec(
-        name="simple_safety_tests",
-        scenario_spec=scenario_spec,
-        adapter_spec=adapter_spec,
-        metric_specs=metric_specs,
-        annotators=annotator_specs,
-        groups=["simple_safety_tests"],
-    )
-
-
 @run_spec_function("harm_bench")
 def get_simple_safety_tests_spec() -> RunSpec:
     adapter_spec = AdapterSpec(
