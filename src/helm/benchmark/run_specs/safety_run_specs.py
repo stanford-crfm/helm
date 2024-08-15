472aa35b7f5033ba42fc41698a0288ef1ea400d0
farzaank <fkaiyom@gmail.com>
2024-08-15 11:54:04 -0700

    fixed

--- src/helm/benchmark/run_specs/safety_run_specs.py
+++ src/helm/benchmark/run_specs/safety_run_specs.py
@@ -7,7 +7,7 @@ from helm.benchmark.metrics.metric import MetricSpec
 
 
 @run_spec_function("xstest")
-def get_simple_safety_tests_spec() -> RunSpec:
+def get_xstest_spec() -> RunSpec:
     adapter_spec = AdapterSpec(
         method=ADAPT_GENERATION,
         global_prefix="",
