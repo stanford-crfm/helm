1f515d9a6cb60cd2b0ccea9427673d0c82a4df6c
farzaank <fkaiyom@gmail.com>
2024-08-20 09:44:38 -0700

    format

--- helm-frontend/src/components/Indicator/Indicator.tsx
+++ helm-frontend/src/components/Indicator/Indicator.tsx
@@ -2,70 +2,70 @@ import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { Badge } from "@tremor/react";
 
 interface BadgeProps {
-	value: string | number;
+  value: string | number;
 }
 
 function BadgeSuccess({ value }: BadgeProps) {
-	return (
-		<Badge icon={CheckCircleIcon} color="green">
-			{value}
-		</Badge>
-	);
+  return (
+    <Badge icon={CheckCircleIcon} color="green">
+      {value}
+    </Badge>
+  );
 }
 
 function BadgeError({ value }: BadgeProps) {
-	return (
-		<Badge icon={XCircleIcon} color="red">
-			{value}
-		</Badge>
-	);
+  return (
+    <Badge icon={XCircleIcon} color="red">
+      {value}
+    </Badge>
+  );
 }
 
 interface Props {
-	stats: {
-		[key: string]: number;
-	};
+  stats: {
+    [key: string]: number;
+  };
 }
 
 export default function Indicator({ stats }: Props) {
-	if (stats["quasi_exact_match"] !== undefined) {
-		if (stats["quasi_exact_match"]) {
-			return (
-				<BadgeSuccess
-					value={`quasi exact match: ${stats["quasi_exact_match"]}`}
-				/>
-			);
-		}
-		return (
-			<BadgeError value={`quasi exact match: ${stats["quasi_exact_match"]}`} />
-		);
-	}
+  if (stats["quasi_exact_match"] !== undefined) {
+    if (stats["quasi_exact_match"]) {
+      return (
+        <BadgeSuccess
+          value={`quasi exact match: ${stats["quasi_exact_match"]}`}
+        />
+      );
+    }
+    return (
+      <BadgeError value={`quasi exact match: ${stats["quasi_exact_match"]}`} />
+    );
+  }
 
-	if (stats["toxic_frac"] !== undefined) {
-		if (stats["toxic_frac"] > 0) {
-			return <BadgeError value={`toxic frac: ${stats["toxic_frac"]}`} />;
-		}
-		return <BadgeSuccess value={`toxic frac: ${stats["toxic_frac"]}`} />;
-	}
+  if (stats["toxic_frac"] !== undefined) {
+    if (stats["toxic_frac"] > 0) {
+      return <BadgeError value={`toxic frac: ${stats["toxic_frac"]}`} />;
+    }
+    return <BadgeSuccess value={`toxic frac: ${stats["toxic_frac"]}`} />;
+  }
 
-	if (stats["safety_mean_score"] !== undefined) {
-		if (stats["safety_mean_score"] > 0) {
-			return (
-				<BadgeError value={`evaluator safety score: ${stats["art_score"]}`} />
-			);
-		}
-		return (
-			<BadgeSuccess value={`evaluator safety score: ${stats["art_score"]}`} />
-		);
-	}
+  if (stats["safety_mean_score"] !== undefined) {
+    if (stats["safety_mean_score"] > 0) {
+      return (
+        <BadgeError value={`evaluator safety score: ${stats["art_score"]}`} />
+      );
+    }
+    return (
+      <BadgeSuccess value={`evaluator safety score: ${stats["art_score"]}`} />
+    );
+  }
 
-	/**
-	 * @TODO is this always 1 or 0
-	 */
-	if (stats["exact_match"] !== undefined) {
-		if (stats["exact_match"] > 0) {
-			return <BadgeSuccess value={`exact match: ${stats["exact_match"]}`} />;
-		}
-		return <BadgeError value={`exact match: ${stats["exact_match"]}`} />;
-	}
+  /**
+   * @TODO is this always 1 or 0
+   */
+  if (stats["exact_match"] !== undefined) {
+    if (stats["exact_match"] > 0) {
+      return <BadgeSuccess value={`exact match: ${stats["exact_match"]}`} />;
+    }
+    return <BadgeError value={`exact match: ${stats["exact_match"]}`} />;
+  }
 }
