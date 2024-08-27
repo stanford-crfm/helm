f6b2fbe98dd2ff76fb064e8df12926f57641409d
Yifan Mai <yifan@cs.stanford.edu>
2024-08-27 14:15:48 -0700

    Remove stray changes

--- helm-frontend/src/routes/Home.tsx
+++ helm-frontend/src/routes/Home.tsx
@@ -9,7 +9,6 @@ import VHELMLanding from "@/components/VHELMLanding";
 import CallCenterLanding from "@/components/Landing/CallCenterLanding";
 import CLEVALanding from "@/components/Landing/CLEVALanding";
 import TablesLanding from "@/components/Landing/TablesLanding";
-import EWoKLanding from "@/components/Landing/EWoKLanding";
 import HomeLanding from "@/components/Landing/HomeLanding";
 import Image2StructLanding from "@/components/Landing/Image2StructLanding";
 
@@ -39,8 +38,6 @@ export default function Home() {
     return <CLEVALanding />;
   } else if (window.PROJECT_ID === "tables") {
     return <TablesLanding />;
-  } else if (window.PROJECT_ID === "ewok") {
-    return <EWoKLanding />;
   } else if (window.PROJECT_ID === "home") {
     return <HomeLanding />;
   } else {
