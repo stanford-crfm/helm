9a9933020e8c9e6ca4bb3a3a51ddb6ca7df8957b
Yifan Mai <yifan@cs.stanford.edu>
2024-08-27 11:39:46 -0700

    Handle perturbations in run predictions frontend

--- helm-frontend/src/routes/Home.tsx
+++ helm-frontend/src/routes/Home.tsx
@@ -9,6 +9,7 @@ import VHELMLanding from "@/components/VHELMLanding";
 import CallCenterLanding from "@/components/Landing/CallCenterLanding";
 import CLEVALanding from "@/components/Landing/CLEVALanding";
 import TablesLanding from "@/components/Landing/TablesLanding";
+import EWoKLanding from "@/components/Landing/EWoKLanding";
 import HomeLanding from "@/components/Landing/HomeLanding";
 import Image2StructLanding from "@/components/Landing/Image2StructLanding";
 
@@ -38,6 +39,8 @@ export default function Home() {
     return <CLEVALanding />;
   } else if (window.PROJECT_ID === "tables") {
     return <TablesLanding />;
+  } else if (window.PROJECT_ID === "ewok") {
+    return <EWoKLanding />;
   } else if (window.PROJECT_ID === "home") {
     return <HomeLanding />;
   } else {
--- helm-frontend/src/types/DisplayRequestsMap.ts
+++
@@ -1,5 +0,0 @@
-import type DisplayRequest from "@/types/DisplayRequest";
-
-export default interface DisplayRequestsMap {
-  [key: string]: DisplayRequest[];
-}
