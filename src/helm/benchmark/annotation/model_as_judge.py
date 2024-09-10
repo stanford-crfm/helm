c4d8b24bec51d090e3244856a16a5d4fdfe9a3a1
farzaank <fkaiyom@gmail.com>
2024-09-10 15:06:01 -0700

    fix typing issue

--- src/helm/benchmark/annotation/model_as_judge.py
+++ src/helm/benchmark/annotation/model_as_judge.py
@@ -30,7 +30,12 @@ def score_with_reasoning(
     reasoning = reasoning_match.group(1).strip() if reasoning_match else "Reasoning not found"
     score = score_match.group(1).strip() if score_match else None
 
-    if score == None:
+    if isinstance(score, (float, int, str)):
+        try:
+            score = float(score)
+        except ValueError:
+            raise Exception(f"Score not found in: {annotator_response_text}")
+    else:
         raise Exception(f"Score not found in: {annotator_response_text}")
 
-    return {"reasoning": reasoning, "score": float(score)}
+    return {"reasoning": reasoning, "score": score}
