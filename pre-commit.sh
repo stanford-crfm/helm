9bae33bb4375dd10054749f81b8f7e92bbb3027b
Yifan Mai <yifan@cs.stanford.edu>
2024-09-23 12:59:11 -0700

    Improve local frontend pre-commit (#3012)

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,18 +7,18 @@ set -e
 valid_version=$(python3 -c 'import sys; print(sys.version_info[:2] >= (3, 8))')
 if [ "$valid_version" == "False" ]; then
   echo "Python 3 version (python3 --version) must be at least 3.8, but was:"
-  echo "$(python3 --version 2>&1)"
+  python3 --version 2>&1
   exit 1
 fi
 
 # Python style checks and linting
 black --check --diff src scripts || (
   echo ""
+  echo ""
   echo "The code formatting check failed. To fix the formatting, run:"
   echo ""
-  echo ""
   echo -e "\tblack src scripts"
-  echo ""
+  echo -e "\tgit commit -a"
   echo ""
   exit 1
 )
