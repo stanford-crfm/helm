c452c4b4a5b57e4ef0658a87fba7e92b589db938
Yifan Mai <yifan@cs.stanford.edu>
2024-09-23 09:37:16 -0700

    Improve pre-commit messages

--- pre-commit-frontend.sh
+++ pre-commit-frontend.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-cd "$(dirname "$0")/helm-frontend"
+FRONTEND_DIR="$(dirname "$0")/helm-frontend"
+cd "$FRONTEND_DIR"
 
 if ! yarn --version &> /dev/null; then
   echo ""
-  echo "Failed: Yarn is not installed"
+  echo ""
+  echo "FAILED: Yarn is not installed"
   echo "Follow the instructions at https://yarnpkg.com/getting-started/install to install Yarn."
   echo ""
   exit 1
@@ -14,25 +16,37 @@ fi
 
 if [ ! -f "yarn.lock" ]; then
   echo ""
-  echo "Failed: Frontend is not installed."
-  echo "Run \`yarn install\` to install the frontend."
+  echo ""
+  echo "FAILED: Frontend dependencies are not installed. To install frontend dependencies, run:"
+  echo ""
+  echo -e "\tcd $FRONTEND_DIR"
+  echo -e "\tyarn install"
   echo ""
   exit 1
 fi
 
 yarn format:check || (
   echo ""
-  echo "Failed: 'yarn format:check'"
-  echo "Run 'yarn format' from helm-frontend/ to fix these errors."
+  echo ""
+  echo "FAILED: The frontend code formatting check failed. To fix the formatting, run:"
+  echo ""
+  echo -e "\tcd $FRONTEND_DIR"
+  echo -e "\tyarn format"
+  echo -e "\tgit commit -a"
   echo ""
   exit 1
 )
 
 yarn lint:check || (
   echo ""
-  echo "Failed: 'yarn lint:check'"
-  echo "Run 'yarn lint' from helm-frontend/ to attempt to automatically fix these errors."
-  echo "You may have to manually fix some errors."
+  echo ""
+  echo "FAILED: The frontend code linter check failed. To lint the frontend code, run:"
+  echo ""
+  echo -e "\tcd $FRONTEND_DIR"
+  echo -e "\tyarn lint"
+  echo -e "\tgit commit -a"
+  echo ""
+  echo "You may have to manually fix some errors if the automatic linter fails."
   echo ""
   exit 1
 )
