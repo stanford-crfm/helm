3934c8c1a9d09da3fe800e2b90231a19e6233f1a
Yifan Mai <yifan@cs.stanford.edu>
2024-10-16 14:57:08 -0700

    Revert finance run specs changes

--- src/helm/benchmark/run_specs/finance_run_specs.py
+++ src/helm/benchmark/run_specs/finance_run_specs.py
@@ -9,7 +9,6 @@ from helm.benchmark.adaptation.common_adapter_specs import (
 from helm.benchmark.annotation.annotator import AnnotatorSpec
 from helm.benchmark.metrics.common_metric_specs import (
     get_basic_metric_specs,
-    get_classification_metric_specs,
     get_exact_match_metric_specs,
 )
 from helm.benchmark.metrics.metric import MetricSpec
@@ -112,26 +111,4 @@ def get_banking77_spec() -> RunSpec:
         adapter_spec=adapter_spec,
         metric_specs=metric_specs,
         groups=["banking77"],
-    )
-
-
-@run_spec_function("gold_commodity_news")
-def get_news_headline_spec(category: str) -> RunSpec:
-    from helm.benchmark.scenarios.gold_commodity_news_scenario import GoldCommodityNewsScenario
-
-    scenario_spec = ScenarioSpec(
-        class_name="helm.benchmark.scenarios.gold_commodity_news_scenario.GoldCommodityNewsScenario",
-        args={"category": category},
-    )
-
-    adapter_spec = get_generation_adapter_spec(
-        instructions=GoldCommodityNewsScenario.get_instructions(category), input_noun="Headline", output_noun="Answer"
-    )
-
-    return RunSpec(
-        name=f"gold_commodity_news:category={category}",
-        scenario_spec=scenario_spec,
-        adapter_spec=adapter_spec,
-        metric_specs=get_exact_match_metric_specs() + get_classification_metric_specs(),
-        groups=["gold_commodity_news"],
     )
