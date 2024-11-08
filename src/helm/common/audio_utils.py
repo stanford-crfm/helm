04a30685bca28cc01992ba10880b25a5d084a9be
Yifan Mai <yifan@cs.stanford.edu>
2024-11-07 20:04:14 -0800

    Add MELD Audio scenario (#3142)

--- src/helm/common/audio_utils.py
+++ src/helm/common/audio_utils.py
@@ -1,8 +1,13 @@
+from io import BytesIO
 import os
+from typing import Optional
 from filelock import FileLock
 
+import librosa
 import numpy as np
 from scipy.io import wavfile
+
+from helm.common.multimodal_request_utils import get_contents_as_bytes
 
 
 def ensure_wav_file_exists_from_array(path: str, array: np.ndarray, sample_rate: int) -> None:
@@ -18,7 +23,7 @@ def ensure_wav_file_exists_from_array(path: str, array: np.ndarray, sample_rate:
             return
         path_prefix = path.removesuffix(".wav")
         tmp_path = f"{path_prefix}.tmp.wav"
-        wavfile.write(tmp_path, array, samplerate=sample_rate)
+        wavfile.write(filename=tmp_path, rate=sample_rate, data=array)
         os.rename(tmp_path, path)
 
 
@@ -35,5 +40,15 @@ def ensure_mp3_file_exists_from_array(path: str, array: np.ndarray, sample_rate:
             return
         path_prefix = path.removesuffix(".mp3")
         tmp_path = f"{path_prefix}.tmp.mp3"
-        wavfile.write(tmp_path, array, samplerate=sample_rate)
+        wavfile.write(filename=tmp_path, rate=sample_rate, data=array)
         os.rename(tmp_path, path)
+
+
+def get_array_from_audio_file(path: str, sample_rate: Optional[int]) -> np.ndarray:
+    """Get an array from an audio file"""
+    audio_file = (
+        BytesIO(get_contents_as_bytes(path)) if path.startswith("http://") or path.startswith("https://") else path
+    )
+    # librosa accepts a local file path or a file-like object
+    audio_array, _ = librosa.load(audio_file, sr=sample_rate)
+    return audio_array
