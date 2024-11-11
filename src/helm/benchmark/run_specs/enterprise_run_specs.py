88e35af24194ff5ac8d51ad17006da87120cf6c5
Yifan Mai <yifan@cs.stanford.edu>
2024-11-11 10:10:46 -0800

    Fixes

--- src/helm/benchmark/run_specs/enterprise_run_specs.py
+++ src/helm/benchmark/run_specs/enterprise_run_specs.py
@@ -1,12 +1,15 @@
 """Run spec functions for HELM Enterprise scenarios."""
 
+from helm.benchmark.adaptation.adapter_spec import ADAPT_MULTIPLE_CHOICE_JOINT
 from helm.benchmark.adaptation.common_adapter_specs import (
     get_generation_adapter_spec,
+    get_multiple_choice_adapter_spec,
 )
 from helm.benchmark.metrics.common_metric_specs import (
     get_basic_metric_specs,
     get_classification_metric_specs,
     get_exact_match_metric_specs,
+    get_f1_metric_specs,
 )
 from helm.benchmark.run_spec import RunSpec, run_spec_function
 from helm.benchmark.scenarios.scenario import ScenarioSpec
@@ -37,6 +40,9 @@ def get_news_headline_spec(category: str) -> RunSpec:
     )
 
 
+# Legal
+
+
 @run_spec_function("legal_contract_summarization")
 def get_legal_contract_spec() -> RunSpec:
     scenario_spec = ScenarioSpec(
@@ -58,4 +64,28 @@ def get_legal_contract_spec() -> RunSpec:
         adapter_spec=adapter_spec,
         metric_specs=get_basic_metric_specs(["rouge_1", "rouge_2"]),
         groups=["legal_contract_summarization"],
+    )
+
+
+@run_spec_function("casehold_qa")
+def get_casehold_qa_spec() -> RunSpec:
+    scenario_spec = ScenarioSpec(class_name="helm.benchmark.scenarios.casehold_qa_scenario.CaseHOLDQAScenario", args={})
+
+    method = ADAPT_MULTIPLE_CHOICE_JOINT
+    adapter_spec = get_multiple_choice_adapter_spec(
+        method=method,
+        instructions="Give a letter answer among A, B, C, D, or E.",
+        input_noun="Passage",
+        output_noun="Answer",
+        max_train_instances=2,
+    )
+
+    metric_specs = get_f1_metric_specs()
+
+    return RunSpec(
+        name="casehold_qa",
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=metric_specs,
+        groups=["casehold_qa"],
     )
--- src/helm/benchmark/scenarios/casehold_qa_scenario.py
+++ src/helm/benchmark/scenarios/casehold_qa_scenario.py
@@ -1,46 +1,20 @@
-import json
+from typing import List
 import os
 import os.path
-import shutil
-import datasets
-from typing import List, Dict, Any, cast
+
+from datasets import load_dataset, DatasetDict
 
 from helm.common.general import ensure_directory_exists
 from helm.benchmark.scenarios.scenario import (
+    Input,
     Scenario,
     Instance,
     Reference,
     TRAIN_SPLIT,
-    VALID_SPLIT,
     TEST_SPLIT,
-    ALL_SPLITS,
     CORRECT_TAG,
-    PassageQuestionInput,
     Output,
 )
-
-
-def download_dataset(data_path: str, splits: List[str]):
-    if False not in [os.path.exists(f"{data_path}/{split}.jsonl") for split in splits]:
-        return
-
-    # https://huggingface.co/docs/datasets/index
-    ds_names: List[str] = datasets.list_datasets()
-    # https://huggingface.co/casehold
-    # https://huggingface.co/datasets/casehold/casehold
-    ds_name = "casehold/casehold"
-    if ds_name not in ds_names:
-        raise Exception(f"{ds_name} not included in datasets")
-    casehold: datasets.DatasetDict = cast(datasets.DatasetDict, datasets.load_dataset(ds_name))
-
-    for split in splits:
-        casehold[split].to_json(f"{data_path}/{split}.jsonl")
-
-    # **WORK-AROUND**
-    # since "test.jsonl" includes no label info., we use "validation.jsonl" as a substitute.
-    if os.path.exists(f"{data_path}/test.jsonl"):
-        os.remove(f"{data_path}/test.jsonl")
-    shutil.copy(f"{data_path}/validation.jsonl", f"{data_path}/test.jsonl")
 
 
 class CaseHOLDQAScenario(Scenario):
@@ -64,46 +38,42 @@ class CaseHOLDQAScenario(Scenario):
       The correct answer is the holding statement that corresponds to the citing text.
       The four incorrect answers are other holding statements.
 
-    """
+    """  # noqa: E501
 
     name = "casehold_qa"
-    description = "CaseHOLD QA"
+    description = "CaseHOLD (Case Holdings On Legal Decisions) is a multiple choice question answering scenario where the task is to identify the relevant holding of a cited case [(Zheng et al, 2021)](https://arxiv.org/pdf/2104.08671.pdf)."  # noqa: E501
     tags = ["question_answering", "legal"]
 
-    splits_dict = {TRAIN_SPLIT: "train", VALID_SPLIT: "validation", TEST_SPLIT: "test"}
-
-    def __init__(self, splits: List[str] = ALL_SPLITS):
-        super().__init__()
-        self.splits = splits
+    # Note: Skip the validation split since we don't need it
+    HELM_SPLIT_NAME_TO_DATASETS_SPLIT_NAME = {TRAIN_SPLIT: "train", TEST_SPLIT: "test"}
+    NUM_REFERENCES = 5
 
     def get_instances(self, output_path: str) -> List[Instance]:
         data_path: str = os.path.join(output_path, "data")
         ensure_directory_exists(data_path)
-        download_dataset(data_path, list(self.splits_dict.values()))
-
-        def to_instance(line: str, split: str) -> Instance:
-            case: Dict[str, Any] = json.loads(line)
-            example_id: int = case["example_id"]
-            context: str = case["citing_prompt"]
-            question: str = "holding statement"
-            holdings: List[str] = [case[f"holding_{i}"] for i in range(5)]
-            label: str = case["label"]
-            instance: Instance = Instance(
-                input=PassageQuestionInput(passage=context, question=question),
-                references=[
-                    Reference(Output(text=holdings[i]), tags=([CORRECT_TAG] if label == str(i) else []))
-                    for i in range(5)
-                ],
-                split=split,
-                id=str(example_id),
-            )
-            return instance
+        dataset: DatasetDict = load_dataset(
+            "casehold/casehold",
+            "all",
+            cache_dir=data_path,
+        )
 
         instances: List[Instance] = []
-        # TRAIN, VALID
-        for split in self.splits:
-            with open(f"{data_path}/{self.splits_dict[split]}.jsonl", mode="r") as f:
-                for line in f.readlines():
-                    instances.append(to_instance(line, split))
-
+        for helm_split_name, datasets_split_name in self.HELM_SPLIT_NAME_TO_DATASETS_SPLIT_NAME.items():
+            split_data = dataset[datasets_split_name]
+            for example in split_data:
+                example_id = example["example_id"]
+                citing_prompt = example["citing_prompt"]
+                holdings = [example[f"holding_{i}"] for i in range(self.NUM_REFERENCES)]
+                correct_label: str = example["label"]
+                references = [
+                    Reference(Output(text=holdings[i]), tags=([CORRECT_TAG] if correct_label == str(i) else []))
+                    for i in range(self.NUM_REFERENCES)
+                ]
+                instance = Instance(
+                    input=Input(text=citing_prompt),
+                    references=references,
+                    split=helm_split_name,
+                    id=f"id{example_id}",
+                )
+                instances.append(instance)
         return instances
