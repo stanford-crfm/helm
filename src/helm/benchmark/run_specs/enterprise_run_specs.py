6bb36628e618228b769140b9d9e7636ca28cf43f
Yifan Mai <yifan@cs.stanford.edu>
2024-11-12 13:15:25 -0800

    Add Legal Contract Summarization scenario (#3131)
    Co-authored-by: Mikio Takeuchi <mtake@jp.ibm.com>
    Co-authored-by: Ryo Kawahara <ryokawa@jp.ibm.com>

--- src/helm/benchmark/run_specs/enterprise_run_specs.py
+++ src/helm/benchmark/run_specs/enterprise_run_specs.py
@@ -4,6 +4,7 @@ from helm.benchmark.adaptation.common_adapter_specs import (
     get_generation_adapter_spec,
 )
 from helm.benchmark.metrics.common_metric_specs import (
+    get_basic_metric_specs,
     get_classification_metric_specs,
     get_exact_match_metric_specs,
 )
@@ -33,4 +34,28 @@ def get_news_headline_spec(category: str) -> RunSpec:
         adapter_spec=adapter_spec,
         metric_specs=get_exact_match_metric_specs() + get_classification_metric_specs(),
         groups=["gold_commodity_news"],
+    )
+
+
+@run_spec_function("legal_contract_summarization")
+def get_legal_contract_spec() -> RunSpec:
+    scenario_spec = ScenarioSpec(
+        class_name="helm.benchmark.scenarios.legal_contract_summarization_scenario.LegalContractSummarizationScenario",
+        args={},
+    )
+
+    adapter_spec = get_generation_adapter_spec(
+        instructions="Summarize the legal document in plain English.",
+        input_noun="Document",
+        output_noun="Summary",
+        max_tokens=100,
+        stop_sequences=["\n\n"],
+    )
+
+    return RunSpec(
+        name="legal_contract_summarization",
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=get_basic_metric_specs(["rouge_1", "rouge_2"]),
+        groups=["legal_contract_summarization"],
     )
