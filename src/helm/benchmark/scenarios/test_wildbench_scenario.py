fc7368e03a0682c8f2bf5171a08fd932594e0f28
Jialiang Xu <xjl@stanford.edu>
2024-11-13 13:55:19 -0800

    scenario test

--- src/helm/benchmark/scenarios/test_wildbench_scenario.py
+++ src/helm/benchmark/scenarios/test_wildbench_scenario.py
@@ -2,6 +2,7 @@ import pytest
 from tempfile import TemporaryDirectory
 
 from helm.benchmark.scenarios.wildbench_scenario import WildBenchScenario
+from helm.benchmark.scenarios.scenario import TEST_SPLIT
 
 
 @pytest.mark.scenarios
@@ -10,13 +11,15 @@ def test_wildbench_scenario_get_instances():
     with TemporaryDirectory() as tmpdir:
         instances = wildbench_scenario.get_instances(tmpdir)
     assert len(instances) == 1024
-    # assert instances[0].input.text.startswith("add 10 more balanced governments[aoc2]\n{\n\tGovernment")
-    # assert len(instances[0].input.text) == 17619
-    # assert instances[0].split == TEST_SPLIT
-    # assert instances[0].extra_data
+    assert instances[0].split == TEST_SPLIT
+    assert instances[0].extra_data
 
-    # assert instances[0].extra_data["gpt-4-turbo-2024-04-09"].startswith("Here are 10 addition")
-    # assert len(instances[0].extra_data["gpt-4-turbo-2024-04-09"]) == 10574
-    # assert instances[0].extra_data["claude-3-haiku-20240307"].startswith("Here are 10 more bal")
-    # assert len(instances[0].extra_data["claude-3-haiku-20240307"]) == 7873
-    # assert instances[0].extra_data["Llama-2-70b-chat-hf"] == ""
+    assert instances[0].extra_data["user_query"].startswith("add 10 more balanced governments[aoc2]\n{\n\tGovernment")
+    assert len(instances[0].extra_data["user_query"]) == 17619
+
+    assert instances[0].extra_data["baseline_outputs"]
+    assert instances[0].extra_data["baseline_outputs"]["gpt-4-turbo-2024-04-09"].startswith("Here are 10 addition")
+    assert len(instances[0].extra_data["baseline_outputs"]["gpt-4-turbo-2024-04-09"]) == 10574
+    assert instances[0].extra_data["baseline_outputs"]["claude-3-haiku-20240307"].startswith("Here are 10 more bal")
+    assert len(instances[0].extra_data["baseline_outputs"]["claude-3-haiku-20240307"]) == 7873
+    assert instances[0].extra_data["baseline_outputs"]["Llama-2-70b-chat-hf"] == ""
