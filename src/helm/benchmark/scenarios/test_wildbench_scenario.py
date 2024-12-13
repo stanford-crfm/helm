0fa0390e3c5b4f98c0f7fc6cefa2afc1fa9d425e
Jialiang Xu <xjl@stanford.edu>
2024-12-13 14:28:18 -0800

    updated scenario test for WildBench

--- src/helm/benchmark/scenarios/test_wildbench_scenario.py
+++ src/helm/benchmark/scenarios/test_wildbench_scenario.py
@@ -16,10 +16,3 @@ def test_wildbench_scenario_get_instances():
 
     assert instances[0].extra_data["user_query"].startswith("add 10 more balanced governments[aoc2]\n{\n\tGovernment")
     assert len(instances[0].extra_data["user_query"]) == 17619
-
-    assert instances[0].extra_data["baseline_outputs"]
-    assert instances[0].extra_data["baseline_outputs"]["gpt-4-turbo-2024-04-09"].startswith("Here are 10 addition")
-    assert len(instances[0].extra_data["baseline_outputs"]["gpt-4-turbo-2024-04-09"]) == 10574
-    assert instances[0].extra_data["baseline_outputs"]["claude-3-haiku-20240307"].startswith("Here are 10 more bal")
-    assert len(instances[0].extra_data["baseline_outputs"]["claude-3-haiku-20240307"]) == 7873
-    assert instances[0].extra_data["baseline_outputs"]["Llama-2-70b-chat-hf"] == ""
