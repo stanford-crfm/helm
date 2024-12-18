3278e12dd64b83269a4b9d70bb6b63d4a520ff25
thallysonjsa <thallyson.alves@ccc.ufcg.edu.br>
2024-12-18 15:50:56 -0300

    Pre-commit

--- src/helm/benchmark/scenarios/tweetsentbr_scenario.py
+++ src/helm/benchmark/scenarios/tweetsentbr_scenario.py
@@ -16,16 +16,16 @@ from helm.benchmark.scenarios.scenario import (
 
 class TweetSentBRScenario(Scenario):
     """
-    TweetSentBR is a corpus of Tweets in Brazilian Portuguese. It was labeled by several 
-    annotators following steps stablished on the literature for improving reliability on 
+    TweetSentBR is a corpus of Tweets in Brazilian Portuguese. It was labeled by several
+    annotators following steps stablished on the literature for improving reliability on
     the task of Sentiment Analysis. Each Tweet was annotated in one of the three following classes:
 
     Positive - tweets where a user meant a positive reaction or evaluation about the main topic on the post;
     Negative - tweets where a user meant a negative reaction or evaluation about the main topic on the post;
-    Neutral - tweets not belonging to any of the last classes, usually not making a point, out of topic, 
+    Neutral - tweets not belonging to any of the last classes, usually not making a point, out of topic,
     irrelevant, confusing or containing only objective data.
 
-    This dataset is a subset of the tweetSentBR, it contains only 75 samples from the training set 
+    This dataset is a subset of the tweetSentBR, it contains only 75 samples from the training set
     and all 2.000+ instances of the test set. This is meant for evaluating language models in a few-shot setting.
     """
 
