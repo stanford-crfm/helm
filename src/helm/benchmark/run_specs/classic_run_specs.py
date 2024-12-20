5e9bf74bd8d7da3d0a26b8017de38d91adb5170c
Alyssa Unell <62268012+aunell@users.noreply.github.com>
2024-12-20 14:42:58 -0800

    Add MedAlign scenario (#3038)
    Co-authored-by: Yifan Mai <yifan@cs.stanford.edu>

--- src/helm/benchmark/run_specs/classic_run_specs.py
+++ src/helm/benchmark/run_specs/classic_run_specs.py
@@ -14,6 +14,7 @@ from helm.benchmark.adaptation.adapter_spec import (
     ADAPT_MULTIPLE_CHOICE_SEPARATE_ORIGINAL,
     ADAPT_RANKING_BINARY,
     AdapterSpec,
+    ADAPT_EHR_INSTRUCTION,
 )
 from helm.benchmark.adaptation.adapters.binary_ranking_adapter import BinaryRankingAdapter
 from helm.benchmark.adaptation.common_adapter_specs import (
@@ -1215,6 +1216,41 @@ def get_medication_qa_spec() -> RunSpec:
         annotators=annotator_specs,
         metric_specs=metric_specs,
         groups=["medication_qa"],
+    )
+
+
+def get_medalign_adapter_spec() -> AdapterSpec:
+    return AdapterSpec(
+        method=ADAPT_EHR_INSTRUCTION,
+        max_train_instances=0,
+        max_eval_instances=None,
+        num_outputs=1,
+        max_tokens=256,  # MedAlign default number of generation tokens
+    )
+
+
+def get_comet_metric_specs(args: Dict[str, Any]) -> List[MetricSpec]:
+    return [MetricSpec(class_name="helm.benchmark.metrics.comet_metric.CometMetric", args=args)]
+
+
+@run_spec_function("medalign")
+def get_medalign_spec(prompt_template: str = "generic.txt") -> RunSpec:
+    from helm.common.gpu_utils import get_torch_device_name
+
+    scenario_spec = ScenarioSpec(
+        class_name="helm.benchmark.scenarios.medalign_scenario.MedAlignScenario",
+        args={"prompt_template": prompt_template},
+    )
+
+    adapter_spec = get_medalign_adapter_spec()
+
+    metric_args = {"task": "medalign", "device": get_torch_device_name()}
+    return RunSpec(
+        name="medalign",
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=get_summarization_metric_specs(metric_args) + get_comet_metric_specs(metric_args),
+        groups=["medalign", "med_helm"],
     )
 
 
