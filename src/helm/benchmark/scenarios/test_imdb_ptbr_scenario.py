ce189770a77f43cd3fa67b99dd785c53be7af656
thallysonjsa <thallyson.alves@ccc.ufcg.edu.br>
2025-01-22 15:08:18 -0300

    rewriting files

--- src/helm/benchmark/scenarios/test_imdb_ptbr_scenario.py
+++ src/helm/benchmark/scenarios/test_imdb_ptbr_scenario.py
@@ -5,7 +5,7 @@ from helm.benchmark.scenarios.imdb_ptbr_scenario import IMDB_PTBRScenario
 from helm.benchmark.scenarios.scenario import TRAIN_SPLIT, CORRECT_TAG, Output, Reference
 
 
-#@pytest.mark.scenarios
+@pytest.mark.scenarios
 def test_imdb_ptbr_scenario():
     imdb_ptbr = IMDB_PTBRScenario()
     with TemporaryDirectory() as tmpdir:
@@ -14,7 +14,8 @@ def test_imdb_ptbr_scenario():
     assert instances[0].split == TRAIN_SPLIT
 
     assert instances[10].input.text.startswith(
-        "Foi ótimo ver algumas das minhas estrelas favoritas de 30 anos atrás, incluindo John Ritter, Ben Gazarra e Audrey Hepburn."
+        "Foi ótimo ver algumas das minhas estrelas favoritas de 30 anos atrás, \
+        incluindo John Ritter, Ben Gazarra e Audrey Hepburn."
     )
     assert len(instances[10].input.text) == 1549
 
