ae38c2fc938910e8ba12184eee1b43fbb68c4612
Yifan Mai <yifan@cs.stanford.edu>
2025-02-26 13:11:04 -0800

    Remove Tables landing since it was replaced by ToRR

--- helm-frontend/src/routes/Home.tsx
+++ helm-frontend/src/routes/Home.tsx
@@ -8,7 +8,6 @@ import HEIMLanding from "@/components/Landing/HEIMLanding";
 import VHELMLanding from "@/components/VHELMLanding";
 import CallCenterLanding from "@/components/Landing/CallCenterLanding";
 import CLEVALanding from "@/components/Landing/CLEVALanding";
-import TablesLanding from "@/components/Landing/TablesLanding";
 import ToRRLanding from "@/components/Landing/ToRRLanding";
 import HomeLanding from "@/components/Landing/HomeLanding";
 import Image2StructLanding from "@/components/Landing/Image2StructLanding";
@@ -43,8 +42,6 @@ export default function Home() {
     return <CallCenterLanding />;
   } else if (window.PROJECT_ID === "cleva") {
     return <CLEVALanding />;
-  } else if (window.PROJECT_ID === "tables") {
-    return <TablesLanding />;
   } else if (window.PROJECT_ID === "torr") {
     return <ToRRLanding />;
   } else if (window.PROJECT_ID === "ewok") {
