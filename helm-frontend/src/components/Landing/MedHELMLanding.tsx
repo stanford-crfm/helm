d40ab627457f00f60ee44db77b77c57a0c7e3f7b
Yifan Mai <yifan@cs.stanford.edu>
2025-02-27 14:24:43 -0800

    Update MedHELM landing (#3383)

--- helm-frontend/src/components/Landing/MedHELMLanding.tsx
+++ helm-frontend/src/components/Landing/MedHELMLanding.tsx
@@ -3,11 +3,28 @@ import { Link } from "react-router-dom";
 import overview from "@/assets/medhelm/medhelm-overview.png";
 
 export default function MedHELMLanding() {
+  const benchmarkName = <strong className="font-bold">MedHELM</strong>;
   return (
     <div className="container mx-auto px-16">
-      <h1 className="text-3xl my-8 font-bold text-center">MedHELM</h1>
+      <h1 className="text-3xl my-4 font-bold text-center">MedHELM</h1>
+      <p className="text-xl my-4 italic text-center">
+        Medical and AI experts build a benchmark for evaluation of LLMs grounded
+        in real-world healthcare needs.
+      </p>
       <div className="flex flex-col lg:flex-row gap-8">
         <div className="flex-1 text-l">
+          <p className="my-4">
+            We introduce {benchmarkName}, a comprehensive healthcare benchmark
+            to evaluate language models on real-world clinical tasks using real
+            electronic health records. Building on the HELM framework,{" "}
+            {benchmarkName} comprises a structured taxonomy with 5 categories,
+            22 subcategories, and 121 distinct clinical tasks as well as 31
+            diverse datasets (12 private, 6 gated-access, and 13 public). The
+            datasets represent a spectrum of healthcare scenarios, from
+            diagnostic decision-making to patient communication, providing a
+            more nuanced and clinically relevant assessment of AI capabilities
+            in healthcare settings.
+          </p>
           <img
             src={overview}
             alt="MedHELM Task Categories"
@@ -15,25 +32,11 @@ export default function MedHELMLanding() {
             sizes="100vw"
           />
           <p className="my-4">
-            We introduce <strong className="font-bold">MedHELM</strong>, a
-            comprehensive healthcare benchmark to evaluate language models on
-            real-world clinical tasks using real electronic health records.
-            Building on the HELM framework,{" "}
-            <strong className="font-bold">MedHELM</strong> comprises a
-            structured taxonomy with 5 categories, 22 subcategories, and 121
-            distinct clinical tasks as well as 31 diverse datasets (12 private,
-            6 gated-access, and 13 public). The datasets represent a spectrum of
-            healthcare scenarios, from diagnostic decision-making to patient
-            communication, providing a more nuanced and clinically relevant
-            assessment of AI capabilities in healthcare settings.
-          </p>
-          <p className="my-4">
-            <strong className="font-bold">MedHELM</strong> establishes a
-            foundation for testing and evaluation of the real-world
-            applicability of language models in healthcare. It is made possible
-            by a unique collaboration between the Center for Research on
-            Foundation Models, Technology and Digital Solutions at Stanford
-            Healthcare, and Microsoft Healthcare and Life Sciences in
+            {benchmarkName} establishes a foundation for testing and evaluation
+            of the real-world applicability of language models in healthcare. It
+            is made possible by a unique collaboration between the Center for
+            Research on Foundation Models, Technology and Digital Solutions at
+            Stanford Healthcare, and Microsoft Healthcare and Life Sciences in
             partnership with faculty in the Departments of Medicine, Computer
             Science, Anesthesiology, Dermatology, Pediatrics and Biomedical Data
             Science as well as trainees from the MCiM program at the Clinical
