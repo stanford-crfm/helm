c2e2e18b60dd56cfd5f997e99d481be821a67f2b
Yifan Mai <yifan@cs.stanford.edu>
2025-04-02 15:49:42 -0700

    Fix MetricSpecs

--- src/helm/benchmark/run_specs/enterprise_run_specs.py
+++ src/helm/benchmark/run_specs/enterprise_run_specs.py
@@ -115,8 +115,7 @@ def get_kpi_edgar_spec() -> RunSpec:
         scenario_spec=scenario_spec,
         adapter_spec=adapter_spec,
         metric_specs=get_basic_metric_specs([])
-        + [MetricSpec(class_name="helm.benchmark.metrics.kpi_edgar_metrics.SimpleNERAdjustedMetric"),
-           MetricSpec(class_name="helm.benchmark.metrics.kpi_edgar_old_metrics.NERAdjustedF1Metric")],
+        + [MetricSpec(class_name="helm.benchmark.metrics.kpi_edgar_metrics.SimpleNERAdjustedMetric")],
         groups=["kpi_edgar"],
     )
 
