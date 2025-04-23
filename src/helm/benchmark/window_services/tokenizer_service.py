68b178151eab12bf14bfcaad70bbded43c910adc
Yifan Mai <yifan@cs.stanford.edu>
2025-04-22 17:17:20 -0700

    Split non-server services into context

--- src/helm/benchmark/window_services/tokenizer_service.py
+++ src/helm/benchmark/window_services/tokenizer_service.py
@@ -1,26 +1,25 @@
-from helm.common.authentication import Authentication
+from helm.common.context import Context
 from helm.common.tokenization_request import (
     TokenizationRequest,
     TokenizationRequestResult,
     DecodeRequest,
     DecodeRequestResult,
 )
-from helm.proxy.services.service import Service
 
 
+# TODO: Rename this to TokenizerContext
 class TokenizerService:
     """
-    A wrapper around `Service` that makes only necessary server requests to tokenize.
+    A wrapper around `Context` that makes only necessary server requests to tokenize.
     """
 
-    def __init__(self, service: Service, auth: Authentication):
-        self._service: Service = service
-        self._auth: Authentication = auth
+    def __init__(self, context: Context):
+        self._context: Context = context
 
     def tokenize(self, request: TokenizationRequest) -> TokenizationRequestResult:
         """Tokenize via an API."""
-        return self._service.tokenize(self._auth, request)
+        return self._context.tokenize(request)
 
     def decode(self, request: DecodeRequest) -> DecodeRequestResult:
         """Decode via an API."""
-        return self._service.decode(self._auth, request)
+        return self._context.decode(request)
