b06844f7003776d835dcc2575be457b8fe8e032d
Martin Nguyen <nguyenquangduc2000@gmail.com>
2025-04-24 09:13:34 +0700

    MELT: Add code for knowledge, toxicity detection and information retrieval scenarios

--- src/helm/benchmark/scenarios/melt_scenarios.py
+++ src/helm/benchmark/scenarios/melt_scenarios.py
@@ -9,6 +9,7 @@ import numpy as np
 from copy import copy
 from dataclasses import dataclass
 from datasets import load_dataset
+from helm.common.hierarchical_logger import hlog
 from helm.common.general import ensure_directory_exists
 from helm.common.hierarchical_logger import htrack_block
 from helm.benchmark.scenarios.scenario import (
@@ -22,6 +23,7 @@ from helm.benchmark.scenarios.scenario import (
     PassageQuestionInput,
     Input,
     Output,
+    make_rank_tag,
 )
 
 
@@ -1894,4 +1896,461 @@ class MELTLMSpellingCorrectionVSECScenario(MELTLMSpellingCorrectionScenario):
             splits={
                 TEST_SPLIT: "test",
             },
+        )
+
+
+class MELTClosedBookQAScenario(Scenario):
+    name = "melt_closed_book_qa"
+    description = "Closed Book Question Answering scenario."
+    tags = ["question_answering"]
+
+    def __init__(
+        self,
+        dataset_name: str,
+        revision: str,
+        subset: Optional[str] = "",
+        splits: Optional[Dict[str, str]] = None,
+    ):
+        """
+        Initializes the question answering scenario.
+
+        Args:
+            dataset_name: The name of the dataset.
+            revision: The revision of the dataset to use.
+            subset: The subset of the dataset to use. Defaults to "".
+            splits: The splits to use for the dataset. Defaults to None.
+        """
+        super().__init__()
+        self.dataset_name = dataset_name
+        self.subset = subset
+        self.revision = revision
+        self.splits = splits
+
+    def get_instances_for_splits(self, splits: Dict[str, str]) -> List[Instance]:
+        """
+        Helper for generating instances for a split.
+        Args:
+            splits (dict): Which splits to partition the data into.
+        Returns:
+            List[Instance]: Instances from the file for the specified split.
+        """
+        instances: List[Instance] = []
+        dataset = load_dataset(
+            self.dataset_name,
+            self.subset,
+            revision=self.revision,
+            trust_remote_code=True,
+        )
+        for dataset_split_name, helm_split_name in splits.items():
+
+            for sample in dataset[dataset_split_name]:
+                instance = Instance(
+                    input=sample["question"],
+                    references=[Reference(Output(text=sample["answer"]), tags=[CORRECT_TAG])],
+                    split=helm_split_name,
+                )
+                instances.append(instance)
+
+        return instances
+
+    def get_instances(self, output_path: str) -> List[Instance]:
+        if self.splits is None:
+            splits = {"train": TRAIN_SPLIT, "validation": VALID_SPLIT, "test": TEST_SPLIT}
+        else:
+            splits = {}
+            if "train" in self.splits:
+                splits[self.splits[TRAIN_SPLIT]] = TRAIN_SPLIT
+            if "validation" in self.splits:
+                splits[self.splits[VALID_SPLIT]] = VALID_SPLIT
+            if "test" in self.splits:
+                splits[self.splits[TEST_SPLIT]] = TEST_SPLIT
+
+        instances: List[Instance] = self.get_instances_for_splits(splits=splits)
+        return instances
+
+
+class MELTKnowledgeZaloScenario(MELTClosedBookQAScenario):
+    """
+    Scenario for the Zalo dataset.
+    """
+
+    name = "melt_knowledge_zalo"
+    description = "Zalo dataset for closed-book question answering."
+    tags = ["question_answering", "knowledge"]
+
+    def __init__(self):
+        super().__init__(
+            dataset_name="ura-hcmut/zalo_e2eqa",
+            revision="63494521f4de949bfa57a5f0b79bc3ee47e635ad",
+            splits={
+                TRAIN_SPLIT: "train",
+                TEST_SPLIT: "test",
+            },
+        )
+
+
+class MELTMultipleChoiceQAScenario(Scenario):
+    name = "melt_multiple_choice_qa"
+    description = "Multiple Choice Question Answering scenario."
+    tags = ["question_answering"]
+
+    def __init__(
+        self,
+        dataset_name: str,
+        revision: str,
+        subset: Optional[str] = "",
+        splits: Optional[Dict[str, str]] = None,
+    ):
+        """
+        Initializes the question answering scenario.
+
+        Args:
+            dataset_name: The name of the dataset.
+            revision: The revision of the dataset to use.
+            subset: The subset of the dataset to use. Defaults to "".
+            splits: The splits to use for the dataset. Defaults to None.
+        """
+        super().__init__()
+        self.dataset_name = dataset_name
+        self.subset = subset
+        self.revision = revision
+        self.splits = splits
+
+    @abstractmethod
+    def process_example(self, sample: dict) -> Tuple[Input, List[Reference]]:
+        """
+        Given an sample from the dataset, create the input text and
+        list of answers for the instance.
+        """
+        pass
+
+    def get_instances_for_splits(self, splits: Dict[str, str]) -> List[Instance]:
+        """
+        Helper for generating instances for a split.
+        Args:
+            splits (dict): Which splits to partition the data into.
+        Returns:
+            List[Instance]: Instances from the file for the specified split.
+        """
+        instances: List[Instance] = []
+        dataset = load_dataset(
+            self.dataset_name,
+            self.subset,
+            revision=self.revision,
+            trust_remote_code=True,
+        )
+        for dataset_split_name, helm_split_name in splits.items():
+            for sample in dataset[dataset_split_name]:
+                inputs, references = self.process_example(sample)
+                instance = Instance(
+                    # input=sample["question"],
+                    # references=[Reference(Output(text=sample["answer"]), tags=[CORRECT_TAG])],
+                    input=inputs,
+                    references=references,
+                    split=helm_split_name,
+                )
+                instances.append(instance)
+
+        return instances
+
+    def get_instances(self, output_path: str) -> List[Instance]:
+        if self.splits is None:
+            splits = {"train": TRAIN_SPLIT, "validation": VALID_SPLIT, "test": TEST_SPLIT}
+        else:
+            splits = {}
+            if "train" in self.splits:
+                splits[self.splits[TRAIN_SPLIT]] = TRAIN_SPLIT
+            if "validation" in self.splits:
+                splits[self.splits[VALID_SPLIT]] = VALID_SPLIT
+            if "test" in self.splits:
+                splits[self.splits[TEST_SPLIT]] = TEST_SPLIT
+
+        instances: List[Instance] = self.get_instances_for_splits(splits=splits)
+        return instances
+
+
+class MELTKnowledgeViMMRCScenario(MELTMultipleChoiceQAScenario):
+    """
+    Scenario for the ViMMRC dataset.
+    """
+
+    name = "melt_knowledge_vimmrc"
+    description = "ViMMRC dataset for multiple choice question answering."
+    tags = ["question_answering", "knowledge"]
+
+    def __init__(self, randomize_order: bool = False):
+        super().__init__(
+            dataset_name="ura-hcmut/ViMMRC",
+            revision="fe68800e37aaa84d80b1d93466b36c3fa60d8bcb",
+            splits={
+                TRAIN_SPLIT: "train",
+                VALID_SPLIT: "validation",
+                TEST_SPLIT: "test",
+            },
+        )
+        self.randomize_order = randomize_order
+        self.correct_answer_mapping = {
+            "A": 0,
+            "B": 1,
+            "C": 2,
+            "D": 3,
+            "E": 4,
+            "F": 5,
+            "G": 6,
+            "H": 7,
+        }
+        random.seed(42)
+
+    def process_example(self, sample: dict) -> Tuple[Input, List[Reference]]:
+        """
+        Given an sample from the dataset, create the input text and
+        list of answers for the instance.
+        """
+        inputs = PassageQuestionInput(
+            passage=sample["article"],
+            passage_prefix="Ngữ cảnh: ",
+            question=sample["question"],
+            question_prefix="Câu hỏi: ",
+            separator="\n\n",
+        )
+
+        correct_idx = self.correct_answer_mapping[sample["answer"]]
+        references = []
+        for idx, answer in enumerate(eval(sample["options"])):
+            if idx == correct_idx:
+                tags = [CORRECT_TAG]
+            else:
+                tags = []
+
+            references.append(Reference(Output(text=answer), tags=tags))
+
+        if self.randomize_order:
+            k = random.randint(0, len(references) - 1)
+            references = references[k:] + references[:k]
+
+        return inputs, references
+
+
+class MELTToxicityDetectionViHSDScenario(MELTTextClassificationScenario):
+    """
+    Scenario for the UIT-ViHSD dataset.
+    """
+
+    name = "melt_toxicity_detection_vihsd"
+    description = "UIT-ViHSD dataset for toxicity detection."
+    tags = ["toxicity_detection"]
+
+    def __init__(self):
+        super().__init__(
+            dataset_name="ura-hcmut/UIT-ViHSD",
+            revision="16c4f67cf509d4f9f36ca5b63c5503c7c8830557",
+            text_key="free_text",
+            label_key="label_id",
+            splits={
+                TRAIN_SPLIT: "train",
+                VALID_SPLIT: "validation",
+                TEST_SPLIT: "test",
+            },
+        )
+        self.label_mapping = {
+            0: "clean",
+            1: "offensive",
+            2: "hate",
+        }
+
+    def process_example(self, sample: dict) -> Tuple[str, List[str]]:
+        """
+        Given an sample from the dataset, create the input text and
+        list of answers for the instance.
+        """
+        label = sample[self.label_key]
+        return sample[self.text_key], [self.label_mapping[label]]
+
+
+class MELTToxicityDetectionViCTSDScenario(MELTTextClassificationScenario):
+    """
+    Scenario for the UIT-ViCTSD dataset.
+    """
+
+    name = "melt_toxicity_detection_victsd"
+    description = "UIT-ViCTSD dataset for toxicity detection."
+    tags = ["toxicity_detection"]
+
+    def __init__(self):
+        super().__init__(
+            dataset_name="tarudesu/ViCTSD",
+            revision="65a073f2c48401410b264213229a6c52417f367a",
+            text_key="Comment",
+            label_key="Toxicity",
+            splits={
+                TRAIN_SPLIT: "train",
+                VALID_SPLIT: "validation",
+                TEST_SPLIT: "test",
+            },
+        )
+        self.label_mapping = {
+            0: "clean",
+            1: "toxicity",
+        }
+
+    def process_example(self, sample: dict) -> Tuple[str, List[str]]:
+        """
+        Given an sample from the dataset, create the input text and
+        list of answers for the instance.
+        """
+        label = sample[self.label_key]
+        return sample[self.text_key], [self.label_mapping[label]]
+
+
+class MELTInformationRetrievalScenario(Scenario):
+    name = "melt_information_retrieval"
+    description = "Scenario for information retrieval tasks."
+    tags = ["information_retrieval"]
+
+    """ Dictionary mapping task track tuples to the number of queries. """
+    NUM_TRAIN_QUERIES = 1000
+
+    """ Upper and lower bounds on top-k.
+
+    The top-k number represents the number of passages we will consider per
+    query. Max top-k for the train and validation files are set to the number
+    of passages included in the corresponding top-k files.
+    """
+    MIN_TOPK: int = 11
+    MAX_TRAIN_TOPK: int = 20
+    MAX_VALID_TOPK: int = 1000
+
+    def __init__(self, dataset_name: str, revision: str, subset: Optional[str] = "", valid_topk: Optional[int] = None):
+        """The constructor for the MSMARCOScenario.
+
+        Args:
+            dataset_name: The name of the dataset.
+            revision: The revision of the dataset to use.
+            subset: The subset of the dataset to use. Defaults to "".
+            valid_topk: If set, specifies the number of top documents for which the
+                validation instances will be created. Must be in the range
+                [self.MIN_TOPK, self.MAX_VALID_TOPK].
+        """
+        super().__init__()
+
+        # Input validation
+        self.dataset_name = dataset_name
+        self.revision = revision
+        self.subset = subset
+        self.valid_topk: Optional[int] = valid_topk
+        if self.valid_topk is not None:
+            assert valid_topk and self.MIN_TOPK <= valid_topk <= self.MAX_VALID_TOPK
+
+    def get_train_instances(self) -> List[Instance]:
+        """Get training instances.
+        References for each instance are selected as follows:
+            1. We select 1 correct reference, where the documents included
+            corresponds to the best document for the given train query.
+            2. We create 1 wrong reference, where the document included
+            corresponds to a non-gold document for the given train query.
+        """
+        dataset = load_dataset(
+            self.dataset_name,
+            self.subset,
+            revision=self.revision,
+            trust_remote_code=True,
+        )
+        instances = []
+        for i, sample in enumerate(dataset["train"]):
+
+            if i >= self.NUM_TRAIN_QUERIES:
+                break
+
+            references = [
+                Reference(Output(text=sample["positive"]), tags=[CORRECT_TAG]),
+                Reference(Output(text=sample["negative"]), tags=[]),
+            ]
+
+            instances.append(Instance(Input(text=sample["query"]), references=references, split=TRAIN_SPLIT))
+        return instances
+
+    def get_valid_instances(self) -> List[Instance]:
+        """Get validation instances.
+        By default, we create a reference for each Document ID for which there
+        is a judgment with respect to the provided Query ID.
+
+        If self.valid_topk is not None, we ensure that a reference is created
+        for all the documents that appear in top self.valid_topk documents for
+        the given validation query.
+        """
+        dataset = load_dataset(
+            self.dataset_name,
+            f"runs-{self.subset}",
+            revision=self.revision,
+            trust_remote_code=True,
+        )
+        instances = []
+        for sample in dataset["bm25"]:
+            references = []
+
+            for k, passage_dict in enumerate(sample["passages"]):
+                if self.valid_topk is None or k >= self.valid_topk:
+                    break
+                tags = []
+                tags.append(f"docid={passage_dict['id']}")
+                if k == 0:
+                    tags.append(CORRECT_TAG)
+                tags.append(make_rank_tag(rank=k + 1))  # Top-k rank
+                references.append(Reference(Output(text=passage_dict["passage"]), tags=tags))
+
+            instances.append(Instance(Input(text=sample["query"]), references=references, split=VALID_SPLIT))
+
+        return instances
+
+    def get_instances(self, output_path: str) -> List[Instance]:
+        """Get instances for this scenario.
+
+        Refer to the documentation of the following methods for details on how
+        the instances are created:
+            * self.get_train_instances
+            * self.get_valid_instances
+        """
+
+        hlog("Preparing training instances.")
+        train_instances = self.get_train_instances()
+
+        hlog("Preparing validation instances.")
+        valid_instances = self.get_valid_instances()
+
+        return train_instances + valid_instances
+
+
+class MELTInformationRetrievalMMARCOScenario(MELTTextClassificationScenario):
+    """
+    Scenario for the MMARCO dataset.
+    """
+
+    name = "melt_information_retrieval_mmarco"
+    description = "MMARCO dataset for information retrieval in Vietnamese."
+    tags = ["information_retrieval"]
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            dataset_name="unicamp-dl/mmarco",
+            revision="6d039c4638c0ba3e46a9cb7b498b145e7edc6230",
+            subset="vietnamese",
+            **kwargs,
+        )
+
+
+class MELTInformationRetrievalMRobustScenario(MELTTextClassificationScenario):
+    """
+    Scenario for the MRobust dataset.
+    """
+
+    name = "melt_information_retrieval_mrobust"
+    description = "MRobust dataset for information retrieval in Vietnamese."
+    tags = ["information_retrieval"]
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            dataset_name="unicamp-dl/mrobust",
+            revision="fda452a7fbfd9550db2f78d9d98e6b3ec16734df",
+            subset="vietnamese",
+            **kwargs,
         )
