3580f7338afb60a2f476439d770edee5531106eb
MiguelAFH <miguel.angel.fh@hotmail.com>
2025-05-03 13:25:47 -0700

    Refactored jury code

--- src/helm/benchmark/metrics/medalign_metrics.py
+++ src/helm/benchmark/metrics/medalign_metrics.py
@@ -1,34 +1,14 @@
-from typing import Any, Dict, List
-
-from helm.benchmark.adaptation.adapter_spec import AdapterSpec
-from helm.benchmark.adaptation.request_state import RequestState
 from helm.benchmark.annotation.medalign_annotator import ANNOTATOR_MODELS
-from helm.benchmark.metrics.metric import Metric
-from helm.benchmark.metrics.metric_name import MetricName
-from helm.benchmark.metrics.metric_service import MetricService
-from helm.benchmark.metrics.statistic import Stat
+from helm.benchmark.metrics.llm_jury_metrics import LLMJuryMetric
 
 
-class MedalignMetric(Metric):
+class MedalignMetric(LLMJuryMetric):
     """Score metrics for Medalign."""
 
-    def evaluate_generation(
-        self,
-        adapter_spec: AdapterSpec,
-        request_state: RequestState,
-        metric_service: MetricService,
-        eval_cache_path: str,
-    ) -> List[Stat]:
-        assert request_state.annotations
-        annotations: Dict[str, Any] = request_state.annotations["medalign"]
-        scores: List[int] = []
-        score = 0.0
-        for annotation_key, annotation_dict in annotations.items():
-            if annotation_key in ANNOTATOR_MODELS.keys() and annotation_dict is not None:
-                for val in annotation_dict.values():
-                    scores.append(int(val["score"]))
-        if scores:
-            score = sum(scores) / len(scores)
-        return [
-            Stat(MetricName("medalign_accuracy")).add(score),
-        ]
+    def __init__(self):
+        super().__init__(
+            metric_name="medalign_accuracy",
+            scenario_name="medalign",
+            annotator_models=ANNOTATOR_MODELS,
+            default_score=1.0,
+        )
