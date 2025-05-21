05994f5a5563a1ca6b088887102c9394f911d429
Yifan Mai <yifan@cs.stanford.edu>
2025-05-21 16:01:05 -0700

    Transpose frontend tables for MedHELM (#3602)

--- helm-frontend/src/components/GroupLeaderboard.tsx
+++ helm-frontend/src/components/GroupLeaderboard.tsx
@@ -3,6 +3,7 @@ import type Schema from "@/types/Schema";
 import Tab from "@/components/Tab";
 import Tabs from "@/components/Tabs";
 import LeaderboardTable from "@/components/LeaderboardTable";
+import TransposedLeaderboardTable from "@/components/TransposedLeaderboardTable";
 import type GroupsTable from "@/types/GroupsTable";
 import getGroupsTablesByName from "@/services/getGroupTablesByName";
 import Loading from "@/components/Loading";
@@ -44,6 +45,9 @@ export default function GroupLeaderboard({
     return <div>Group currently has no tables.</div>;
   }
 
+  // TODO: Add an flag to the schema for transposing tables.
+  const isMedHELM = schema.run_groups[0].name == "medhelm_scenarios";
+
   return (
     <div>
       {groupTables.length > 1 ? (
@@ -59,13 +63,23 @@ export default function GroupLeaderboard({
           ))}
         </Tabs>
       ) : null}
-      <LeaderboardTable
-        key={`${runGroupName}-${tableIndex}`}
-        schema={schema}
-        groupTable={groupTables[tableIndex]}
-        numRowsToDisplay={numRowsToDisplay}
-        sortColumnIndex={1}
-      />
+      {isMedHELM ? (
+        <TransposedLeaderboardTable
+          key={`${runGroupName}-${tableIndex}`}
+          schema={schema}
+          groupTable={groupTables[tableIndex]}
+          numRowsToDisplay={numRowsToDisplay}
+          sortColumnIndex={1}
+        />
+      ) : (
+        <LeaderboardTable
+          key={`${runGroupName}-${tableIndex}`}
+          schema={schema}
+          groupTable={groupTables[tableIndex]}
+          numRowsToDisplay={numRowsToDisplay}
+          sortColumnIndex={1}
+        />
+      )}
     </div>
   );
 }
