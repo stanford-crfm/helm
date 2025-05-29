78fef3c7abf284831677715b879c5081d67787b9
Yifan Mai <yifan@cs.stanford.edu>
2025-05-29 10:51:53 -0700

    Add new MedHELM landing page

--- helm-frontend/src/components/Landing/MedHELMLanding.tsx
+++ helm-frontend/src/components/Landing/MedHELMLanding.tsx
@@ -8,22 +8,20 @@ export default function MedHELMLanding() {
     <div className="container mx-auto px-16">
       <h1 className="text-3xl my-4 font-bold text-center">MedHELM</h1>
       <p className="text-xl my-4 italic text-center">
-        Medical and AI experts build a benchmark for evaluation of LLMs grounded
-        in real-world healthcare needs.
+        Holistic Evaluation of Large Language Models for Medical Tasks
       </p>
       <div className="flex flex-col lg:flex-row gap-8">
-        <div className="flex-1 text-l">
-          <p className="my-4">
-            We introduce {benchmarkName}, a comprehensive healthcare benchmark
-            to evaluate language models on real-world clinical tasks using real
-            electronic health records. Building on the HELM framework,{" "}
-            {benchmarkName} comprises a structured taxonomy with 5 categories,
-            22 subcategories, and 121 distinct clinical tasks as well as 31
-            diverse datasets (12 private, 6 gated-access, and 13 public). The
-            datasets represent a spectrum of healthcare scenarios, from
-            diagnostic decision-making to patient communication, providing a
-            more nuanced and clinically relevant assessment of AI capabilities
-            in healthcare settings.
+        <div className="flex-[7] text-l">
+          <p className="my-">
+            We introduce {benchmarkName}, an extensible evaluation framework for
+            assessing LLM performance for medical tasks. Building on the HELM
+            framework, {benchmarkName} comprises a structured taxonomy with 5
+            categories, 22 subcategories, and 121 distinct clinical tasks as
+            well as 35 distinct benchmarks (14 private, 7 gated-access, and 14
+            public). The benchmarks represent a spectrum of healthcare
+            scenarios, from diagnostic decision-making to patient communication,
+            providing a more nuanced and medically relevant assessment of AI
+            capabilities in healthcare settings.
           </p>
           <img
             src={overview}
@@ -56,8 +54,7 @@ export default function MedHELMLanding() {
           </div>
         </div>
         <div
-          className="py-2 pb-6 rounded-3xl bg-gray-100 h-full" // Stretched to full height
-          style={{ maxWidth: "100%" }}
+          className="flex-[3] py-2 rounded-3xl bg-gray-100 h-full" // Stretched to full height
         >
           <MiniLeaderboard />
           <div className="flex justify-end">
