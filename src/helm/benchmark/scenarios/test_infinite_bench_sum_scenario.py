535774881f7825a2e22cb71a9782de73bf032858
Yifan Mai <yifan@cs.stanford.edu>
2025-06-11 13:41:59 -0700

    Fix tests

--- src/helm/benchmark/scenarios/test_infinite_bench_sum_scenario.py
+++ src/helm/benchmark/scenarios/test_infinite_bench_sum_scenario.py
@@ -12,7 +12,7 @@ def count_words(text: str) -> int:
 @pytest.mark.scenarios
 def test_infinite_bench_sum_scenario():
     with TemporaryDirectory() as tmpdir:
-        scenario = InfiniteBenchSumScenario(min_num_words=0, max_num_words=10000000)
+        scenario = InfiniteBenchSumScenario(max_num_words=10000000)
         instances = scenario.get_instances(tmpdir)
         assert len(instances) == 103
         assert instances[0].split == "test"
@@ -23,7 +23,7 @@ def test_infinite_bench_sum_scenario():
         assert len(references[0].output.text) == 2865
         assert references[0].tags == [CORRECT_TAG]
 
-        scenario = InfiniteBenchSumScenario(min_num_words=0, max_num_words=100000)
+        scenario = InfiniteBenchSumScenario(max_num_words=100000)
         instances = scenario.get_instances(tmpdir)
         assert len(instances) == 48
         assert instances[0].split == "test"
@@ -32,15 +32,4 @@ def test_infinite_bench_sum_scenario():
         assert instances[0].extra_data["word_count"] == 69458
         references = instances[0].references
         assert len(references[0].output.text) == 4217
-        assert references[0].tags == [CORRECT_TAG]
-
-        scenario = InfiniteBenchSumScenario(min_num_words=30000, max_num_words=80000)
-        instances = scenario.get_instances(tmpdir)
-        assert len(instances) == 32
-        assert instances[0].split == "test"
-        assert len(instances[1].input.text) == 383396
-        assert instances[1].extra_data
-        assert instances[1].extra_data["word_count"] == 68482
-        references = instances[1].references
-        assert len(references[0].output.text) == 5667
         assert references[0].tags == [CORRECT_TAG]
