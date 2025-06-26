6b72910d8cbabed1ef8a617c84c984e476ebc61b
Yifan Mai <yifan@cs.stanford.edu>
2025-06-26 12:02:02 -0700

    Fixes

--- src/helm/clients/vllm_granite_thinking_client.py
+++ src/helm/clients/vllm_granite_thinking_client.py
@@ -1,7 +1,9 @@
-from typing import Any, Dict
+from dataclasses import replace
+import re
+from typing import Any, Dict, List, Tuple
 
 from helm.clients.vllm_client import VLLMChatClient
-from helm.common.request import Request
+from helm.common.request import GeneratedOutput, Request, RequestResult, Thinking
 
 
 class VLLMGraniteThinkingClient(VLLMChatClient):
@@ -18,3 +20,36 @@ class VLLMGraniteThinkingClient(VLLMChatClient):
         raw_request = super()._make_chat_raw_request(request)
         raw_request["extra_body"] = {"chat_template_kwargs": {"thinking": True}}
         return raw_request
+
+    def _parse_thinking(self, input: str) -> Tuple[str, str]:
+        """Return a tuple of thinking text and output text."""
+        match = re.match(r"<think>(.*)</think>\s*<response>(.*)</response>", input, re.DOTALL)
+        if match:
+            return (match.group(1), match.group(2))
+        
+        match = re.match(r"<think>(.*)</think>\s*<response>(.*)</response>", input, re.DOTALL)
+        if match:
+            return (match.group(1), match.group(2))
+        
+        match = re.match(r"<think>(.*)</think>\s*", input, re.DOTALL)
+        if match:
+            return (match.group(1), "")
+
+        match = re.match(r"<think>(.*)", input, re.DOTALL)
+        if match:
+            return (match.group(1), "")
+
+        return (input, "")
+
+    def _make_chat_request(self, request: Request) -> RequestResult:
+        request_result = super()._make_chat_request(request)
+        modified_completions: List[GeneratedOutput] = []
+        for completion in request_result.completions:
+            thinking, modified_text = self._parse_thinking(completion.text)
+            modified_completions.append(replace(
+                completion.text,
+                text=modified_text,
+                thinking=Thinking(text=thinking),
+            ))
+        return replace(request_result, completions=modified_completions)
+
