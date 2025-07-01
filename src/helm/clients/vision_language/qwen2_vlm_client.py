94e57d05abb48f4f6506b4dfdfc93da2b8365ea8
Tony Lee <tonyh.lee@yahoo.com>
2025-06-30 21:35:05 -0700

    update

--- src/helm/clients/vision_language/qwen2_vlm_client.py
+++ src/helm/clients/vision_language/qwen2_vlm_client.py
@@ -23,7 +23,7 @@ class LoadedModelProcessor:
 
 
 LOCAL_CHECKPOINT_3B = (
-    "/nlp/scr4/nlp/crfm/text2image/text2image-rlhf/robotics/Qwen2.5-VL/qwen-vl-finetune/output_more_data_6_27"
+    "/nlp/scr4/nlp/crfm/text2image/text2image-rlhf/robotics/Qwen2.5-VL/qwen-vl-finetune/output_more_data_6_29"
 )
 
 # Global cache for all models
