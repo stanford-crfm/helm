49573a2a302ad43b6efa37a8bec6fe4519b8b08f
Yifan Mai <yifan@cs.stanford.edu>
2025-07-02 10:11:04 -0700

    Add Arabic MMLU scenario (#3700)

--- src/helm/benchmark/run_specs/arabic_run_specs.py
+++ src/helm/benchmark/run_specs/arabic_run_specs.py
@@ -1,3 +1,7 @@
+"""Run specs for Arabic leaderboard
+
+EXPERIMENTAL: Run specs here may have future reverse incompatible changes."""
+
 from helm.benchmark.adaptation.adapter_spec import ADAPT_MULTIPLE_CHOICE_JOINT
 from helm.benchmark.adaptation.common_adapter_specs import get_multiple_choice_adapter_spec
 from helm.benchmark.metrics.common_metric_specs import get_exact_match_metric_specs
@@ -5,23 +9,22 @@ from helm.benchmark.run_spec import RunSpec, run_spec_function
 from helm.benchmark.scenarios.scenario import ScenarioSpec
 
 
-@run_spec_function("mmmlu")
-def get_mmmlu_spec(locale: str, subject: str) -> RunSpec:
-    scenario_spec = ScenarioSpec(
-        class_name="helm.benchmark.scenarios.mmmlu_scenario.MMMLUScenario", args={"locale": locale, "subject": subject}
-    )
+@run_spec_function("arabic_mmlu")
+def get_arabic_mmlu_spec() -> RunSpec:
+    """EXPERIMENTAL: This run spec here may have future reverse incompatible changes."""
+    scenario_spec = ScenarioSpec(class_name="helm.benchmark.scenarios.arabic_mmlu_scenario.ArabicMMLUScenario")
 
     adapter_spec = get_multiple_choice_adapter_spec(
         method=ADAPT_MULTIPLE_CHOICE_JOINT,
-        instructions=f"The following are multiple choice questions (with answers) about {subject.replace('_', ' ')}. Answer the last question. Respond only with only a single letter corresponding to your choice.",  # noqa: E501
+        instructions="The following are multiple choice questions. Answer the last question. Respond only with only a single letter corresponding to your choice.",  # noqa: E501
         input_noun="Question",
         output_noun="Answer",
     )
 
     return RunSpec(
-        name=f"mmmlu:locale={locale},subject={subject}",
+        name="arabic_mmlu",
         scenario_spec=scenario_spec,
         adapter_spec=adapter_spec,
         metric_specs=get_exact_match_metric_specs(),
-        groups=["mmmlu", f"mmmlu_{locale}_{subject}"],
+        groups=["arabic_mmlu"],
     )
