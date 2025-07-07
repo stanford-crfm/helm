0465d984a4f36151c730ba1cb8c850c3f7bc100b
Yifan Mai <yifan@cs.stanford.edu>
2025-07-07 15:52:27 -0700

    Add AraTrust (#3715)

--- src/helm/benchmark/run_specs/arabic_run_specs.py
+++ src/helm/benchmark/run_specs/arabic_run_specs.py
@@ -3,7 +3,7 @@
 EXPERIMENTAL: Run specs here may have future reverse incompatible changes."""
 
 from helm.benchmark.adaptation.adapter_spec import ADAPT_MULTIPLE_CHOICE_JOINT
-from helm.benchmark.adaptation.common_adapter_specs import get_multiple_choice_adapter_spec
+from helm.benchmark.adaptation.common_adapter_specs import get_multiple_choice_adapter_spec, get_generation_adapter_spec
 from helm.benchmark.metrics.common_metric_specs import get_exact_match_metric_specs
 from helm.benchmark.run_spec import RunSpec, run_spec_function
 from helm.benchmark.scenarios.scenario import ScenarioSpec
@@ -50,4 +50,24 @@ def get_alghafa_spec(subset: str) -> RunSpec:
         adapter_spec=adapter_spec,
         metric_specs=get_exact_match_metric_specs(),
         groups=["alghafa", f"alghafa_{subset}"],
+    )
+
+
+@run_spec_function("aratrust")
+def get_aratrust_spec() -> RunSpec:
+    """EXPERIMENTAL: This run spec here may have future reverse incompatible changes."""
+    scenario_spec = ScenarioSpec(class_name="helm.benchmark.scenarios.aratrust_scenario.AraTrustScenario")
+
+    adapter_spec = get_generation_adapter_spec(
+        instructions="The following are multiple choice questions. Answer the last question. Respond only with only a single letter corresponding to your choice.",  # noqa: E501
+        input_noun="Question",
+        output_noun="Answer",
+    )
+
+    return RunSpec(
+        name="aratrust",
+        scenario_spec=scenario_spec,
+        adapter_spec=adapter_spec,
+        metric_specs=get_exact_match_metric_specs(),
+        groups=["aratrust"],
     )
