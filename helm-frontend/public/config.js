b788295583817cb59e6c4b175db5df00c33be067
Yifan Mai <yifan@cs.stanford.edu>
2025-08-26 11:07:54 -0700

    Fix invalid sort column index error in leaderboard frontend

--- helm-frontend/public/config.js
+++ helm-frontend/public/config.js
@@ -1,4 +1,4 @@
-window.RELEASE = "v1.9.0";
+window.SUITE = "capabilities";
 window.BENCHMARK_OUTPUT_BASE_URL =
-	"https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/";
+	"/benchmark_output/";
 window.PROJECT_ID = "lite";
