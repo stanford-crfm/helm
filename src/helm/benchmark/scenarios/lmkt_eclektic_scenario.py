46b14bd7c8e7c273b880b980471c53ff5fc8ac66
Martin Nguyen <nguyenquangduc2000@gmail.com>
2025-09-02 09:50:12 -0700

    Fix for Yifan's comments

--- src/helm/benchmark/scenarios/lmkt_eclektic_scenario.py
+++ src/helm/benchmark/scenarios/lmkt_eclektic_scenario.py
@@ -1,14 +1,18 @@
 """A multilingual closed-book QA (CBQA) dataset that Evaluates
 Cross-Lingual Knowledge Transfer in a simple, black-box manner"""
 
+import os
 from typing import List
 from datasets import load_dataset
 
 from helm.benchmark.scenarios.scenario import (
     Scenario,
     Instance,
+    Reference,
     TEST_SPLIT,
+    CORRECT_TAG,
     Input,
+    Output,
 )
 
 SUPPORTED_LANGUAGES = {
@@ -41,6 +45,7 @@ class CulturalKnowledgeRememberingEclekticScenario(Scenario):
         dataset = load_dataset(
             "ura-hcmut/ECLeKTic",
             data_files="eclektic_main.jsonl",
+            cache_dir=os.path.join(output_path, "data"),
             trust_remote_code=True,
             revision="86650a00986420df9939b5f29d256aebad04c767",
             split="train",
@@ -50,7 +55,6 @@ class CulturalKnowledgeRememberingEclekticScenario(Scenario):
         for _, row in enumerate(dataset):
             for lang in SUPPORTED_LANGUAGES:
                 new_ex = {
-                    "q_id": row["q_id"],
                     "original_lang": row["original_lang"],
                     "lang": lang,
                     "title": row["title"],
@@ -58,15 +62,14 @@ class CulturalKnowledgeRememberingEclekticScenario(Scenario):
                     "orig_content": row["content"],
                     "orig_question": row["question"],
                     "orig_answer": row["answer"],
-                    "question": row[f"{lang}_q"],
-                    "answer": row[f"{lang}_a"],
                     "content": row[f"{lang}_c"],
                 }
 
                 input = Input(text=row[f"{lang}_q"].strip())
+                reference = Reference(Output(text=row[f"{lang}_a"]), tags=[CORRECT_TAG])
                 instance = Instance(
                     input=input,
-                    references=[],
+                    references=[reference],
                     split=TEST_SPLIT,
                     extra_data=new_ex,
                 )
