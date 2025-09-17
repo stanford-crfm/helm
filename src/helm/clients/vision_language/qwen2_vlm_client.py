a01f469934b4d37ad09c52b1653d85aedf0e2d5b
Tony Lee <tonyh.lee@yahoo.com>
2025-09-17 10:44:36 -0700

    resolve conflicts

--- src/helm/clients/vision_language/qwen2_vlm_client.py
+++ src/helm/clients/vision_language/qwen2_vlm_client.py
@@ -209,20 +209,14 @@ class Qwen2VLMClient(CachingClient):
                         processor = loaded.processor
 
                         # Prepare text and vision inputs.
-<<<<<<< HEAD
-                        text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+                        text = processor.apply_chat_template(   # type: ignore
+                            messages, tokenize=False, add_generation_prompt=True
+                        )
                         # Return video kwargs for fps, max_pixels, etc.
                         image_inputs, video_inputs, video_kwargs = process_vision_info(
                             messages, return_video_kwargs=True
                         )
-                        inputs = processor(
-=======
-                        text = processor.apply_chat_template(  # type: ignore
-                            messages, tokenize=False, add_generation_prompt=True
-                        )
-                        image_inputs, video_inputs = process_vision_info(messages)
-                        inputs = processor(  # type: ignore
->>>>>>> 83686ced2fb65e4438732cc58dd00089f6a7962c
+                        inputs = processor(     # type: ignore
                             text=[text],
                             images=image_inputs,
                             videos=video_inputs,
