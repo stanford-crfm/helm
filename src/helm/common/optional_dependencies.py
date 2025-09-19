3799406ef9ea860d5fb23daf410e40ea250c9892
Yifan Mai <yifan@cs.stanford.edu>
2025-09-19 10:26:45 -0700

    Add quotes to pip install instructions

--- src/helm/common/optional_dependencies.py
+++ src/helm/common/optional_dependencies.py
@@ -9,7 +9,7 @@ def handle_module_not_found_error(e: ModuleNotFoundError, suggestions: Optional[
     # TODO: Ask user to install more specific optional dependencies
     # e.g. crfm-helm[plots] or crfm-helm[server]
     suggested_commands = " or ".join(
-        [f"`pip install crfm-helm[{suggestion}]`" for suggestion in (suggestions or []) + ["all"]]
+        [f'`pip install "crfm-helm[{suggestion}]"`' for suggestion in (suggestions or []) + ["all"]]
     )
     raise OptionalDependencyNotInstalled(
         f"Optional dependency {e.name} is not installed. Please run {suggested_commands} to install it."
