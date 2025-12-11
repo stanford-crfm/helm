a03aaef85202c1d4138737c2ecb3263754b6a9aa
Yifan Mai <yifan@cs.stanford.edu>
2025-12-11 13:24:29 -0800

    Revert config

--- helm-frontend/public/config.js
+++ helm-frontend/public/config.js
@@ -1,4 +1,4 @@
 window.RELEASE = "v1.9.0";
 window.BENCHMARK_OUTPUT_BASE_URL =
 	"https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/";
-window.PROJECT_ID = "arabic";
+window.PROJECT_ID = "lite";
