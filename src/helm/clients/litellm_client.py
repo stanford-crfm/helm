774eeab7198da9434b591ceb5d48dbb84e05798c
Yifan Mai <yifan@cs.stanford.edu>
2026-01-09 11:59:11 -0800

    Remove commented out part

--- src/helm/clients/litellm_client.py
+++ src/helm/clients/litellm_client.py
@@ -114,63 +114,6 @@ class LiteLLMCompletionClient(CachingClient):
                     text=output_text, logprob=0.0, tokens=[], finish_reason={"reason": choice.finish_reason}
                 )
                 completions.append(completion)
-            #     choice.logprobs
-            #     choice.message
-
-            #     # The OpenAI chat completion API doesn't support echo.
-            #     # If `echo_prompt` is true, combine the prompt and completion.
-            #     raw_completion_content = raw_completion["message"]["content"]
-            #     if self.output_processor:
-            #         raw_completion_content = self.output_processor(raw_completion_content)
-            #     text: str = request.prompt + raw_completion_content if request.echo_prompt else raw_completion_content
-            #     # The OpenAI chat completion API doesn't return us tokens or logprobs, so we tokenize ourselves.
-            #     tokenization_result: TokenizationRequestResult = self.tokenizer.tokenize(
-            #         TokenizationRequest(text, tokenizer=self.tokenizer_name)
-            #     )
-            #     # Log probs are not currently not supported by the OpenAI chat completion API, so set to 0 for now.
-            #     tokens: List[Token] = [
-            #         Token(text=cast(str, raw_token), logprob=0) for raw_token in tokenization_result.raw_tokens
-            #     ]
-            #     # vLLM has a optional `reasoning_content` field in the message
-            #     # that is not in the standard OpenAI API.
-            #     # This field is also used by some model providers such as Grok.
-            #     thinking = (
-            #         Thinking(text=raw_completion["message"]["reasoning_content"])
-            #         if "reasoning_content" in raw_completion["message"]
-            #         else None
-            #     )
-            #     completion = GeneratedOutput(
-            #         text=,
-            #         logprob=0,  # OpenAI does not provide logprobs
-            #         tokens=tokens,
-            #         finish_reason={"reason": choice.finish_reason},
-            #         thinking=thinking,
-            #     )
-            #     completions.append(truncate_sequence(completion, request))  # Truncate the text by stop sequences
-
-            # # We can only return one completition really,
-            # # but we get an array of messages back, so we need to handle them
-
-            # reasoning_output = ""
-            # text_output = ""
-
-            # if request.echo_prompt:
-            #     text_output += request.prompt
-            # for output in response.output:
-            #     output_type = (
-            #         output.type
-            #     )  # one of "message" or "reasoning" from API observation, but can also include tool calls
-
-            #     if output_type == "reasoning":
-            #         reasoning_output += "\n\n".join([summary.text for summary in output.summary])
-            #     elif output_type == "message":
-            #         text_output += "\n\n".join([content.text for content in output.content])
-            #     # (Other output types are ignored)
-
-            # completion = GeneratedOutput(text=text_output, logprob=0.0, tokens=[], finish_reason={"reason": ""})
-            # if reasoning_output:
-            #     completion = dataclasses.replace(completion, thinking=Thinking(text=reasoning_output))
-            # completions.append(completion)
 
         return RequestResult(
             success=True,
