d2749ea0cd0c84d29c54575ffe4f03d3e3aa3134
Yifan Mai <yifan@cs.stanford.edu>
2026-03-17 14:04:33 -0700

    Remove unneeded code

--- src/helm/benchmark/tokenizer_config_registry.py
+++ src/helm/benchmark/tokenizer_config_registry.py
@@ -52,29 +52,6 @@ def register_tokenizer_configs_from_path(path: str) -> None:
         register_tokenizer_config(tokenizer_config)
 
 
-def auto_generate_tokenizer_config(name: str) -> TokenizerConfig:
-    name_parts = name.split("/")
-    name_base = name_parts[0]
-    if name_base == "huggingface":
-        from helm.tokenizers.huggingface_tokenizer import HuggingFaceTokenizer
-
-        pretrained_model_name_or_path = "/".join(name_parts[1:])
-        with HuggingFaceTokenizer.create_tokenizer(pretrained_model_name_or_path) as tokenizer:
-            end_of_text_token = tokenizer.eos_token or ""
-            prefix_token = tokenizer.bos_token or ""
-        return TokenizerConfig(
-            name=name,
-            tokenizer_spec=TokenizerSpec(
-                "helm.tokenizers.huggingface_tokenizer.HuggingFaceTokenizer",
-                args={"pretrained_model_name_or_path": pretrained_model_name_or_path},
-            ),
-            end_of_text_token=end_of_text_token,
-            prefix_token=prefix_token,
-        )
-    else:
-        raise NotImplementedError(f"Could not auto generate tokenizer config for tokenizer {name}")
-
-
 def get_tokenizer_config(name: str) -> Optional[TokenizerConfig]:
     tokenizer_config: Optional[TokenizerConfig] = TOKENIZER_NAME_TO_CONFIG.get(name)
     if not tokenizer_config:
